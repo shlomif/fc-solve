e9c09235701eea462bd8bef1f0e26ef864cc93c3
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-06-01 01:58:06 +0000

    Added an option to build an RPM.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@37 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/prepare_package.sh
+++ fc-solve/source/prepare_package.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+OPT="$1"
+
 automake
 autoconf
 (cd board_gen && automake && autoconf)
 ./configure && make dist
 
+if [ "$OPT" == "--rpm" ] ; then
+    rpm -bb freecell-solver-`cat ver.txt`.tar.gz
+fi
+    
