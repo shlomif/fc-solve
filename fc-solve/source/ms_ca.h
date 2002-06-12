4648bded89a2beb9cd77ada1e95ae3eac4da2548
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-06-12 17:24:25 +0000

    Added ms_ca.h - the header with the function that contains the move stacks compact allocation.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@89 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/source/ms_ca.h
@@ -0,0 +1,24 @@
+#include "caas.h"
+
+static GCC_INLINE fcs_move_stack_t * freecell_solver_move_stack_compact_allocate(freecell_solver_hard_thread_t * hard_thread, fcs_move_stack_t * old_move_stack_to_parent)
+{
+    char * ptr;
+    fcs_move_stack_t * new_move_stack_to_parent;
+    fcs_move_t * new_moves_to_parent;
+
+    fcs_compact_alloc_typed_ptr_into_var(
+        ptr, 
+        char, 
+        hard_thread->move_stacks_allocator, 
+        (sizeof(fcs_move_stack_t) + sizeof(fcs_move_t)*old_move_stack_to_parent->num_moves)
+        );
+    new_move_stack_to_parent = (fcs_move_stack_t *)ptr;
+    new_moves_to_parent = (fcs_move_t *)(ptr+sizeof(fcs_move_stack_t));
+    new_move_stack_to_parent->moves = new_moves_to_parent;
+    new_move_stack_to_parent->num_moves = 
+        new_move_stack_to_parent->max_num_moves = 
+        old_move_stack_to_parent->num_moves;
+    memcpy(new_moves_to_parent, old_move_stack_to_parent->moves, sizeof(fcs_move_t)*old_move_stack_to_parent->num_moves);
+    return new_move_stack_to_parent;
+}
+
