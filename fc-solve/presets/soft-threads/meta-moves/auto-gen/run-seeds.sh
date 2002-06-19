b6d38796709fffb8c677bf718c3ba1d391f44ecd
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-06-19 16:03:01 +0000

    Added run-seeds.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@133 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/run-seeds.sh
@@ -0,0 +1,16 @@
+seed=100
+max_seed=0
+max_num_solved=0
+
+while true ; do
+    echo "seed is $seed"
+    num_solved="`bash Analyze.sh --method random-dfs -seed $seed`"
+    if expr $num_solved \> $max_num_solved ; then
+        max_seed=$seed
+        max_num_solved=$num_solved
+    fi
+    echo "$max_num_solved were solved with $max_seed"
+
+    seed=`expr $seed + 1`
+done
+    
