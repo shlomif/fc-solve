df3a05a0e7b516a473bfe7a5e724f7efc4269ae0
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-07-05 11:13:29 +0000

    Modified fill_pos.pl so that it will process freecells and foundations.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@174 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- ext-ifaces/FC-Pro/fill_pos.pl
+++ ext-ifaces/FC-Pro/fill_pos.pl
@@ -9,13 +9,39 @@ my %suits_map = qw(C 0 D 1 S 2 H 3);
 sub card_to_num
 {
     my $card = shift;
+    if ($card eq "-")
+    {
+        return 0;
+    }
     my ($rank, $suit) = split(//, $card);
     return ($ranks_map{$rank} | ($suits_map{$suit}<<4));
 }
 
+my @foundation = (0,0,0,0);
+my @freecells = ();
+
 my $stack = 0;
 while(<>)
 {
+    if (/^Foundations:/)
+    {
+        s/^Foundations:\s*//;
+        my @f_s = split(/\s+/, $_);
+        foreach my $f (@f_s)
+        {
+            $f =~ m/([CHDS])-([1-9ATJKQ])/;
+            $foundation[$suits_map{$1}] = $ranks_map{$2};
+        }
+
+        next;
+    }
+    if (/^Freecells:/)
+    {
+        s/^Freecells:\s*//;
+        my $string = $_;
+        @freecells = (map { &card_to_num($_) } split(/\s+/, $string));
+        next;
+    }
     my @cards = split(/\s+/, $_);
     print "mypos.tableau[$stack].count = " . scalar(@cards) . ";\n";
     for my $i (0 .. $#cards)
@@ -26,20 +52,9 @@ while(<>)
     $stack++;
 }
 
+push @freecells, ((0) x (8 - scalar(@freecells)));
+
 my ($i);
-print <<"EOF";
-mypos.foundations[0] = 0;
-mypos.foundations[1] = 0;
-mypos.foundations[2] = 0;
-mypos.foundations[3] = 0;
-mypos.hold[0] = 0;
-mypos.hold[1] = 0;
-mypos.hold[2] = 0;
-mypos.hold[3] = 0;
-mypos.hold[4] = 0;
-mypos.hold[5] = 0;
-mypos.hold[6] = 0;
-mypos.hold[7] = 0;
-EOF
-;
+print join("", (map { "mypos.foundations[$_] = $foundation[$_]\n" } (0 .. 3)));
+print join("", (map { "mypos.hold[$_] = $freecells[$_]\n" } (0 .. 7)));
 
