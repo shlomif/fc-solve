186d5c11d0c9950c40b4352744cea5c20c51f189
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-07-06 07:30:38 +0000

    Added a script to upload all the stuff semi-automatically.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@182 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/site/wml/upload.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+make
+BERLIOS=BERLIOS make
+cd /var/www/html/fc-solve
+echo "Uploading to Vipe"
+echo
+scp * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
+cd /var/www/html/fc-solve-berlios
+echo "Uploading to Berlios"
+echo
+scp * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
+
