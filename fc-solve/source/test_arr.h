96bf476e99969c379cff2d0e15ef1c44927bbde2
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-07-11 01:32:34 +0000

    Added comments to test_arr.h
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@225 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/test_arr.h
+++ fc-solve/source/test_arr.h
@@ -1,3 +1,12 @@
+/*
+ * test_arr.h - header file for some routines and macros involving tests and 
+ * the like for Freecell Solver.
+ *
+ * Written by Shlomi Fish (shlomif@vipe.technion.ac.il), 2002
+ *
+ * This file is in the public domain (it's uncopyrighted).
+ * */
+
 #ifndef __TEST_ARR_H
 #define __TEST_ARR_H
 
@@ -33,12 +42,20 @@ extern freecell_solver_solve_for_state_test_t freecell_solver_sfs_tests[FCS_TEST
         ))                \
     )
 
+/*
+ * This macro traces the path of the state up to the original state,
+ * and thus calculates its real depth.
+ *
+ * It then assigns the newly updated depth throughout the path.
+ * 
+ * */
 #define calculate_real_depth(ptr_state_orig)                       \
 {                                                                  \
     if (calc_real_depth)                                           \
     {                                                              \
         int this_real_depth = 0;                                   \
         fcs_state_with_locations_t * ptr_state = (ptr_state_orig); \
+        /* Count the number of states until the original state. */ \
         while(ptr_state != NULL)                                   \
         {                                                          \
             ptr_state = ptr_state->parent;                         \
@@ -46,6 +63,7 @@ extern freecell_solver_solve_for_state_test_t freecell_solver_sfs_tests[FCS_TEST
         }                                                          \
         this_real_depth--;                                         \
         ptr_state = (ptr_state_orig);                              \
+        /* Assign the new depth throughout the path*/              \
         while (ptr_state->depth != this_real_depth)                \
         {                                                          \
             ptr_state->depth = this_real_depth;                    \
@@ -55,30 +73,44 @@ extern freecell_solver_solve_for_state_test_t freecell_solver_sfs_tests[FCS_TEST
     }                                                              \
 }                                                                  \
 
+/*
+ * This macro marks a state as a dead end, and afterwards propogates
+ * this information to its parent and ancestor states.
+ * */
 #define mark_as_dead_end(ptr_state_input) \
 {      \
     if (scans_synergy)      \
     {        \
         fcs_state_with_locations_t * ptr_state = (ptr_state_input); \
+        /* Mark as a dead end */        \
         ptr_state->visited |= FCS_VISITED_DEAD_END; \
         ptr_state = ptr_state->parent;          \
         if (ptr_state != NULL)                    \
         {           \
+            /* Decrease the refcount of the state */    \
             ptr_state->num_active_children--;   \
             while((ptr_state->num_active_children == 0) && (ptr_state->visited & FCS_VISITED_ALL_TESTS_DONE))  \
             {          \
+                /* Mark as dead end */        \
                 ptr_state->visited |= FCS_VISITED_DEAD_END;  \
+                /* Go to its parent state */       \
                 ptr_state = ptr_state->parent;    \
                 if (ptr_state == NULL)         \
                 {                \
                     break;             \
                 }      \
+                /* Decrease the refcount */       \
                 ptr_state->num_active_children--;     \
             }       \
         }   \
     }      \
 }
 
+/*
+ * This macro checks if we need to terminate from running this soft
+ * thread and return to the soft thread manager with an
+ * FCS_STATE_SUSPEND_PROCESS
+ * */
 #define check_if_limits_exceeded()                                    \
     (                                                                 \
         ((instance->max_num_times >= 0) &&                            \
