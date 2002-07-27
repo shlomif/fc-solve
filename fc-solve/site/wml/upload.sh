2275d52745b93c2c02886cbe4b7783a6a6e29cad
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-07-27 01:54:51 +0000

    1. I now use rsync to deliver the files.
    2. Deleted old win32 binaries.
    
    
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@260 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/upload.sh
+++ fc-solve/site/wml/upload.sh
@@ -5,9 +5,11 @@ BERLIOS=BERLIOS make
 cd /var/www/html/fc-solve
 echo "Uploading to Vipe"
 echo
-scp * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
+# scp * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
+rsync --rsh=ssh * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
 cd /var/www/html/fc-solve-berlios
 echo "Uploading to Berlios"
 echo
-scp * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
+# scp * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
+rsync --rsh=ssh * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
 
