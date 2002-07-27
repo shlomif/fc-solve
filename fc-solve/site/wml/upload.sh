4aed0d78fefb8d4497d370eb86975ba7152a36c2
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-07-27 02:00:05 +0000

    Temporarily changed the Berlios' transport to scp, since they don't seem to have rsync around.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@261 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/upload.sh
+++ fc-solve/site/wml/upload.sh
@@ -10,6 +10,6 @@ rsync --rsh=ssh * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
 cd /var/www/html/fc-solve-berlios
 echo "Uploading to Berlios"
 echo
-# scp * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
-rsync --rsh=ssh * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
+scp * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
+# rsync --rsh=ssh * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
 
