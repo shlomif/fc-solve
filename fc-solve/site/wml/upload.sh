c8260b29c720a34ef959220dab4a0ac7f89f3fb8
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-07-27 02:48:26 +0000

    Made the rsync transfer more verbose.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@262 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/upload.sh
+++ fc-solve/site/wml/upload.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+RSYNC="rsync --progress --verbose --rsh=ssh"
+
 make
 BERLIOS=BERLIOS make
 cd /var/www/html/fc-solve
 echo "Uploading to Vipe"
 echo
 # scp * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
-rsync --rsh=ssh * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
+$RSYNC * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
 cd /var/www/html/fc-solve-berlios
 echo "Uploading to Berlios"
 echo
-scp * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
-# rsync --rsh=ssh * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
+# scp * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
+$RSYNC * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
 
