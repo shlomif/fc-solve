5799c79c1d51cfebeb6375bdf3299c80374c288a
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-07-30 02:14:48 +0000

    win32_build is working A.OK on the client side.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@290 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/site/wml/src/win32_build/bootstrap/build.bat
@@ -0,0 +1,51 @@
+
+REM Assign the base URL of the site
+SET SERVER_URL=http://132.68.52.118/fc-solve/
+
+REM Assign the relative place where all the files required for
+REM Building the Win32 package can be found
+SET SERVER_W32_BUILD_URL=%SERVER_URL%win32_build/
+
+REM REM REM
+REM 
+REM     Cancelled
+REM 
+REM REM REM
+REM Retrieve the Version Number from the Web
+REM curl -o ver.txt %SERVER_W32_BUILD_URL%dynamic/ver.txt
+
+
+REM Fetch zip.exe and unzip.exe
+curl -o zip.exe %SERVER_W32_BUILD_URL%static/zip.exe
+curl -o unzip.exe %SERVER_W32_BUILD_URL%static/unzip.exe
+
+REM Retrieve the archive
+curl -o fcs.zip %SERVER_W32_BUILD_URL%dynamic/fcs.zip
+
+mkdir src
+cd src
+..\unzip ..\fcs.zip
+
+REM Set the MSVC building parameters
+
+SET MSVC_PATH=C:\Program Files\Microsoft Visual Studio\VC98\Bin
+
+call "%MSVC_PATH%\vcvars32"
+
+REM Build Everything
+copy config.h.win32 config.h
+nmake /f Makefile.win32
+cd board_gen
+nmake /f Makefile.win32
+cd ..\..\
+
+REM Put the version number inside the variable VERSION
+FOR /F " tokens=1 delims=, " %%i in (.\src\ver.txt) do set VERSION=%%i
+
+SET ARC_DIR=freecell-solver-%VERSION%-bin
+mkdir %ARC_DIR%
+FOR %%i in (ChangeLog fc-solve.exe freecell-solver-range-parallel-solve.exe README README.win32.txt TODO USAGE) do copy src\%%i %ARC_DIR%\
+mkdir %ARC_DIR%\board_gen
+FOR %%i in (COPYING make_pysol_freecell_board.py make-aisleriot-freecell-board.exe make-gnome-freecell-board.exe make-microsoft-freecell-board.exe README) do copy src\board_gen\%%i %ARC_DIR%\board_gen
+
+zip -r freecell-solver-%VERSION%.win32.bin.zip %ARC_DIR%
