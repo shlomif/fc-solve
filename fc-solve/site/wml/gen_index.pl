ad9e6a0c0943d7b7bf725021bc65e4bd290a1808
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-08-07 17:08:46 +0000

    Fixed some bugs.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@303 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/gen_index.pl
+++ fc-solve/site/wml/gen_index.pl
@@ -5,15 +5,18 @@ use strict;
 use CGI;
 
 my $dir_name = shift || ".";
+my $filename = shift || "index.html";
 
-opendir D, ".";
-my @files = <D>;
+opendir D, $dir_name;
+my @files = readdir(D);
 closedir(D);
 
-@files = grep { ($_ ne "index.html") && (! /\.\.?/) } @files;
+@files = grep { ($_ ne "index.html") && (! /^\.\.?$/) && ($_ ne "CVS")} @files;
+@files = sort { $a cmp $b } @files;
+@files = map { $_ . ((-d "$dir_name/$_") ? "/" : "") } @files;
 
-open O, ">index.html";
-print O, <<"EOF" ;
+open O, ">$filename";
+print O <<"EOF" ;
 <html>
 <head>
 <title>Index for $dir_name</title>
@@ -24,13 +27,15 @@ print O, <<"EOF" ;
 <ul>
 EOF
 
-print O join("", map { my $escaped = CGI::escapeHTML($_); "<li><a href=\"$escaped\">$escaped</a></li>\n" });
+print O join("", map { my $escaped = CGI::escapeHTML($_); "<li><a href=\"$escaped\">$escaped</a></li>\n" } @files);
 
-print O, <<"EOF" ;
+print O <<"EOF" ;
 </ul>
+<h3><a href="../">Up one level</a></h3>
 </body>
 </html>
 EOF
 ;
 
+close(O);
 
