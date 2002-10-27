82ec43c8bd9f1f233a3394358010418c574ee781
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-10-27 03:33:35 +0000

    A more or less finished ds_order.h file.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@424 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/ds_order.h
+++ fc-solve/source/ds_order.h
@@ -5,6 +5,48 @@
 #ifndef __DS_ORDER_H
 #define __DS_ORDER_H
 
+#include "move.h"
 
+struct fcs_derived_states_order_struct;
+
+typedef struct fcs_derived_states_order_struct fcs_derived_states_order_t;
+
+struct fcs_derived_states_order_instance_struct;
+
+typedef struct fcs_derived_states_order_instance_struct fcs_derived_states_order_instance_t;
+
+
+struct fcs_derived_states_order_struct
+{
+    char * name;
+    int (*init_instance)(
+        fcs_derived_states_order_t * order,
+        fcs_derived_states_order_instance_t * order_instance,        
+        freecell_solver_soft_thread_t * soft_thread,
+        char * spec_string,
+        char * * end_of_spec
+        );
+    int (*give_init_state)(
+        fcs_derived_states_order_instance_t * order_instance,
+        fcs_state_with_locations_t * init_state
+        );
+    void (*free_instance)(
+        fcs_derived_states_order_instance_t * order_instance
+        );
+    int (*order_states)(
+        fcs_derived_states_order_instance_t * order_instance,
+        fcs_state_with_locations_t * src_state,
+        fcs_derived_states_list_t * derived_states,
+        int * * out_indexes
+        );
+};
+
+struct fcs_derived_states_order_instance_struct
+{
+    fcs_derived_states_order_t * order;
+    freecell_solver_soft_thread_t * soft_thread;
+    fcs_state_with_locations_t * init_state;
+    void * context;
+};
 
 #endif /* #ifndef __DS_ORDER_H */
