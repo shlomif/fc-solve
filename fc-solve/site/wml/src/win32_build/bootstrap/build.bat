1136ec57c07319752da2fd37bdda4d070a62e770
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2002-10-31 04:06:41 +0000

    Added the 2.8.x-compatible build.bat.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@437 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/src/win32_build/bootstrap/build.bat
+++ fc-solve/site/wml/src/win32_build/bootstrap/build.bat
@@ -26,6 +26,26 @@ REM           If they were not already fetched.
 IF NOT EXIST zip.exe curl -o zip.exe %SERVER_W32_BUILD_URL%static/zip.exe
 IF NOT EXIST unzip.exe curl -o unzip.exe %SERVER_W32_BUILD_URL%static/unzip.exe
 
+SET MSVC_PATH=C:\Program Files\Microsoft Visual Studio\VC98\Bin
+
+call "%MSVC_PATH%\vcvars32"
+
+
+IF EXIST add-cr.exe GOTO AFTER_ADD_CR
+echo #include "stdio.h" > add_cr.c
+echo int main(int argc, char * argv[]) >> add_cr.c
+echo { >> add_cr.c
+echo FILE * i, * o; char buf[1]; >> add_cr.c
+echo i = fopen(argv[1], "rb"); >> add_cr.c
+echo o = fopen(argv[2], "wb"); >> add_cr.c
+echo while(fread(buf, 1,1,i)) { if (buf[0] == '\n') { fputs("\r\n",o); } else {fputc(buf[0],o);}} >> add_cr.c
+echo fclose(i); fclose(o); >> add_cr.c
+echo } >> add_cr.c
+cl /Feadd-cr.exe add_cr.c
+
+:AFTER_ADD_CR
+
+
 REM Retrieve the archive
 curl -o fcs.zip %SERVER_W32_BUILD_URL%dynamic/fcs.zip
 
@@ -35,12 +55,10 @@ cd src
 
 REM Set the MSVC building parameters
 
-SET MSVC_PATH=C:\Program Files\Microsoft Visual Studio\VC98\Bin
-
-call "%MSVC_PATH%\vcvars32"
 
 REM Build Everything
 copy config.h.win32 config.h
+copy prefix.h.win32 prefix.h
 nmake /f Makefile.win32
 cd board_gen
 nmake /f Makefile.win32
@@ -54,5 +72,14 @@ mkdir %ARC_DIR%
 FOR %%i in (ChangeLog fc-solve.exe freecell-solver-range-parallel-solve.exe README README.win32.txt TODO USAGE) do copy src\%%i %ARC_DIR%\
 mkdir %ARC_DIR%\board_gen
 FOR %%i in (COPYING make_pysol_freecell_board.py make-aisleriot-freecell-board.exe make-gnome-freecell-board.exe make-microsoft-freecell-board.exe README) do copy src\board_gen\%%i %ARC_DIR%\board_gen
+mkdir %ARC_DIR%\"To-Program-Files"
+mkdir %ARC_DIR%\"To-Program-Files"\"Freecell Solver"
+mkdir %ARC_DIR%\"To-Program-Files"\"Freecell Solver"\share
+SET DATA_DIR=%ARC_DIR%\To-Program-Files\Freecell Solver\share\freecell-solver
+mkdir %ARC_DIR%\To-Program-Files\"Freecell Solver"\share\freecell-solver
+.\add-cr src\Presets\presetrc.win32 p
+copy p %ARC_DIR%\To-Program-Files\"Freecell Solver"\share\freecell-solver\presetrc
+mkdir %ARC_DIR%\To-Program-Files\"Freecell Solver"\share\freecell-solver\presets
+copy src\Presets\presets\*.sh %ARC_DIR%\To-Program-Files\"Freecell Solver"\share\freecell-solver\presets
 
 zip -r freecell-solver-%VERSION%.win32.bin.zip %ARC_DIR%
