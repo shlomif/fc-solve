0ffe2021bce222232c0fa549eb07006980afaeb4
(no author) <(no author)@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2003-03-19 08:29:13 +0000

    This commit was manufactured by cvs2svn to create tag 'LATEST_DEVEL'.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/tags/LATEST_DEVEL@574 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/download_arcs.sh
+++
@@ -1,5 +0,0 @@
-cd src
-scp -r shlomif@vipe:public_html/freecell-solver/\*.zip .
-scp -r shlomif@vipe:public_html/freecell-solver/\*.tar.gz .
-scp -r shlomif@vipe:public_html/freecell-solver/\*.patch .
-
