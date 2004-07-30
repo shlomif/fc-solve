dca1c4f9ded9486a02e42a03ac1eac92cf423745
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2004-07-30 12:49:59 +0000

    1. Made sure everything is generated into the local directory instead of a /-based path. (which makes the build process more portable).
    2. Updated to FCS 2.8.7.
    
    
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@605 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/put-recent-devel-version.sh
+++ fc-solve/site/wml/put-recent-devel-version.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 mypath="$1"
-cd t/fc-solve/source
+( cd t/fc-solve/source
 arc_name="freecell-solver-`cat ver.txt`.tar.gz"
-if [ ! -e $arc_name ] ; then
+if [ ! -e "$arc_name" ] ; then
     ./prepare_package.sh
-fi
-cp -f $arc_name $mypath
+fi 
+cat "$arc_name"
+) > "$mypath"
 
