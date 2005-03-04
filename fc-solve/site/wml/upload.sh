6a5d0af7657e704f0b7f899eeba3010443cb1c4a
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2005-03-04 12:15:09 +0000

    berlios_url.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@614 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/upload.sh
+++ fc-solve/site/wml/upload.sh
@@ -4,14 +4,22 @@ RSYNC="rsync --progress --verbose --rsh=ssh -r"
 
 make
 BERLIOS=BERLIOS make
-cd /var/www/html/fc-solve
+stable_ver="`./get-recent-stable-version.sh`"
+pushd .
+cd ./dest
+ls freecell-solver-*.tar.gz | 
+    grep -vF 'freecell-solver-'"${stable_ver}" |
+    grep -vP 'freecell-solver-[0-9]+\.[0-9]*[13579]\.' |
+    xargs rm -f
 echo "Uploading to Vipe"
 echo
 # scp * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
 $RSYNC * shlomif@vipe.technion.ac.il:public_html/freecell-solver/
-cd /var/www/html/fc-solve-berlios
+popd
+pushd .
+cd ./dest-berlios
 echo "Uploading to Berlios"
 echo
 # scp * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
 $RSYNC * shlomif@shell.berlios.de:/home/groups/fc-solve/htdocs/
-
+popd
