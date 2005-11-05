c4f8419b631ed225b278e0ec3c14815ad197492f
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2005-11-05 15:47:35 +0000

    Got the compilation of the source to work again. Added a few scripts, etc.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@643 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/gen_index.pl
+++ fc-solve/site/wml/gen_index.pl
@@ -17,6 +17,9 @@ closedir(D);
 
 open O, ">$filename";
 print O <<"EOF" ;
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
 <title>Index for $dir_name</title>
--- fc-solve/site/wml/get-recent-stable-version.sh
+++ fc-solve/site/wml/get-recent-stable-version.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-cd src && echo $(ls freecell-solver-* | 
-    grep -v win32 | 
-    sed 's/^freecell-solver-//' | 
-    sed 's/\.tar\.gz$//' | 
-    grep '[0-9]*\.[0-9]*[02468]\.' | 
-    perl -e '@a=(map { chomp($_); [split(/\./, $_) ] } <>) ;print join("", (map { join(".", @$_) ."\n"; } (sort { ($a->[0] <=> $b->[0]) || ($a->[1] <=> $b->[1]) || ($a->[2] <=> $b->[2]) } @a)));' | 
-    tail -1)
-    
+(if [ -e Source ] ; then
+    svn update Source 
+else
+    svn checkout 'svn://svn.berlios.de/fc-solve/branches/Version-2_8_x/fc-solve/source/' Source
+fi) > /dev/null 2>&1
+(cd Source && cat ver.txt)
