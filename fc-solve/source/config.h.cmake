88bf56053fbdd3621aa7aff95fd6f636a1bab240
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2008-08-01 12:25:30 +0000

    Merged in the changes from the "conversion-to-cmake" branch.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@916 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/config.h.cmake
+++ fc-solve/source/config.h.cmake
@@ -14,17 +14,17 @@
 extern "C" {
 #endif
 
-#undef DEBUG_STATES
-#undef COMPACT_STATES
-#undef INDIRECT_STACK_STATES
+#cmakedefine DEBUG_STATES
+#cmakedefine COMPACT_STATES
+#cmakedefine INDIRECT_STACK_STATES
 
-#undef CARD_DEBUG_PRES
+#cmakedefine CARD_DEBUG_PRES
 
 /*
  * Define this macro if the C compiler supports the keyword inline or
  * a similar keyword that was found by Autoconf (and defined as inline).
  * */
-#undef HAVE_C_INLINE
+#cmakedefine HAVE_C_INLINE
 
 
 /*
@@ -43,27 +43,27 @@ extern "C" {
     The amount the pack pointers array grows by. Shouldn't be too high
     because it doesn't happen too often.
 */
-#define IA_STATE_PACKS_GROW_BY 32
+#cmakedefine IA_STATE_PACKS_GROW_BY 32
 
 /*
  * The maximal number of Freecells. For efficiency's sake it should be a
  * multiple of 4.
  * */
 
-#define MAX_NUM_FREECELLS 4
+#define MAX_NUM_FREECELLS ${MAX_NUM_FREECELLS}
 
 /*
  * The maximal number of Stacks. For efficiency's sake it should be a
  * multiple of 4.
  * */
 
-#define MAX_NUM_STACKS 10
+#define MAX_NUM_STACKS ${MAX_NUM_STACKS}
 /*
  * The maximal number of initial cards that can be found in a stack.
  * */
-#define MAX_NUM_INITIAL_CARDS_IN_A_STACK 8
+#define MAX_NUM_INITIAL_CARDS_IN_A_STACK ${MAX_NUM_INITIAL_CARDS_IN_A_STACK}
 
-#define MAX_NUM_DECKS 2
+#define MAX_NUM_DECKS ${MAX_NUM_DECKS}
 
 
 #define FCS_STATE_STORAGE_INDIRECT 0
@@ -82,11 +82,51 @@ extern "C" {
 #define FCS_STACK_STORAGE_GLIB_TREE 4
 #define FCS_STACK_STORAGE_GLIB_HASH 5
 
-#undef FCS_STATE_STORAGE
-#undef FCS_STACK_STORAGE
+#define FCS_STATE_STORAGE ${FCS_STATE_STORAGE}
+#define FCS_STACK_STORAGE ${FCS_STACK_STORAGE}
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif
+
+/* Define to 1 if you have the `avl' library (-lavl). */
+#undef HAVE_LIBAVL
+
+/* Define to 1 if you have the `glib' library (-lglib). */
+#undef HAVE_LIBGLIB
+
+/* Define to 1 if you have the `m' library (-lm). */
+#cmakedefine HAVE_LIBM
+
+/* Define to 1 if you have the `redblack' library (-lredblack). */
+#undef HAVE_LIBREDBLACK
+
+/* Name of package */
+#define PACKAGE "${PACKAGE}"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "${PACKAGE_BUGREPORT}"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "${PACKAGE_NAME}"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "${PACKAGE_STRING}"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "${PACKAGE_TARNAME}"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "${PACKAGE_VERSION}"
+
+/* Version number of package */
+#define VERSION "${VERSION}"
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+#undef inline
+#endif
+
