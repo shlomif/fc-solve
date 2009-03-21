1ebf52cfc8dc41d8349ae01fb78c9d739beab5af
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-03-21 17:33:41 +0000

    Added rudimentary integration with the blogspot blog.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@1344 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/site/wml/lib/MyNews.pm
+++ fc-solve/site/wml/lib/MyNews.pm
@@ -3,11 +3,41 @@ package MyNews;
 use strict;
 use warnings;
 
-use MyOldNews;
 use POSIX;
-use DateTime;
 
-sub _render_old_news_item
+use CGI;
+
+use DateTime;
+use XML::Feed;
+
+use MyOldNews;
+
+sub _render_news_item
+{
+    my $item = shift;
+
+    my $issued = $item->{'issued'};
+    my $id = $item->{'id'};
+    my $html = $item->{'html'};
+    my $title = $item->{'title'} || "";
+
+    $title = $issued->strftime("%d-%b-%Y")
+        . (length($title) ? ": " : "") 
+        . $title;
+
+    # Remove a leading 0 which is ugly.
+    $title =~ s{\A0}{};
+
+    my $id_esc = CGI::escapeHTML($id);
+    my $title_esc = CGI::escapeHTML($title);
+
+    return <<"EOF";
+<h3 id="$id_esc">$title_esc</h3>
+$html
+EOF
+}
+
+sub _convert_from_old_news_item
 {
     my $item = shift;
 
@@ -17,37 +47,76 @@ sub _render_old_news_item
 
     my $html = $item->{'html'};
 
-    my ($h3_id, $title) = DateTime->new(
+    my $issued = DateTime->new(
         year => $y, month => $m, day => $d,
         hour => 12, minute => 0, second => 47,
-    )->strftime("news-%Y-%m-%d", "%d-%b-%Y");
+    );
 
-    # Remove a leading 0 which is ugly.
-    $title =~ s{\A0}{};
+    return 
+        {
+            issued => $issued,
+            html => $html,
+            id => $issued->strftime("news-%Y-%m-%d"),
+        };
+}
 
-    return <<"EOF";
-<h3 id="$h3_id">$title</h3>
-$html
-EOF
+sub _filter_out_br_tags
+{
+    my $text = shift;
+    $text =~ s{< *br */? *>}{}g;
+    return $text;
+}
+
+sub _calc_blog_news
+{
+    my $feed = XML::Feed->parse(URI->new("http://fc-solve.blogspot.com/feeds/posts/default"))
+        or die XML::Feed->errstr;
+
+    my @results;
+    for my $entry ($feed->entries) {
+        push @results, 
+            {
+                issued => $entry->issued->clone(),
+                html => _filter_out_br_tags($entry->content->body()),
+                title => $entry->title(),
+                id => 
+                ($entry->issued->strftime("news-%Y-%m-%d-") . $entry->id()),
+            }
+            ;
+    }
+
+    return \@results;
 }
 
 my $num_new_items = 6;
 
+my $results =
+    [ 
+        (map { _convert_from_old_news_item($_) } @{MyOldNews::get_old_news()}), 
+        @{_calc_blog_news()}
+    ];
+
+sub _print_results
+{
+    my $start_idx = shift;
+    my $end_idx = shift;
+
+    print map { _render_news_item($_) } 
+        reverse(@{$results}[$start_idx .. $end_idx])
+        ;
+
+    return;
+}
+
 sub print_old
 {
-    my $results = MyOldNews::get_old_news();
-    print map { _render_old_news_item($_) } 
-        reverse(@{$results}[0 .. ($#{$results} - $num_new_items)])
-        ;
+    return _print_results(0, ($#{$results} - $num_new_items));
 }
 
 sub print_new
 {
-    my $results = MyOldNews::get_old_news();
-    print map { _render_old_news_item($_) } 
-        reverse(@{$results}[(@{$results} - $num_new_items) .. $#{$results}])
-        ;
-
+    return _print_results((@{$results} - $num_new_items), $#{$results});
 }
+
 1;
 
