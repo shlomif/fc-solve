4543b719b19fc5fe1a4945103408c3319909aee8
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-03-31 14:52:34 +0000

    Converted the board_gen executables to the MIT/X11 Licence.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@1384 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/board_gen/make_aisleriot_freecell_board.c
+++ fc-solve/source/board_gen/make_aisleriot_freecell_board.c
@@ -1,3 +1,26 @@
+/* Copyright (c) 2000 Shlomi Fish
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 /*
  * make_aisleriot_freecell_board.c - Program to generate a GNOME Aisleriot
  * Freecell board for input to Freecell Solver.
@@ -8,11 +31,6 @@
  * boards on different systems. If you want it to generate the board
  * you are playing, make sure it uses the same libc as the computer on
  * which you run GNOME Aisleriot.
- *
- * Written by Shlomi Fish, 2000
- *
- * This code is under the public domain.
- *
  *
  */
 
--- fc-solve/source/board_gen/make_pysol_freecell_board.c
+++ fc-solve/source/board_gen/make_pysol_freecell_board.c
@@ -1,3 +1,26 @@
+/* Copyright (c) 2000 Shlomi Fish
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 /*
  * make_pysol_freecell_board.c - Program to generate a PySol Freecell board
  * for input to Freecell Solver.
@@ -12,9 +35,6 @@
  * Another Note: Boards 1 through 32000 are the same as Microsoft Freecell's.
  *
  * Written by Shlomi Fish, 2000
- *
- * This code is under the public domain.
- *
  *
  */
 
