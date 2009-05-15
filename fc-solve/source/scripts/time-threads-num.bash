ff3c4b663643c35e06736aa8cc9459ee3a5ed26a
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-05-15 19:04:35 +0000

    Fixed the time-threads-num.bash to handle lack of global FCS installations.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@1663 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/scripts/time-threads-num.bash
+++ fc-solve/source/scripts/time-threads-num.bash
@@ -2,12 +2,21 @@
 MAX="${1:-6}"
 shift
 
-strip *
+strip * > /dev/null 2>&1
 
 if ! test -e DUMPS ; then
     mkdir DUMPS
 fi
 
+p_dir="__p"
+if ! test -e "$p_dir" ; then
+    cp -R Presets "$p_dir"
+    
+    P="$(cd "$p_dir"/presets && pwd)" \
+        perl -lpi -e 's!\A(dir=).*\z!$1$ENV{P}/!ms' ./"$p_dir"/presetrc
+fi
+
+export FREECELL_SOLVER_PRESETRC="$(ls $(pwd)/"$p_dir"/presetrc)"
 for NUM in $(seq 1 "$MAX") ; do
     echo "Testing $NUM"
     ./freecell-solver-multi-thread-solve 1 32000 500 \
