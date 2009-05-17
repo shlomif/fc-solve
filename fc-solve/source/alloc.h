1194684aa0d6bee1709fc689a961923ef9b83c1a
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-05-17 19:46:12 +0000

    Bug fix: fixed an off-by-one allocation error in alloc.h.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@1709 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/alloc.h
+++ fc-solve/source/alloc.h
@@ -58,15 +58,16 @@ extern char *
         int how_much
             );
 #else
+#define fcs_compact_alloc_into_var_MYSIZE(what_t) ((sizeof(what_t))+(sizeof(char *)-((sizeof(what_t))&(sizeof(char *)-1))))
 #define fcs_compact_alloc_into_var(result,allocator_orig,what_t) \
 { \
    register fcs_compact_allocator_t * allocator = (allocator_orig); \
-   if (allocator->max_ptr - allocator->ptr < sizeof(what_t))  \
+   if (allocator->max_ptr - allocator->ptr < fcs_compact_alloc_into_var_MYSIZE(what_t))  \
     {      \
         fc_solve_compact_allocator_extend(allocator);      \
     }         \
     allocator->rollback_ptr = allocator->ptr;       \
-    allocator->ptr += ((sizeof(what_t))+(sizeof(char *)-((sizeof(what_t))&(sizeof(char *)-1))));      \
+    allocator->ptr += fcs_compact_alloc_into_var_MYSIZE(what_t);      \
     result = (what_t *)allocator->rollback_ptr;       \
 }
 
