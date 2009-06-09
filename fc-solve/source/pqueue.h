c6e09fd667c539ef0cfbab648b9fc843136f7716
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-06-09 13:50:06 +0000

    Removed more unused identifiers.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@1905 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/pqueue.h
+++ fc-solve/source/pqueue.h
@@ -37,7 +37,6 @@ typedef struct _PQUEUE
     int MaxSize;
     int CurrentSize;
     pq_element_t * Elements; /* pointer to void pointers */
-    pq_rating_t MaxRating; /* biggest element possible */
 } PQUEUE;
 
 /* given an index to any element in a binary tree stored in a linear array with the root at 1 and
@@ -49,7 +48,6 @@ typedef struct _PQUEUE
 
 /* left and right children are index * 2 and (index * 2) +1 respectively */
 #define PQ_LEFT_CHILD_INDEX(i) ((i)<<1)
-#define PQ_RIGHT_CHILD_INDEX(i) (((i)<<1)+1)
 
 void fc_solve_PQueueInitialise(
     PQUEUE *pq,
