1fc144778c5bc0ab8d40d3ff4799fba6b4b70120
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-06-09 18:25:44 +0000

    Removed fc_solve_hash_free_with_callback from the fcs_hash.c
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@1931 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/fcs_hash.c
+++ fc-solve/source/fcs_hash.c
@@ -180,29 +180,6 @@ rehash_check:
     return 0;
 }
 
-void fc_solve_hash_free_with_callback(
-    SFO_hash_t * hash,
-    void (*function_ptr)(void * key, void * context)
-    )
-{
-    int i;
-    SFO_hash_symlink_item_t * item, * next_item;
-
-    for(i=0;i<hash->size;i++)
-    {
-        item = hash->entries[i].first_item;
-        while (item != NULL)
-        {
-            function_ptr(item->key, hash->context);
-            next_item = item->next;
-
-            item = next_item;
-        }
-    }
-
-    fc_solve_hash_free(hash);
-}
-
 void fc_solve_hash_free(
     SFO_hash_t * hash
     )
