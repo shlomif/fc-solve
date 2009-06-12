13aa6a814e262a6131f11995d0f22d812855ea58
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-06-12 16:19:07 +0000

    More inlining.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@1979 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/move.h
+++ fc-solve/source/move.h
@@ -43,12 +43,26 @@ extern "C" {
 #include "state.h"
 #include "fcs_move.h"
 
+#include "inline.h"
+
 extern const fcs_move_t fc_solve_empty_move;
 
-#if 0
-int fcs_move_stack_push(fcs_move_stack_t * stack, fcs_move_t move);
-#endif
-
+#define fcs_move_stack_push(stack, move) \
+{           \
+    /* If all the moves inside the stack are taken then    \
+       resize the move vector */       \
+              \
+    if (! ((stack->num_moves+1) & (FCS_MOVE_STACK_GROW_BY-1))) \
+    {      \
+        stack->moves = realloc(     \
+            stack->moves,     \
+            (stack->num_moves+1 + FCS_MOVE_STACK_GROW_BY) * \
+                sizeof(stack->moves[0])   \
+            );     \
+    }       \
+    stack->moves[stack->num_moves++] = move;    \
+            \
+}
 #define fcs_move_stack_pop(stack,move) (fc_solve_move_stack_pop(stack,move))
 extern int fc_solve_move_stack_pop(fcs_move_stack_t * stack, fcs_move_t * move);
 
@@ -79,13 +93,140 @@ void fc_solve_apply_move(
         int decks_num
 );
 
-void fc_solve_move_stack_normalize(
+
+/*
+    The purpose of this function is to convert the moves from using
+    the canonized positions of the stacks and freecells to their
+    user positions.
+*/
+
+#define FCS_MOVE_STACK_GROW_BY 16
+/* This macro allocates an empty move stack */
+#define fcs_move_stack_alloc_into_var(final_ret) \
+{       \
+    fcs_move_stack_t * ret; \
+        \
+    /* Allocate the data structure itself */      \
+    ret = (fcs_move_stack_t *)malloc(sizeof(fcs_move_stack_t));    \
+       \
+    ret->num_moves = 0;        \
+    /* Allocate some space for the moves */     \
+    ret->moves = (fcs_move_t *)malloc(sizeof(ret->moves[0])*FCS_MOVE_STACK_GROW_BY);  \
+                \
+    (final_ret) = ret;       \
+}
+
+static GCC_INLINE void fc_solve_move_stack_normalize(
     fcs_move_stack_t * moves,
     fcs_state_extra_info_t * init_state_val,
     int freecells_num,
     int stacks_num,
     int decks_num
-    );
+    )
+{
+    fcs_move_stack_t * temp_moves;
+    fcs_move_t in_move, out_move;
+    fcs_state_keyval_pair_t dynamic_state;
+#ifdef INDIRECT_STACK_STATES
+    char buffer[MAX_NUM_STACKS << 7];
+    int a;
+#endif
+
+    out_move = fc_solve_empty_move;
+
+    fcs_move_stack_alloc_into_var(temp_moves);
+
+    fcs_duplicate_state(
+            &(dynamic_state.s), &(dynamic_state.info),
+            (init_state_val->key), init_state_val
+            );
+#ifdef INDIRECT_STACK_STATES
+    for(a=0;a<stacks_num;a++)
+    {
+        fcs_copy_stack(dynamic_state.s, dynamic_state.info, a, buffer);
+    }
+#endif
+
+    while (
+        fcs_move_stack_pop(
+            moves,
+            &in_move
+            ) == 0)
+    {
+        fc_solve_apply_move(
+            &dynamic_state.info,
+            in_move,
+            freecells_num,
+            stacks_num,
+            decks_num
+            );
+        if (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_CANONIZE)
+        {
+            /* Do Nothing */
+        }
+        else
+        {
+            fcs_move_set_type(out_move, fcs_move_get_type(in_move));
+            if ((fcs_move_get_type(in_move) == FCS_MOVE_TYPE_STACK_TO_STACK) ||
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_STACK_TO_FREECELL) ||
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_STACK_TO_FOUNDATION) ||
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_SEQ_TO_FOUNDATION)
+                )
+            {
+                fcs_move_set_src_stack(out_move,dynamic_state.info.stack_locs[(int)fcs_move_get_src_stack(in_move)]);
+            }
+
+            if (
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_FREECELL_TO_STACK) ||
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_FREECELL_TO_FREECELL) ||
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_FREECELL_TO_FOUNDATION))
+            {
+                fcs_move_set_src_freecell(out_move,dynamic_state.info.fc_locs[(int)fcs_move_get_src_freecell(in_move)]);
+            }
+
+            if (
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_STACK_TO_STACK) ||
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_FREECELL_TO_STACK)
+                )
+            {
+                fcs_move_set_dest_stack(out_move,dynamic_state.info.stack_locs[(int)fcs_move_get_dest_stack(in_move)]);
+            }
+
+            if (
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_STACK_TO_FREECELL) ||
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_FREECELL_TO_FREECELL)
+                )
+            {
+                fcs_move_set_dest_freecell(out_move,dynamic_state.info.fc_locs[(int)fcs_move_get_dest_freecell(in_move)]);
+            }
+
+            if ((fcs_move_get_type(in_move) == FCS_MOVE_TYPE_STACK_TO_FOUNDATION) ||
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_FREECELL_TO_FOUNDATION) ||
+                (fcs_move_get_type(in_move) == FCS_MOVE_TYPE_SEQ_TO_FOUNDATION)
+
+               )
+            {
+                fcs_move_set_foundation(out_move,fcs_move_get_foundation(in_move));
+            }
+
+            if ((fcs_move_get_type(in_move) == FCS_MOVE_TYPE_STACK_TO_STACK))
+            {
+                fcs_move_set_num_cards_in_seq(out_move,fcs_move_get_num_cards_in_seq(in_move));
+            }
+
+            fcs_move_stack_push(temp_moves, out_move);
+        }
+    }
+
+    /*
+     * temp_moves contain the needed moves in reverse order. So let's use
+     * swallow_stack to put them in the original in the correct order.
+     *
+     * */
+    fcs_move_stack_reset(moves);
+
+    fc_solve_move_stack_swallow_stack(moves, temp_moves);
+}
 
 extern char * fc_solve_move_to_string(fcs_move_t move, int standard_notation);
 
@@ -114,39 +255,7 @@ typedef struct
     fcs_derived_states_list_item_t * states;
 } fcs_derived_states_list_t;
 
-#define FCS_MOVE_STACK_GROW_BY 16
 
-/* This macro allocates an empty move stack */
-#define fcs_move_stack_alloc_into_var(final_ret) \
-{       \
-    fcs_move_stack_t * ret; \
-        \
-    /* Allocate the data structure itself */      \
-    ret = (fcs_move_stack_t *)malloc(sizeof(fcs_move_stack_t));    \
-       \
-    ret->num_moves = 0;        \
-    /* Allocate some space for the moves */     \
-    ret->moves = (fcs_move_t *)malloc(sizeof(ret->moves[0])*FCS_MOVE_STACK_GROW_BY);  \
-                \
-    (final_ret) = ret;       \
-}
-
-#define fcs_move_stack_push(stack, move) \
-{           \
-    /* If all the moves inside the stack are taken then    \
-       resize the move vector */       \
-              \
-    if (! ((stack->num_moves+1) & (FCS_MOVE_STACK_GROW_BY-1))) \
-    {      \
-        stack->moves = realloc(     \
-            stack->moves,     \
-            (stack->num_moves+1 + FCS_MOVE_STACK_GROW_BY) * \
-                sizeof(stack->moves[0])   \
-            );     \
-    }       \
-    stack->moves[stack->num_moves++] = move;    \
-            \
-}
 
 extern void fc_solve_derived_states_list_add_state(
         fcs_derived_states_list_t * list,
