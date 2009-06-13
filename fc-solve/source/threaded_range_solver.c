b57870fdd09eb40823ac1e1ea91e2d73b366801b
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-06-13 12:14:07 +0000

    IDs cleanup - moved a fcs_state_validity_check() to the rejects.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@1992 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/threaded_range_solver.c
+++ fc-solve/source/threaded_range_solver.c
@@ -102,41 +102,6 @@ static void my_iter_handler(
         free((void*)state_string);
     }
 
-#ifdef MYDEBUG
-    {
-        fcs_card_t card;
-        int ret;
-        char card_str[10];
-
-        ret = fcs_check_state_validity(
-            ptr_state_with_locations,
-            context->freecells_num,
-            context->stacks_num,
-            context->decks_num,
-            &card
-            );
-
-        if (ret != 0)
-        {
-
-            fcs_card_perl2user(card, card_str, context->display_10_as_t);
-            if (ret == 3)
-            {
-                fprintf(stdout, "%s\n",
-                    "There's an empty slot in one of the stacks."
-                    );
-            }
-            else
-            {
-                fprintf(stdout, "%s%s.\n",
-                    ((ret == 2)? "There's an extra card: " : "There's a missing card: "),
-                    card_str
-                );
-            }
-            exit(-1);
-        }
-    }
-#endif
 }
 
 struct pack_item_struct
