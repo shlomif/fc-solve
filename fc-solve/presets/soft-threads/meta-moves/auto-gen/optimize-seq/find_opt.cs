ac74dbf9d2605c6806e07905df41e50cf69cdcc6
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-12-24 22:39:09 +0000

    Added the input of $data_dir/scans.txt (with a test).
    Nothing too much here.
    
    
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@2361 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/presets/soft-threads/meta-moves/auto-gen/optimize-seq/find_opt.cs
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/optimize-seq/find_opt.cs
@@ -2,6 +2,24 @@ using System.Collections.Generic;
 using System;
 using System.IO;
 
+class InputScan
+{
+	public int id;
+	public string cmd_line;
+	
+	public InputScan(int new_id, string new_cmd_line)
+	{
+		id = new_id;
+		cmd_line = new_cmd_line;
+	}
+	
+	static public InputScan from_line(string line)
+	{
+		string [] e = line.Split('\t');
+		return new InputScan(Convert.ToInt32(e[0]), e[1]);
+	}
+}
+
 class Input
 {
 	public int start_board;
@@ -10,12 +28,14 @@ class Input
 	public const string data_dir = "..";
 	
 	public List<int> blacklist;
+	public List<InputScan> scans;
 	
 	public Input(int new_start_board, int new_num_boards)
 	{
 		start_board = new_start_board;
 		num_boards = new_num_boards;
 	}
+	
 	
 	public void read_scan_black_list()
 	{
@@ -28,6 +48,25 @@ class Input
         {
       		blacklist.Add(Convert.ToInt32(line));
         }
+	}
+	
+	public void read_scans_file()
+	{
+		scans = new List<InputScan>();
+		read_scan_black_list();
+		StreamReader f = new StreamReader(data_dir + "/scans.txt");
+		
+		string line;
+
+        while ((line = f.ReadLine()) != null) 
+        {
+			InputScan scan = InputScan.from_line(line);
+			
+			if (! blacklist.Exists(i => i == scan.id))
+			{
+				scans.Add(scan);
+			}
+        }		
 	}
 }
 
@@ -48,6 +87,16 @@ class Program
 			{
 				Console.WriteLine(blacked);
 			}
+		}
+		else if (cmd == "test_lookup_scan_cmd_line_by_id")
+		{
+			Input input = new Input(start_board, num_boards);
+			input.read_scans_file();
+			
+			int id = Convert.ToInt32(args[1]);
+			
+			InputScan scan = input.scans.Find(e => e.id == id);
+			Console.WriteLine(scan.cmd_line);
 		}
         // List<double> myList = new List<double>();
 		else
--- fc-solve/presets/soft-threads/meta-moves/auto-gen/optimize-seq/t/various.t
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/optimize-seq/t/various.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 3;
+use Test::More tests => 6;
 use Test::Trap qw( trap $trap :flow:stderr(systemsafe):stdout(systemsafe):warn );
 
 {
@@ -19,4 +19,22 @@ use Test::Trap qw( trap $trap :flow:stderr(systemsafe):stdout(systemsafe):warn )
 
     # TEST
     is ($trap->stdout(), "7\n8\n", "Black list is OK.");
+}
+
+{
+    trap {    
+        # TEST
+        ok (!system("mono find_opt.exe test_lookup_scan_cmd_line_by_id 3"));
+    };
+
+    # TEST
+    $trap->stderr_is("", 
+        "test_lookup_scan_cmd_line did not return any errors."
+    );
+
+    # TEST
+    $trap->stdout_is(
+        "--method random-dfs -seed 2 -to 0[01][23456789]\n", 
+        "test_lookup_scan_cmd_line(3) is OK.",
+    );
 }
