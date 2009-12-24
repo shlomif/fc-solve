38bec5203625b29feaeafe8c700e22ad25b8c816
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-12-24 21:08:07 +0000

    Added the beginning of the program in C# to do the find_optimal_sequence.
    It doesn't do anything at all at the moment except print Hello World.
    
    
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@2358 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/optimize-seq/find_opt.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // List<double> myList = new List<double>();
+        Console.WriteLine("Hello World!");
+    }
+}
