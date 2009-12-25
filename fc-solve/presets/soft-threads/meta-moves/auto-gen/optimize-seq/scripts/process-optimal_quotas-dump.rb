d77c3499272031826e7eef8ae312a1c77590ed9a
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2009-12-25 18:06:10 +0000

    Generalised optimize-seq/scripts/process-optimal_quotas-dump.rb .
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@2397 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/presets/soft-threads/meta-moves/auto-gen/optimize-seq/scripts/process-optimal_quotas-dump.rb
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/optimize-seq/scripts/process-optimal_quotas-dump.rb
@@ -1,5 +1,5 @@
 next_idx = 0
-File.open("find_optimal_quotas-300.dump", "r").each do |l| 
+File.open(ARGV[0], "r").each do |l| 
     if mo = /^Found (\d+) for No. (\d+) \((\d+)\)$/.match(l) then
         quota, idx, total_iters = mo[1], mo[2], mo[3]
         if (next_idx != idx.to_i) then
