1d0cce3c3da7fc0169910836ba29879fb6ca3f0b
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2010-01-15 20:17:16 +0000

    Add the run-against-simulation scripts.
    These make sure that the optimised meta-scan yields the correct results
    in all cases.
    
    
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@2489 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/source/scripts/run-against-simulation-from-11982.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+presets_dir = "/home/shlomi/progs/freecell/trunk/fc-solve/presets/soft-threads/meta-moves/auto-gen"
+
+start_board = 11983
+fcs_output = IO.popen("./freecell-solver-range-parallel-solve #{start_board} 32000 1 --read-from-file 4,#{presets_dir}/nameless-preset.bash --scans-synergy none")
+
+expected_iters = File.open("#{presets_dir}/nameless-simulation.txt", "r")
+
+junk_line = fcs_output.readline()
+total_iters = 0
+
+prev_to_start_board = start_board - 1
+while ((wait_line = expected_iters.readline()) && \
+       (wait_line !~ /^#{prev_to_start_board}:/))
+    # Do nothing - we're waiting to the end.
+end
+
+while (out_line = fcs_output.readline()) do
+    out_line.chomp!
+    exp_line = expected_iters.readline()
+    exp_line.chomp!
+    # puts "{{#{out_line}}}->{{#{exp_line}}}"
+    
+    if exp_line =~ /:(\d+)\z/
+        total_iters += $1.to_i
+    else
+        raise "Incorrect expected format at line '#{exp_line}'"
+    end
+
+    if (out_line =~ /\(total_num_iters=(\d+)\)/)
+        if $1.to_i != total_iters
+            raise "Wrong iteration count at <<#{exp_line}>> and <<#{out_line}>>"
+        end
+    else
+        raise "Incorrect output format at line '#{out_line}'"
+    end      
+end
