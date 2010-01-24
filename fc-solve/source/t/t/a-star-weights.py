6b8e43a9475e7e7aeca447751893955b2ce64b0c
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2010-01-24 16:34:34 +0000

    Add the test for setting the "-asw" parameter.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@2503 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/t/t/a-star-weights.py
+++ fc-solve/source/t/t/a-star-weights.py
@@ -4,8 +4,41 @@ import sys
 sys.path.append("./t/lib");
 
 from TAP.Simple import *
+from ctypes import *
 
-plan(1)
+plan(2)
 
-# TEST
-ok(1, "Everything is OK.")
+def main():
+    fcs = CDLL("../libfreecell-solver.so")
+    libc = CDLL("libc.so.6")
+
+    # TEST
+    ok(1, "Dummy assertion")
+
+    user = fcs.freecell_solver_user_alloc()
+
+    last_arg = c_int()
+    error_string = c_char_p()
+    known_params = c_char_p(None)
+    fcs.freecell_solver_user_cmd_line_parse_args(
+        user, # instance
+        2,    # argc
+        ((c_char_p * 2)("-asw", "5,4,3,2,1")),  # argv
+        0,   # start_arg
+        byref(known_params), # known_params
+        None,   # callback
+        None,   # callback_context
+        byref(error_string),  # error_string
+        byref(last_arg)    # last_arg
+    )
+    
+    # TEST
+    is_ok(last_arg.value, 2, "Processed two arguments");  
+
+    fcs.freecell_solver_user_free(user);
+
+#----------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
+
