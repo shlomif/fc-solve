5d0a5acaca21d3135c0a44a31a59a58fdc4cf077
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2010-05-03 18:44:14 +0000

    Extract the FC_Solve Python module to be used by other tests.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@2784 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/source/t/t/lib/FC_Solve/__init__.py
@@ -0,0 +1,61 @@
+from TAP.Simple import *
+from ctypes import *
+
+class FC_Solve:
+    # TEST:$num_befs_weights=5;
+    NUM_BEFS_WEIGHTS = 5
+
+    def __init__(self):
+        self.fcs = CDLL("../libfreecell-solver.so")
+
+        self.user = self.fcs.freecell_solver_user_alloc()
+
+        self.get_befs_weight = self.fcs.fc_solve_user_INTERNAL_get_befs_weight
+
+        self.get_befs_weight.restype = c_double
+
+    # TEST:$set_befs=0;
+    def _set_befs_weights(self, name, weights_s):
+
+        last_arg = c_int()
+        error_string = c_char_p()
+        known_params = c_char_p(None)
+        self.fcs.freecell_solver_user_cmd_line_parse_args(
+            self.user, # instance
+            2,    # argc
+            ((c_char_p * 2)("-asw", weights_s)),  # argv
+            0,   # start_arg
+            byref(known_params), # known_params
+            None,   # callback
+            None,   # callback_context
+            byref(error_string),  # error_string
+            byref(last_arg)    # last_arg
+        )
+        
+        # TEST:$set_befs++;
+        is_ok(last_arg.value, 2,
+                name + " - assign weights - processed two arguments");
+
+    def __destroy__(self):
+        self.fcs.freecell_solver_user_free(self.user);
+
+    # TEST:$test_befs=0;
+    def test_befs_weights(self, name, string, weights):
+
+        # TEST:$test_befs=$test_befs+$set_befs;
+        self._set_befs_weights(name, string)
+
+        for idx in range(0, self.NUM_BEFS_WEIGHTS):
+            top = bottom = weights[idx]
+            # floating-point values.
+            if (top != int(top) + 0.0):
+                top = top + 1e-6
+                bottom = bottom - 1e-6
+                
+            have = self.get_befs_weight(self.user, idx)
+            # TEST:$test_befs=$test_befs+$num_befs_weights;
+            if (not ok((bottom <= have) and (have <= top), \
+                    name + " - Testing Weight No. " + str(idx))):
+                diag("Should be: [" + str(bottom) + "," + str(top) + "] ; " +
+                        "Is: " + str(have))
+
--- fc-solve/source/t/t/set-befs-weights.py
+++ fc-solve/source/t/t/set-befs-weights.py
@@ -4,67 +4,11 @@ import sys
 sys.path.append("./t/lib");
 
 from TAP.Simple import *
-from ctypes import *
+# TEST:source "$^CURRENT_DIRNAME/lib/FC_Solve/__init__.py"
+from FC_Solve import FC_Solve
 
+# Should be 24.
 plan(24)
-
-class FC_Solve:
-    # TEST:$num_befs_weights=5;
-    NUM_BEFS_WEIGHTS = 5
-
-    def __init__(self):
-        self.fcs = CDLL("../libfreecell-solver.so")
-
-        self.user = self.fcs.freecell_solver_user_alloc()
-
-        self.get_befs_weight = self.fcs.fc_solve_user_INTERNAL_get_befs_weight
-
-        self.get_befs_weight.restype = c_double
-
-    # TEST:$set_befs=0;
-    def _set_befs_weights(self, name, weights_s):
-
-        last_arg = c_int()
-        error_string = c_char_p()
-        known_params = c_char_p(None)
-        self.fcs.freecell_solver_user_cmd_line_parse_args(
-            self.user, # instance
-            2,    # argc
-            ((c_char_p * 2)("-asw", weights_s)),  # argv
-            0,   # start_arg
-            byref(known_params), # known_params
-            None,   # callback
-            None,   # callback_context
-            byref(error_string),  # error_string
-            byref(last_arg)    # last_arg
-        )
-        
-        # TEST:$set_befs++;
-        is_ok(last_arg.value, 2,
-                name + " - assign weights - processed two arguments");
-
-    def __destroy__(self):
-        self.fcs.freecell_solver_user_free(self.user);
-
-    # TEST:$test_befs=0;
-    def test_befs_weights(self, name, string, weights):
-
-        # TEST:$test_befs=$test_befs+$set_befs;
-        self._set_befs_weights(name, string)
-
-        for idx in range(0, self.NUM_BEFS_WEIGHTS):
-            top = bottom = weights[idx]
-            # floating-point values.
-            if (top != int(top) + 0.0):
-                top = top + 1e-6
-                bottom = bottom - 1e-6
-                
-            have = self.get_befs_weight(self.user, idx)
-            # TEST:$test_befs=$test_befs+$num_befs_weights;
-            if (not ok((bottom <= have) and (have <= top), \
-                    name + " - Testing Weight No. " + str(idx))):
-                diag("Should be: [" + str(bottom) + "," + str(top) + "] ; " +
-                        "Is: " + str(have))
 
 def main():
     fcs = FC_Solve()
@@ -88,7 +32,6 @@ def main():
     fcs.test_befs_weights("One number with a trailing comma", 
             "100,\x002,3,4,5,6,7,8,9,100\x00",
             [100,0,0,0,0])
-
 
 #----------------------------------------------------------------------
 
