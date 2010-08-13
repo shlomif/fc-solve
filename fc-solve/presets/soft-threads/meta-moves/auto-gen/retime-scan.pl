58e13b63baa295c09082dce19621340ad9a65e2b
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2010-08-13 12:36:27 +0000

    Add the retime-scan.pl script to remeasure a scan.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/trunk@3083 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/retime-scan.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use MyInput;
+
+my $opt = (($ARGV[0] eq "--gen-bat") ? shift : "");
+
+my $id = shift(@ARGV);
+
+my $prev_scans = MyInput::get_prev_scans();
+
+my @results;
+if ((@results = (grep { $_->{'id'} eq $id } @$prev_scans)) != 1)
+{
+    die "Cannot find scans or too many of them.";
+}
+
+my %params =
+(
+    id => $id,
+    argv => [split /\s+/, $results[0]->{'cmd_line'}]
+);
+
+if (exists($ENV{FC_NUM}))
+{
+    $params{freecells_num} = $ENV{FC_NUM};
+}
+
+if ($opt eq "--gen-bat")
+{
+    open O, ">run_scan_$id.bat";
+    print O join(" ", @{MyInput::get_scan_cmd_line(\%params)}), "\n\r";
+    close(O);
+}
+else
+{
+    MyInput::time_scan(\%params);
+}
+
+# system("$ENV{'HOME'}/apps/alert/finish-cue");
