819b4b2e3a31279fd1b40547b658e3c95bfc9c70
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-02-01 20:33:20 +0000

    Update the URLs in the tag-fc-solve-release script.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3687 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/scripts/tag-fc-solve-release.bash
+++ fc-solve/source/scripts/tag-fc-solve-release.bash
@@ -2,5 +2,5 @@
 set -x
 VER="$(cat ver.txt)"
 svn copy -m "Tagging the Freecell Solver $VER release" \
-    https://svn.berlios.de/svnroot/repos/fc-solve/trunk \
-    https://svn.berlios.de/svnroot/repos/fc-solve/tags/releases/"$VER"
+    https://svn.berlios.de/svnroot/repos/fc-solve/fc-solve/trunk \
+    https://svn.berlios.de/svnroot/repos/fc-solve/fc-solve/tags/releases/"$VER"
