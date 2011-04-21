6769bd1fb42fff79786bb3c9ac9ab4a006d5cb2d
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-04-21 15:03:53 +0000

    Correct some bugs.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3767 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/scripts/run-bakers-game-sequence.bash
+++ fc-solve/source/scripts/run-bakers-game-sequence.bash
@@ -4,19 +4,20 @@ GAME="bakers_game"
 
 RESULTS_DIR="$GAME-results/"
 
-start="$1"
+START="$1"
 shift
-end="$1"
+END="$1"
 shift
 
 MAX_ITERS='10,000,000'
 
 mkdir -p "$RESULTS_DIR"
 
-seq "$start" "$end" |  \
+seq "$START" "$END" |
 (while read DEAL ; do
     echo "${DEAL}:"
     ./board_gen/make_pysol_freecell_board.py "$DEAL" "$GAME" |
-        ./fc-solve --game "$GAME" -l eo -mi "$MAX_ITERS" > "$RESULTS_DIR/$DEAL.sol"
+        ./fc-solve --game "$GAME" -l eo -mi "${MAX_ITERS//,/}" -p -t -sam > \
+        "$(printf "%s/%09d.sol" "$RESULTS_DIR" "$DEAL")"
 done) | tee -a total_dump.txt
 
