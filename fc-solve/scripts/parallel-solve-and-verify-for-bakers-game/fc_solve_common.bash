6b2ce0ef237046f423ec0d512f1a43f2f6106a1b
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-06-02 20:13:50 +0000

    Reformatted and made better.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3803 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/fc_solve_common.bash
+++ fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/fc_solve_common.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-export FCS_PATH="$HOME/fcs"
+export FCS_PATH="$HOME/fc-solve-bakers-game-range"
 export FCS_MAX_DEAL_IDX="10000" FCS_PRESET_CONFIG="-g bakers_game" 
-export FCS_MIN_DEAL_IDX="1" FCS_NUM_SOLVERS="1" FCS_NUM_VERIFIERS="1"
-export FCS_DEALS_PER_BATCH="100"
+export FCS_MIN_DEAL_IDX="1" 
+export FCS_DEALS_PER_BATCH="1000"
+export FCS_NUM_SOLVERS="1" FCS_NUM_VERIFIERS="3"
