1756253ee2a3c49f604158621cb420ac5a01b36f
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-06-03 13:23:30 +0000

    Add an FCS_SOURCE_URL parameter.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3806 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/Setup.bash
+++ fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/Setup.bash
@@ -6,11 +6,11 @@ script_dirname="$(dirname $BASH_SOURCE[-1])"
 source "$script_dirname/fc_solve_common.bash"
 
 if ! test -e "$FCS_PATH" ; then
-    svn export http://svn.berlios.de/svnroot/repos/fc-solve/fc-solve/trunk/fc-solve/source "$FCS_PATH" 
-    cd "$FCS_PATH"
-    ./Tatzer -l x64bb --nobuild-docs --prefix="$HOME/fcs-bin"
-    make
-    perl scripts/parallel-range-solver-total --mode setup
+    svn export "$FCS_SOURCE_URL" "$FCS_PATH" && \
+    cd "$FCS_PATH" && \
+    ./Tatzer -l x64bb --nobuild-docs --prefix="$HOME/fcs-bin" && \
+    make && \
+    perl scripts/parallel-range-solver-total --mode setup &&
     rm -f range-queue.sock
 
 fi
--- fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/fc_solve_common.bash
+++ fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/fc_solve_common.bash
@@ -4,3 +4,7 @@ export FCS_MAX_DEAL_IDX="10000" FCS_PRESET_CONFIG="-g bakers_game"
 export FCS_MIN_DEAL_IDX="1" 
 export FCS_DEALS_PER_BATCH="1000"
 export FCS_NUM_SOLVERS="1" FCS_NUM_VERIFIERS="3"
+
+if -z "$FCS_SOURCE_URL" then
+    export FCS_SOURCE_URL="http://svn.berlios.de/svnroot/repos/fc-solve/fc-solve/trunk/fc-solve/source"
+fi
