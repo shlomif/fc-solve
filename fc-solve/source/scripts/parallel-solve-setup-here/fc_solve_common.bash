e5a16ad240c4498cf67c9629e1d6a8914a2b789b
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-06-10 21:32:13 +0000

    For demonstration purposes, a range of one deal is OK.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3866 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/scripts/parallel-solve-setup-here/fc_solve_common.bash
+++ fc-solve/source/scripts/parallel-solve-setup-here/fc_solve_common.bash
@@ -2,7 +2,7 @@
 export FCS_PATH="$HOME/fc-solve-bakers-game-range"
 export FCS_MAX_DEAL_IDX="4000" FCS_PRESET_CONFIG="-g bakers_game" 
 export FCS_MIN_DEAL_IDX="1" 
-export FCS_DEALS_PER_BATCH="50"
+export FCS_DEALS_PER_BATCH="1"
 export FCS_NUM_SOLVERS="1" FCS_NUM_VERIFIERS="3"
 
 if test -z "$FCS_SOURCE_URL" ; then
