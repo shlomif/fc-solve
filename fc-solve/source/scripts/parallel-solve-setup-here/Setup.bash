9f8f5b5d449540d5cf7ceaafe70e8ea1abaaae81
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-06-10 09:18:56 +0000

    Now using rsync instead of svn export so we won't have to commit
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3825 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/scripts/parallel-solve-setup-here/Setup.bash
+++ fc-solve/source/scripts/parallel-solve-setup-here/Setup.bash
@@ -5,13 +5,14 @@ set -x
 script_dirname="$(dirname $BASH_SOURCE[-1])"
 source "$script_dirname/fc_solve_common.bash"
 
-if ! test -e "$FCS_PATH" ; then
-    svn export "$FCS_SOURCE_URL" "$FCS_PATH" && \
-    cd "$FCS_PATH" && \
-    ./Tatzer -l x64bb --nobuild-docs --prefix="$HOME/fcs-bin" && \
-    make && \
-    perl scripts/parallel-range-solver-total --mode setup &&
-    rm -f range-queue.sock
+rsync -a -v --progress "$script_dirname/../.." "$FCS_PATH" 
 
-fi
+# if ! test -e "$FCS_PATH" ; then
+#     svn export "$FCS_SOURCE_URL" "$FCS_PATH" && \
+# fi
 
+cd "$FCS_PATH" && \
+./Tatzer -l x64bb --nobuild-docs --prefix="$HOME/fcs-bin" && \
+make && \
+perl scripts/parallel-range-solver-total --mode setup &&
+rm -f range-queue.sock
