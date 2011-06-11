12ee3175f14cd70b72a8da75c4badc7166510ae1
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-06-11 14:44:10 +0000

    Add more
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3911 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/fc_solve_common.bash
+++ fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/fc_solve_common.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 export FCS_PATH="$HOME/fc-solve-bakers-game-range"
-export FCS_MAX_DEAL_IDX="10000" FCS_PRESET_CONFIG="-g bakers_game" 
+export FCS_MAX_DEAL_IDX="10000" 
+export FCS_PRESET_CONFIG="-ni -to 01ABCDEF -g bakers_game"
 export FCS_MIN_DEAL_IDX="1" 
 export FCS_DEALS_PER_BATCH="1000"
-export FCS_NUM_SOLVERS="1" FCS_NUM_VERIFIERS="3"
 
 if test -z "$FCS_SOURCE_URL" ; then
     export FCS_SOURCE_URL="http://svn.berlios.de/svnroot/repos/fc-solve/fc-solve/trunk/fc-solve/source"
