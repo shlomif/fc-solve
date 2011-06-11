6a2e2e4bd8082601643d9b4dffec598bdadd7b1c
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-06-11 17:09:19 +0000

    Add a necessary --halt argument.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3918 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/halt.bash
+++ fc-solve/scripts/parallel-solve-and-verify-for-bakers-game/halt.bash
@@ -3,4 +3,12 @@
 script_dirname="$(dirname $BASH_SOURCE[-1])"
 source "$script_dirname/fc_solve_common.bash"
 
+arg="$1"
+shift
+
+if test "$arg" != "--halt" ; then
+    echo "First argument to halt.bash must be '--halt'" 1>&2
+    exit -1
+fi
+
 cd "$FCS_PATH" && perl scripts/parallel-range-solver-total --mode halt
