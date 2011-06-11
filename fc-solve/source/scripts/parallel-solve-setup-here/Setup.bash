6836845c253eb51e61d4f5333322d5151286ba43
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-06-11 12:53:30 +0000

    Made sure it won't be rebuilt unnecessarily.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3905 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/scripts/parallel-solve-setup-here/Setup.bash
+++ fc-solve/source/scripts/parallel-solve-setup-here/Setup.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+rebuild()
+{
+    ./Tatzer -l x64bb --nobuild-docs --prefix="$HOME/fcs-bin" && \
+        make
+}
+
+# Comment out to rebuild everything.
+rebuild() { true ; }
+
 set -x
 
 script_dirname="$(dirname $BASH_SOURCE[-1])"
@@ -11,9 +20,10 @@ rsync -a -v --progress --exclude='**/.svn/**' "$script_dirname/../.." "$FCS_PATH
 #     svn export "$FCS_SOURCE_URL" "$FCS_PATH" && \
 # fi
 
+# true && # ./Tatzer -l x64bb --nobuild-docs --prefix="$HOME/fcs-bin" && \
+
 cd "$FCS_PATH" && \
-true && # ./Tatzer -l x64bb --nobuild-docs --prefix="$HOME/fcs-bin" && \
-make && \
+    rebuild && \
 rm -f FAULT.txt sync.lock sync.txt summary.txt summary.lock \
     global-params.txt summary.stats.perl-storable &&
 perl scripts/parallel-range-solver-total --mode setup &&
