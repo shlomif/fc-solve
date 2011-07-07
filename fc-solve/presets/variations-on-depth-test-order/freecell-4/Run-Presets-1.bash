a29d046500b21ae30058457c33e160e5a66e7933
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-07-07 16:22:14 +0000

    Add the variations on depth-test-order presets directory.
    There we try to construct variations based on --depth-test-order.
    
    
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3963 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/presets/variations-on-depth-test-order/freecell-4/Run-Presets-1.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+max_board="500"
+max_iters="10000"
+
+function _preset()
+{
+    local name="$1"
+    shift
+
+    d="data/$name++--$max_board--$max_iters.dump"
+    echo "Preset $name == $@"
+    if ! test -e "$d" ; then
+        freecell-solver-range-parallel-solve 1 "$max_board" 1 "$@" --max-iters "$max_iters" \
+        | tee "$d"
+    fi
+    echo "   Unsolved = $(grep -cP '^Unsolved' "$d") ; Iters = $(tail -1 "$d" | perl -ne 'print $1 if m{total_num_iters=(\d+)}')"
+}
+
+_preset "default"
+
+_preset "10step" \
+    -to 01234567 \
+    -dto 10,0187465 \
+    -dto 20,0134587
+
+_preset "5step" \
+    -to 01234567 \
+    -dto 5,0187465 \
+    -dto 10,0134587
+
+_preset "15step" \
+    -to 01234567 \
+    -dto 15,0187465 \
+    -dto 30,0134587 \
+
+_preset "scan23" --method soft-dfs -to 0123456789 -dto 19,0126394875
+
