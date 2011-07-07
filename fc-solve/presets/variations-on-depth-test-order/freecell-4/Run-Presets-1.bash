edf80fd3769b9c8ad3c6bab89b802030be9cfba0
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-07-07 16:31:47 +0000

    Add 13step and 14step.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@3969 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/presets/variations-on-depth-test-order/freecell-4/Run-Presets-1.bash
+++ fc-solve/presets/variations-on-depth-test-order/freecell-4/Run-Presets-1.bash
@@ -39,6 +39,16 @@ _preset "12step" \
     -dto 12,0187465 \
     -dto 24,0134587
 
+_preset "13step" \
+    -to 01234567 \
+    -dto 13,0187465 \
+    -dto 26,0134587
+
+_preset "14step" \
+    -to 01234567 \
+    -dto 14,0187465 \
+    -dto 28,0134587
+
 _preset "scan23" --method soft-dfs -to 0123456789 -dto 19,0126394875
 
 _preset "01234567" -to 01234567
