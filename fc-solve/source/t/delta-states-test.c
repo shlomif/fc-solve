5ec941b8d27e910e584911c117464cb3aca71669
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-11-04 20:15:25 +0000

    Add a test.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@4134 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/t/delta-states-test.c
+++ fc-solve/source/t/delta-states-test.c
@@ -351,12 +351,140 @@ int main_tests()
 
         fc_solve_delta_stater_free (delta);
     }
+
+/* More encode_composite tests - this time from the output of:
+ * pi-make-microsoft-freecell-board -t 24 | \
+ *      ./fc-solve -to 01ABCDE --freecells-num 2 -s -i -p -t
+ */
+    {
+        fc_solve_delta_stater_t * delta;
+        fcs_state_keyval_pair_t init_state, derived_state;
+
+#ifdef INDIRECT_STACK_STATES
+        ind_buf_t indirect_stacks_buffer, derived_indirect_stacks_buffer;
+#endif
+
+        /* MS Freecell No. 24 Initial state.
+         * */
+        fc_solve_initial_user_state_to_c(
+("Foundations: H-0 C-0 D-0 S-0 \n"
+"Freecells:        \n"
+"4C 2C 9C 8C QS 4S 2H\n"
+"5H QH 3C AC 3H 4H QD\n"
+"QC 9S 6H 9H 3S KS 3D\n"
+"5D 2S JC 5C JH 6D AS\n"
+"2D KD TH TC TD 8D\n"
+"7H JS KH TS KC 7C\n"
+"AH 5S 6S AD 8H JD\n"
+"7S 6C 7D 4D 8S 9D\n"),
+                &init_state,
+                FREECELLS_NUM,
+                STACKS_NUM,
+                DECKS_NUM
+#ifdef INDIRECT_STACK_STATES
+                , indirect_stacks_buffer
+#endif
+        );
+
+        delta = fc_solve_delta_stater_alloc(
+                &init_state.s,
+                STACKS_NUM,
+                FREECELLS_NUM
+#ifndef FCS_FREECELL_ONLY
+                , FCS_SEQ_BUILT_BY_ALTERNATE_COLOR
+#endif
+                );
+
+        fc_solve_initial_user_state_to_c(
+                (
+"Foundations: H-0 C-0 D-0 S-4 \n"
+"Freecells:  KS  TD\n"
+"2C\n"
+"5H QH 3C AC 3H 4H QD JC TH 9C 8D 7S\n"
+"QC 9S 6H 9H 8C 7D 6C 5D 4C 3D\n"
+"\n"
+"2D KD QS JH TC 9D 8S\n"
+"7H JS KH TS KC 7C 6D 5C 4D\n"
+"AH 5S 6S AD 8H JD\n"
+"2H\n"
+                ) ,
+                &derived_state,
+                FREECELLS_NUM,
+                STACKS_NUM,
+                DECKS_NUM
+#ifdef INDIRECT_STACK_STATES
+                , derived_indirect_stacks_buffer
+#endif
+        );
+
+        fc_solve_delta_stater_set_derived(delta, &(derived_state.s));
+
+        {
+            fcs_state_keyval_pair_t new_derived_state;
+#ifdef INDIRECT_STACK_STATES
+            ind_buf_t new_derived_indirect_stacks_buffer;
+#endif
+            fcs_uchar_t enc_state[24];
+            fc_solve_bit_writer_t bit_w;
+            fc_solve_bit_reader_t bit_r;
+            char * as_str;
+            const char * expected_str = 
+            (
+"Foundations: H-0 C-0 D-0 S-4 \n"
+"Freecells:  TD  KS\n"
+": \n"
+": 5H QH 3C AC 3H 4H QD JC TH 9C 8D 7S\n"
+": QC 9S 6H 9H 8C 7D 6C 5D 4C 3D\n"
+": 2H\n"
+": 2D KD QS JH TC 9D 8S\n"
+": 7H JS KH TS KC 7C 6D 5C 4D\n"
+": AH 5S 6S AD 8H JD\n"
+": 2C\n"
+            );
+
+            fc_solve_state_init(&new_derived_state, STACKS_NUM
+#ifdef INDIRECT_STACK_STATES
+                , new_derived_indirect_stacks_buffer
+#endif
+                );
+
+            fc_solve_bit_writer_init(&bit_w, enc_state);
+            fc_solve_delta_stater_encode_composite(delta, &bit_w);
+            
+            fc_solve_bit_reader_init(&bit_r, enc_state);
+            fc_solve_delta_stater_decode(delta, &bit_r, &(new_derived_state.s));
+
+            as_str =
+                fc_solve_state_as_string(
+                    &new_derived_state,
+                    FREECELLS_NUM,
+                    STACKS_NUM,
+                    DECKS_NUM,
+                    1,
+                    0,
+                    1
+                    );
+
+            /* TEST
+             * */
+            if (!ok(!strcmp(as_str, expected_str), "encode_composite + decode test No. 2 (deal #24)"))
+            {
+                diag("got == <<<\n%s\n>>> ; expected == <<<\n%s\n>>>\n",
+                        as_str,
+                        expected_str
+                    );
+            }
+
+            free(as_str);
+        }
+    }
+
     return 0;
 }
 
 int main(int argc, char * argv[])
 {
-    plan_tests(20);
+    plan_tests(21);
     main_tests();
     return exit_status();
 }
