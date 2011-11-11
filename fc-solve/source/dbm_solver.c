57390a27d975d27d9eded37857f0512975ede579
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-11-11 17:03:55 +0000

    Add more to dbm_solver.c.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@4172 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/dbm_solver.c
+++ fc-solve/source/dbm_solver.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include "config.h"
+
+#include "kaz_tree.h"
 
 typedef unsigned char fcs_encoded_state_buffer_t[24];
 
@@ -13,6 +16,23 @@ struct fcs_cache_key_info_struct
 };
 
 typedef struct fcs_cache_key_info_struct fcs_cache_key_info_t;
+
+typedef struct
+{
+#if (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_JUDY)
+    Pvoid_t states_values_to_keys_map;
+#elif (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_KAZ_TREE)
+    dict_t * kaz_tree;
+#else
+#error unknown FCS_RCS_CACHE_STORAGE
+#endif
+    fcs_compact_allocator_t states_values_to_keys_allocator;
+    long count_elements_in_cache, max_num_elements_in_cache;
+
+    fcs_cache_key_info_t * lowest_pri, * highest_pri;
+
+    fcs_cache_key_info_t * recycle_bin;
+} fcs_lru_cache_t;
 
 /* Temporary main() function to make gcc happy. */
 int main(int argc, char * argv[])
