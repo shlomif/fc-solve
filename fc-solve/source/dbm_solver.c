5f26fd80c3759ae2b5fce15758c3a4529bda7edb
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-11-11 17:23:06 +0000

    Add more.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@4173 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/dbm_solver.c
+++ fc-solve/source/dbm_solver.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "config.h"
 
+#include "inline.h"
+
 #include "kaz_tree.h"
 
 typedef unsigned char fcs_encoded_state_buffer_t[24];
@@ -33,6 +35,36 @@ typedef struct
 
     fcs_cache_key_info_t * recycle_bin;
 } fcs_lru_cache_t;
+
+/* TODO: make sure the key is '\0'-padded. */
+static int fc_solve_compare_lru_cache_keys(
+    const void * void_a, const void * void_b, void * context
+)
+{
+#define GET_PARAM(p) ((((const fcs_cache_key_info_t *)(p))->key))
+    return memcmp(GET_PARAM(void_a), GET_PARAM(void_b), sizeof(GET_PARAM(void_a)));
+#undef GET_PARAM
+}
+
+static void GCC_INLINE cache_init(fcs_lru_cache_t * cache, long max_num_elements_in_cache)
+{
+#if (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_JUDY)
+    cache->states_values_to_keys_map = ((Pvoid_t) NULL);
+#elif (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_KAZ_TREE)
+    cache->kaz_tree = fc_solve_kaz_tree_create(fc_solve_compare_lru_cache_keys, NULL);
+#else
+#error Unknown FCS_RCS_CACHE_STORAGE
+#endif
+
+    fc_solve_compact_allocator_init(
+        &(cache->states_values_to_keys_allocator)
+    );
+    cache->lowest_pri = NULL;
+    cache->highest_pri = NULL;
+    cache->recycle_bin = NULL;
+    cache->count_elements_in_cache = 0;
+    cache->max_num_elements_in_cache = max_num_elements_in_cache;
+}
 
 /* Temporary main() function to make gcc happy. */
 int main(int argc, char * argv[])
