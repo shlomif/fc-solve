f6f555759afb5c634519c05062b6baaef8b0f662
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-11-12 11:34:45 +0000

    Moved the type definitions to a header.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@4182 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/dbm_solver.c
+++ fc-solve/source/dbm_solver.c
@@ -1,41 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "config.h"
 
-#include "inline.h"
 #include "bool.h"
+#include "inline.h"
 
-#include "kaz_tree.h"
-
-typedef unsigned char fcs_encoded_state_buffer_t[24];
-
-struct fcs_cache_key_info_struct
-{
-    fcs_encoded_state_buffer_t key;
-    /* lower_pri and higher_pri form a doubly linked list.
-     *
-     * pri == priority.
-     * */
-    struct fcs_cache_key_info_struct * lower_pri, * higher_pri;
-};
-
-typedef struct fcs_cache_key_info_struct fcs_cache_key_info_t;
-
-typedef struct
-{
-#if (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_JUDY)
-    Pvoid_t states_values_to_keys_map;
-#elif (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_KAZ_TREE)
-    dict_t * kaz_tree;
-#else
-#error unknown FCS_RCS_CACHE_STORAGE
-#endif
-    fcs_compact_allocator_t states_values_to_keys_allocator;
-    long count_elements_in_cache, max_num_elements_in_cache;
-
-    fcs_cache_key_info_t * lowest_pri, * highest_pri;
-
-    fcs_cache_key_info_t * recycle_bin;
-} fcs_lru_cache_t;
+#include "dbm_solver.h"
 
 /* TODO: make sure the key is '\0'-padded. */
 static int fc_solve_compare_lru_cache_keys(
@@ -159,24 +130,6 @@ static void GCC_INLINE cache_insert(fcs_lru_cache_t * cache, unsigned char * key
     fc_solve_kaz_tree_alloc_insert(kaz_tree, cache_key);
 
 }
-
-union fcs_pre_cache_key_val_pair_struct
-{
-    struct {
-        fcs_encoded_state_buffer_t key;
-        fcs_encoded_state_buffer_t parent_and_move;
-    };
-    union fcs_pre_cache_key_val_pair_struct * next;
-};
-
-typedef union fcs_pre_cache_key_val_pair_struct fcs_pre_cache_key_val_pair_t;
-
-typedef struct {
-    dict_t * kaz_tree;
-    fcs_compact_allocator_t kv_allocator;
-    fcs_pre_cache_key_val_pair_t * kv_recycle_bin;
-    long count_elements;
-} fcs_pre_cache_t;
 
 static int fc_solve_compare_pre_cache_keys(
     const void * void_a, const void * void_b, void * context
---
+++ fc-solve/source/dbm_solver.h
@@ -0,0 +1,68 @@
+#ifndef FC_SOLVE_DBM_SOLVER_H
+#define FC_SOLVE_DBM_SOLVER_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "config.h"
+
+#include "bool.h"
+#include "inline.h"
+#include "kaz_tree.h"
+
+typedef unsigned char fcs_encoded_state_buffer_t[24];
+
+struct fcs_cache_key_info_struct
+{
+    fcs_encoded_state_buffer_t key;
+    /* lower_pri and higher_pri form a doubly linked list.
+     *
+     * pri == priority.
+     * */
+    struct fcs_cache_key_info_struct * lower_pri, * higher_pri;
+};
+
+typedef struct fcs_cache_key_info_struct fcs_cache_key_info_t;
+
+typedef struct
+{
+#if (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_JUDY)
+    Pvoid_t states_values_to_keys_map;
+#elif (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_KAZ_TREE)
+    dict_t * kaz_tree;
+#else
+#error unknown FCS_RCS_CACHE_STORAGE
+#endif
+    fcs_compact_allocator_t states_values_to_keys_allocator;
+    long count_elements_in_cache, max_num_elements_in_cache;
+
+    fcs_cache_key_info_t * lowest_pri, * highest_pri;
+
+    fcs_cache_key_info_t * recycle_bin;
+} fcs_lru_cache_t;
+
+union fcs_pre_cache_key_val_pair_struct
+{
+    struct {
+        fcs_encoded_state_buffer_t key;
+        fcs_encoded_state_buffer_t parent_and_move;
+    };
+    union fcs_pre_cache_key_val_pair_struct * next;
+};
+
+typedef union fcs_pre_cache_key_val_pair_struct fcs_pre_cache_key_val_pair_t;
+
+typedef struct {
+    dict_t * kaz_tree;
+    fcs_compact_allocator_t kv_allocator;
+    fcs_pre_cache_key_val_pair_t * kv_recycle_bin;
+    long count_elements;
+} fcs_pre_cache_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*  FC_SOLVE_DBM_SOLVER_H */
