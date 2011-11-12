102e11193864796da1af5b3df5e4742784f65b83
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-11-12 11:19:24 +0000

    Add the does_key_exist.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@4180 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/dbm_leveldb.cpp
+++ fc-solve/source/dbm_leveldb.cpp
@@ -13,3 +13,17 @@ extern "C" void fc_solve_dbm_store_init(fcs_dbm_store_t * store, const char * pa
 
     *store = (fcs_dbm_store_t)db;
 }
+
+extern "C" fcs_bool_t fc_solve_dbm_store_does_key_exist(
+    fcs_dbm_store_t store,
+    const unsigned char * key_raw
+)
+{
+    leveldb::Slice key((const char *)(key_raw+1),key_raw[0]);
+    std::string value;
+
+    leveldb::Status s =
+        ((leveldb::DB *)store)->Get(leveldb::ReadOptions(), key, &value);
+
+    return s.ok();
+}
--- fc-solve/source/dbm_leveldb.h
+++ fc-solve/source/dbm_leveldb.h
@@ -6,8 +6,15 @@ extern "C"
 {
 #endif
 
+#include "bool.h"
+    
 typedef void * fcs_dbm_store_t;
 void fc_solve_dbm_store_init(fcs_dbm_store_t * store, const char * path);
+
+fcs_bool_t fc_solve_dbm_store_does_key_exist(
+    fcs_dbm_store_t store,
+    const unsigned char * key_raw
+);
 
 #ifdef __cplusplus
 }
