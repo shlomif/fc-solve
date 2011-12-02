e42f7a60dcc7f4c1f73000da4332636bdc24a28a
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2011-12-02 12:18:40 +0000

    Add the script to run the DBM solver.
    This is just as a reference for the future to get the command line arguments
    right.
    
    
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@4224 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

---
+++ fc-solve/source/scripts/Run-dbm-solver.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Cwd;
+
+my $filename = shift(@ARGV);
+
+exec(
+    "./dbm_fc_solver",
+    # "--dbm-store-path", (Cwd::getcwd() . "/" . "dbm_store.leveldb"),
+    "--dbm-store-path", (Cwd::getcwd() . "/" . "dbm_store.bdb"),
+    "--pre-cache-max-count", 1_000_000,
+    "--caches-delta", 1_000_000,
+    "--num-threads", 2,
+    $filename
+);
