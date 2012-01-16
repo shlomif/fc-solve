679d12ad0af93f869efdc67ec892938589876257
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2012-01-16 12:47:09 +0000

    Add a copyright+info header to dbm_solver.c.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@4256 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/dbm_solver.c
+++ fc-solve/source/dbm_solver.c
@@ -1,3 +1,33 @@
+/* Copyright (c) 2012 Shlomi Fish
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+/*
+ * dbm_solver.c - a specialised solver that offloads the states' collection
+ * to an on-disk DBM database such as Berkeley DB or Google's LevelDB. In the
+ * future, may be adapted to be completely in-memory. It makes use of 
+ * delta_states.c for very compact storage.
+ *
+ */
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
