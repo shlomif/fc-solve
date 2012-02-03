f36f55bf312a089cbf042db9cc65d3b3d659e502
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2012-02-03 22:30:00 +0000

    [DBM Solver] No longer use the modified kaz_tree implementation.
    It was modified to store the records directly at the place, and as a result
    clashed with the original kaz_tree symbols. Not a good idea, and it was now
    reverted.
    
    
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@4381 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/source/dbm_kaztree.c
+++ fc-solve/source/dbm_kaztree.c
@@ -1,33 +1,51 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "dbm_solver_key.h"
-#define FCS_KAZ_TREE_USE_RECORD_DICT_KEY
+
 #include "dbm_solver.h"
 #include "kaz_tree.h"
-#include "kaz_tree.c"
 
 /*
  * TODO : We waste too much space and fragment it storing the
  * key/parent_move separatley from the dnode_t. We should use a struct
  * for that instead of a pointer.
  * */
+typedef struct
+{
+    fcs_encoded_state_buffer_t key;
+    fcs_encoded_state_buffer_t parent_and_move;
+} record_t;
 
-typedef dict_t dbm_t;
+
+typedef struct
+{
+    dict_t * kaz_tree;
+    fcs_compact_allocator_t allocator;
+} dbm_t;
 
 /* TODO: make sure the key is '\0'-padded. */
 static int compare_records(
     const void * void_a, const void * void_b, void * context
 )
 {
-#define GET_PARAM(p) ((((const fcs_dbm_record_t *)(p))->key))
-    return memcmp(&(GET_PARAM(void_a)), &(GET_PARAM(void_b)), sizeof(GET_PARAM(void_a)));
+#define GET_PARAM(p) (&(((const record_t *)(p))->key))
+    return memcmp(GET_PARAM(void_a), GET_PARAM(void_b), sizeof(GET_PARAM(void_a)));
 #undef GET_PARAM
 }
 
 void fc_solve_dbm_store_init(fcs_dbm_store_t * store, const char * path)
 {
-    *store = (fcs_dbm_store_t)fc_solve_kaz_tree_create(compare_records, NULL);
+    dbm_t * db;
+    db = malloc(sizeof(*db));
+
+    db->kaz_tree =
+        fc_solve_kaz_tree_create(compare_records, NULL);
+
+    fc_solve_compact_allocator_init(
+        &(db->allocator)
+    );
+
+    *store = (fcs_dbm_store_t)db;
     return;
 }
 
@@ -40,11 +58,21 @@ fcs_bool_t fc_solve_dbm_store_insert_key_value(
     fcs_encoded_state_buffer_t * parent_and_move
 )
 {
-    fcs_dbm_record_t to_check;
+    dbm_t * db;
+    record_t * to_check;
+    fcs_bool_t ret;
 
-    to_check.key = *key;
-    to_check.parent_and_move = *parent_and_move;
-    return (fc_solve_kaz_tree_alloc_insert((dbm_t *)store, to_check) == NULL);
+    db = (dbm_t *)store;
+
+    to_check = (record_t *)fcs_compact_alloc_ptr(&(db->allocator), sizeof(*to_check));
+
+    to_check->key = *key;
+    to_check->parent_and_move = *parent_and_move;
+    if (! (ret = (fc_solve_kaz_tree_alloc_insert(db->kaz_tree, to_check) == NULL)))
+    {
+        fcs_compact_alloc_release(&(db->allocator));
+    }
+    return ret;
 }
 
 fcs_bool_t fc_solve_dbm_store_lookup_parent_and_move(
@@ -55,10 +83,10 @@ fcs_bool_t fc_solve_dbm_store_lookup_parent_and_move(
 {
     dnode_t * node;
 
-    fcs_dbm_record_t to_check;
+    record_t to_check;
     to_check.key = *(fcs_encoded_state_buffer_t *)key;
 
-    node = fc_solve_kaz_tree_lookup(((dbm_t *)store), to_check);
+    node = fc_solve_kaz_tree_lookup(((dbm_t *)store)->kaz_tree, &to_check);
 
     if (! node)
     {
@@ -66,12 +94,18 @@ fcs_bool_t fc_solve_dbm_store_lookup_parent_and_move(
     }
     else
     {
-        *(fcs_encoded_state_buffer_t *)parent_and_move = ((node->dict_key)).parent_and_move;
+        *(fcs_encoded_state_buffer_t *)parent_and_move
+            = ((record_t *)(node->dict_key))->parent_and_move;
         return TRUE;
     }
 }
 
 extern void fc_solve_dbm_store_destroy(fcs_dbm_store_t store)
 {
-    fc_solve_kaz_tree_destroy( (dbm_t *)store);
+    dbm_t * db;
+    db = (dbm_t *)store;
+
+    fc_solve_kaz_tree_destroy( db->kaz_tree );
+    fc_solve_compact_allocator_finish( &(db->allocator) );
+    free(db);
 }
