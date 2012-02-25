96a791cc23a3e9d9eb5b9a02ded16ccd90ab041f
shlomif <shlomif@e7e8a897-7ba4-4ee7-b36f-f4c66519b19a>
2012-02-25 20:05:58 +0000

    Add more flamingo presets.
    git-svn-id: file:///home/shlomif/Backup/svn-dumps/google-code/svnsync-repos/fc-solve/fc-solve/trunk@4464 e7e8a897-7ba4-4ee7-b36f-f4c66519b19a

--- fc-solve/presets/variations-on-depth-test-order/freecell-4/Run-Presets-1.bash
+++ fc-solve/presets/variations-on-depth-test-order/freecell-4/Run-Presets-1.bash
@@ -75,8 +75,9 @@ _preset "01234567" -to 01234567
 _preset "0123456" -to 0123456
 
 _preset "flamingo" -to 01234567 -dto 30,0123876594
-_preset "flamingo100" -to 01234567 -dto 100,0123876594
-_preset "flamingo65" -to 01234567 -dto 65,0123876594
+for threshold in 40 50 55 65 70 75 80 85 90 95 100 105 110 115 120 130 140 150 ; do
+    _preset "flamingo${threshold}" -to 01234567 -dto "${threshold},0123876594"
+done    
 
 _preset "qlad"  -to 01234567  -dto 13,0187465  -dto 26,0134587 -dto 35,819435
 _preset "encad"  -to 01234567  -dto 13,0187465  -dto 26,0134587 -dto 35,0123456789
