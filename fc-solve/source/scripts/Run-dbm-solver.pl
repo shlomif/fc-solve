d7d704c040afb1112fcf54e8fd005f94b1903fc8
Shlomi Fish <shlomif@iglu.org.il>
2012-04-13 18:40:48 +0300

    Some improvements to to Run-dbm-solver.pl.
    The dbm_fc_solver still crashes  sometimes.

--- fc-solve/source/scripts/Run-dbm-solver.pl
+++ fc-solve/source/scripts/Run-dbm-solver.pl
@@ -7,10 +7,23 @@ use Cwd;
 
 my $filename = shift(@ARGV);
 
+my $dbm_store = (Cwd::getcwd() . "/" . "dbm_store.leveldb");
+my $dump_file = "$filename-dump";
+
+if (-e $dbm_store)
+{
+    die "DBM store '$dbm_store' exists.";
+}
+
+if (-e $dump_file)
+{
+    die "Dump file $dump_file exists.";
+}
+
 exec(
     "./dbm_fc_solver",
-    "-o", "$filename-dump",
-    "--dbm-store-path", (Cwd::getcwd() . "/" . "dbm_store.leveldb"),
+    "-o", $dump_file,
+    "--dbm-store-path", $dbm_store,
     # "--dbm-store-path", (Cwd::getcwd() . "/" . "dbm_store.bdb"),
     "--pre-cache-max-count", 1_000_000,
     "--caches-delta", 1_000_000,
