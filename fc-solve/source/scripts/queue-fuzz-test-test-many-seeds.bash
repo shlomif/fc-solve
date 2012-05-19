b19a1b317af8e19db9997ef7dd003a5800412284
Shlomi Fish <shlomif@shlomifish.org>
2012-05-19 10:57:56 +0300

    Alternating seeds for the Fuzz test.
    This is so we won't keep testing the same seeds which will yield similar
    patterns.

--- fc-solve/source/scripts/queue-fuzz-test-test-many-seeds.bash
+++ fc-solve/source/scripts/queue-fuzz-test-test-many-seeds.bash
@@ -3,12 +3,14 @@ export USE_C="1"
 export FCS_PATH="$(pwd)"
 let items_per_page=20
 let data_seed=500
+let seed=1
 while true ; do
-    for seed in `seq 1 20`; do
+    for i in `seq 1 20`; do
         if ! perl scripts/queue-fuzz-test.pl "$items_per_page" "$data_seed" "$seed" ; then
             echo "Fuzz test program failed with items_per_page=$items_per_page data_seed=$data_seed and interval_seed=$seed"
             exit 1
         fi
+        let seed++
     done
     let items_per_page++
 done
