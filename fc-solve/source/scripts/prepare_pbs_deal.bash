727fa21f5cb6fcd28759373ff4825b7afbcb5295
Shlomi Fish <shlomif@shlomifish.org>
2012-05-25 01:01:11 +0300

    Some bug fixes for the shar-less env.

--- fc-solve/source/scripts/prepare_pbs_deal.bash
+++ fc-solve/source/scripts/prepare_pbs_deal.bash
@@ -11,6 +11,7 @@ board="$deal.board"
 
 offload_dir="/fastscratch/dhingralab/2fc-dbm_fc_solver-offload/deal-$deal"
 final_out="/scratch/dhingralab/2fc-dbm_fc_solver-results/deal-$deal"
+base_dir='/home/dhingralab/artemus_harper/dbm_fcs_for_subanark'
 
 echo '#!/bin/bash' > "$output"
 echo "#PBS -N 2_fc_Freecell_deal_$deal" >> "$output"
@@ -18,7 +19,7 @@ echo "#PBS -l walltime=120:00:00,mem=200gb" >> "$output"
 echo "#PBS -m abe" >> "$output"
 echo "#" >> "$output"
 echo  >> "$output"
-shar "$board" "$target" | grep -vE '^ *exit *0 *$' >> "$output"
+# shar "$board" "$target" | grep -vE '^ *exit *0 *$' >> "$output"
 echo >> "$output"
 echo "mkdir -p '$offload_dir' '$final_out'" >> "$output"
-echo "./$target --num-threads 16 --offload-dir-path '$offload_dir' '$board' | tee '$final_out/$deal.dump' '$final_out/$deal.dump.backup'" >> "$output"
+echo "$base_dir/$target --num-threads 16 --offload-dir-path '$offload_dir' '$base_dir/$board' | tee '$final_out/$deal.dump' '$final_out/$deal.dump.backup'" >> "$output"
