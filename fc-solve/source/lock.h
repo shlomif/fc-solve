261f87abd69086ac9e4f6cd65e7a606b3875e98a
Shlomi Fish <shlomif@shlomifish.org>
2012-08-14 16:54:03 +0300

    lock.h: refactoring: add whitespace.

--- fc-solve/source/lock.h
+++ fc-solve/source/lock.h
@@ -43,28 +43,34 @@ extern "C" {
 #include <pthread/rwlock_fcfs.h>
 #endif
 
-static const pthread_mutex_t initial_mutex_constant =
-    PTHREAD_MUTEX_INITIALIZER
-    ;
-
 #ifdef FCS_DBM_SINGLE_THREAD
+
 typedef fcs_bool_t fcs_lock_t;
 #define FCS_LOCK(lock) {}
 #define FCS_UNLOCK(lock) {}
 #define FCS_INIT_LOCK(lock) {}
 #define FCS_DESTROY_LOCK(lock) {}
+
 #elif defined(FCS_DBM_USE_RWLOCK)
+
 typedef pthread_rwlock_fcfs_t * fcs_lock_t;
 #define FCS_LOCK(lock) pthread_rwlock_fcfs_gain_write(lock)
 #define FCS_UNLOCK(lock) pthread_rwlock_fcfs_release(lock)
 #define FCS_INIT_LOCK(lock) ((lock) = pthread_rwlock_fcfs_alloc())
 #define FCS_DESTROY_LOCK(lock) pthread_rwlock_fcfs_destroy(lock)
+
 #else
+
+static const pthread_mutex_t initial_mutex_constant =
+    PTHREAD_MUTEX_INITIALIZER
+    ;
+
 typedef pthread_mutex_t fcs_lock_t;
 #define FCS_LOCK(lock) pthread_mutex_lock(&(lock))
 #define FCS_UNLOCK(lock) pthread_mutex_unlock(&(lock))
 #define FCS_INIT_LOCK(lock) ((lock) = initial_mutex_constant)
 #define FCS_DESTROY_LOCK(lock) {}
+
 #endif
 
 #ifdef __cplusplus
