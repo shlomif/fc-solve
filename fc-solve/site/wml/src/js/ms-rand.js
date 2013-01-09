c57dfb87de3b7acd3ea47c894aa8687150bd49f4
Shlomi Fish <shlomif@shlomifish.org>
2013-01-09 17:10:32 +0200

    Add more.

---
+++ fc-solve/site/wml/src/js/ms-rand.js
@@ -0,0 +1,33 @@
+"use strict";
+/*
+ * Microsoft C Run-time-Library-compatible Random Number Generator
+ * Copyright by Shlomi Fish, 2011.
+ * Released under the MIT/X11 License
+ * ( http://en.wikipedia.org/wiki/MIT_License ).
+ * */
+Class('MSRand', {
+    has: {
+        seed: { is: rw, },
+    },
+    methods: {
+        rand: function() {
+            this.setSeed((this.getSeed() * 214013 + 2531011) & 0x7FFFFFFF);
+            return ((this.getSeed() >> 16) & 0x7fff);
+        },
+        max_rand: function(mymax) {
+            return this.rand() % mymax;
+        },
+        shuffle: function(deck) {
+            if (deck.length) {
+                var i = deck.length;
+                while (--i) {
+                    var j = this.max_rand(i+1);
+                    var tmp = deck[i];
+                    deck[i] = deck[j];
+                    deck[j] = tmp;
+                }
+            }
+            return deck;
+        },
+    },
+});
