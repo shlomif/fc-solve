6c62d9c8eb66e8f7a435394922585a461a344275
Shlomi Fish <shlomif@shlomifish.org>
2013-01-10 16:06:10 +0200

    Convert the stylesheet generation to SASS.
    This will make it easier to maintain.

---
+++ fc-solve/site/wml/config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+css_dir = "dest"
+sass_dir = "lib/sass"
+images_dir = "src/images"
+# output_style = :compressed
+output_style = :expanded
+# To enable relative image paths using the images_url() function:
+# http_images_path = :relative
+http_images_path = "images"
