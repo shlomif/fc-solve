dab8d586276621f1ade28479ad307717189162e1
Shlomi Fish <shlomif@shlomifish.org>
2013-01-16 12:57:15 +0200

    Minimal successful tests.

---
+++ fc-solve/site/wml/src/js/web-fc-solve-tests.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function test_js_fc_solve_class()
+{
+    module("FC_Solve.Algorithmic");
+
+    test("FC_Solve main test", function() {
+        expect(1);
+
+        // TEST
+        ok (true, "True is, well, true.");
+    });
+
+    return;
+}
