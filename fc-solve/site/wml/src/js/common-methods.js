e442a744ba618bcea50491bce0bb7f44fbe5a954
Shlomi Fish <shlomif@shlomifish.org>
2013-01-19 15:13:28 +0200

    Fixed a copy+paste error (lowercasing).
    Thanks to ThiefMaster on http://stackoverflow.com/a/14414659/7709 .

--- fc-solve/site/wml/src/js/common-methods.js
+++ fc-solve/site/wml/src/js/common-methods.js
@@ -75,11 +75,11 @@ if (!Array.prototype.map) {
   };
 }
 
-// taken from http://stackoverflow.com/questions/202605/repeat-string-javascript
-if (!string.prototype.repeat) {
-string.prototype.repeat = function(count) {
+// Taken from http://stackoverflow.com/questions/202605/repeat-string-javascript
+if (!String.prototype.repeat) {
+String.prototype.repeat = function(count) {
     if (count < 1) return '';
-    var result = '', pattern = this.valueof();
+    var result = '', pattern = this.valueOf();
     while (count > 0) {
         if (count & 1) result += pattern;
         count >>= 1, pattern += pattern;
