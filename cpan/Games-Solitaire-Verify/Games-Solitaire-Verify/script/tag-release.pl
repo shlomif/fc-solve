36cbdac6f8422334b5ddda2766493331c713b6f6
Shlomi Fish <shlomif@shlomifish.org>
2013-06-28 19:29:10 +0300

    Update tag-release.pl.

--- cpan/Games-Solitaire-Verify/Games-Solitaire-Verify/script/tag-release.pl
+++ cpan/Games-Solitaire-Verify/Games-Solitaire-Verify/script/tag-release.pl
@@ -7,7 +7,7 @@ use IO::All;
 
 my ($version) =
     (map { m{\$VERSION *= *'([^']+)'} ? ($1) : () }
-    io->file('lib/Games/Solitaire/Verify.pm')->getlines()
+    io->file("./lib/Games/Solitaire/Verify.pm")->getlines()
     )
     ;
 
@@ -16,16 +16,11 @@ if (!defined ($version))
     die "Version is undefined!";
 }
 
-my $mini_repos_base = 'https://fc-solve.googlecode.com/svn/fc-solve';
-
-my $mod_name = "Games-Solitaire-Verify";
 my @cmd = (
-    "svn", "copy", "-m",
-    "Tagging the $mod_name release as $version",
-    "$mini_repos_base/trunk",
-    "$mini_repos_base/tags/$mod_name/$mod_name/cpan-releases/$version",
+    "hg", "tag", "-m",
+    "Tagging Games-Solitaire-Verify as $version",
+    "Games-Solitaire-Verify-$version",
 );
 
 print join(" ", map { /\s/ ? qq{"$_"} : $_ } @cmd), "\n";
 exec(@cmd);
-
