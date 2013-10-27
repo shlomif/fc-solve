db51954ecd9c9087981bc857868cf30f0b8d67a0
Shlomi Fish <shlomif@shlomifish.org>
2013-10-27 20:09:42 +0200

    Add the sample-split-fcc invocation script.
    This was not tested yet - prepare for fireworks.

---
+++ fc-solve/scripts/sample-split_fcc_fc_solver-invocation-1.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+export FCS_PATH=/home/shlomif/progs/freecell/git/fc-solve/fc-solve/source/B FCS_SRC_PATH=/home/shlomif/progs/freecell/git/fc-solve/fc-solve/source
+offload_path="./split_fcc_offload_dir_path"
+out_dir="./split_fcc_out_dir"
+idx="384243"
+board="${idx}.board"
+if ! test -e "$offload_path" ; then
+    mkdir "$offload_path"
+fi
+if ! test -e "$out_dir" ; then
+    mkdir "$out_dir"
+fi
+if ! test -e "$board" ; then
+    pi-make-microsoft-freecell-board -t "$idx" > "$board"
+fi
+init_board="${idx}.init.board"
+if ! test -e "$init_board" ; then
+    (
+        echo 'Foundations: H-A'
+        perl -plE 's/ AH *\z//' < "$board"
+    ) > "$init_board"
+fi
+input="${idx}.input.txtish"
+if ! test -e "$input" ; then
+    (
+        export I="${input}"
+        perl -I ../t/t/lib -E 'use strict; use warnings; use FC_Solve::Base64; use Games::Solitaire::FC_Solve::DeltaStater::DeBondt; use IO::All; my $delta = Games::Solitaire::FC_Solve::DeltaStater::DeBondt->new({init_state_str => io->file($ENV{I})->all(), }); $delta->set_derived({state_str => io->file($ENV{I})->all(), }); print (FC_Solve::Base64::base64_encode($delta->encode_composite()), " 0 ", FC_Solve::Base64::base64_encode(q{}), "\n");'
+    ) > "$input"
+fi
+./split_fcc_fc_solver \
+    --offload-dir-path "$offload_path" \
+    --output "$out_dir" \
+    --board "$board" \
+    --fingerprint "$(perl -I ../t/t/lib -E 'use strict; use warnings; use FC_Solve::Base64; print FC_Solve::Base64::base64_encode("\x{02}" . ("\x0" x 12))')" \
+    --input "$input"
