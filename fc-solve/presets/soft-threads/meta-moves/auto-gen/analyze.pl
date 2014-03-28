faab973738d347f579edfbdac11a9db4ee4b559b
Shlomi Fish <shlomif@shlomifish.org>
2014-03-28 22:10:19 +0300

    Refactoring: convert analyze.pl to AI::Pf::OM.

--- fc-solve/presets/soft-threads/meta-moves/auto-gen/analyze.pl
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/analyze.pl
@@ -1,28 +1,37 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
 use strict;
+use warnings;
 
-use PDL;
+use PDL ();
 
-use MyInput;
+use AI::Pathfinding::OptimizeMultiple::DataInputObj;
+
+my $above_how_much = shift(@ARGV) || 2000;
 
 my $start_board = 1;
-
 my $num_boards = 32000;
 
-my $above_how_much = shift || 2000;
+my $input_obj = AI::Pathfinding::OptimizeMultiple::DataInputObj->new(
+    {
+        start_board => $start_board,
+        num_boards => $num_boards,
+    }
+);
 
-my $inp = MyInput->new({start_board => $start_board, num_boards => $num_boards});
+my $data_hash_ref = $input_obj->get_scans_iters_pdls();
+my $scan_ids = $input_obj->get_scan_ids_aref;
 
-my $scans_data =
-    $inp->get_scans_data();
+my $count_of_scans = @$scan_ids;
 
-my $which_boards = which((($scans_data > $above_how_much) | ($scans_data < 0))->xchg(0,1)->sumover() == scalar(@{$inp->selected_scans()}))+1;
+my $scans_data = PDL::cat( @{$data_hash_ref}{@$scan_ids} );
+
+my $which_boards = PDL::which((($scans_data > $above_how_much) | ($scans_data < 0))->xchg(0,1)->sumover() == $count_of_scans)+1;
 
 print "The indexes of the tests above $above_how_much are:\n";
-foreach my $board (list($which_boards))
+foreach my $board (PDL::list($which_boards))
 {
-    print "$board: " . join(",", sort { (($b <=> 0) <=> ($a <=> 0)) || ($a <=> $b) } list($scans_data->slice(($board-1).",:"))) . "\n";
+    print "$board: " . join(",", sort { (($b <=> 0) <=> ($a <=> 0)) || ($a <=> $b) } PDL::list($scans_data->slice(($board-1).",:"))) . "\n";
 }
 
 
