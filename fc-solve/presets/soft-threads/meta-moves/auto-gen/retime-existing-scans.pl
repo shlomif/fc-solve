83f5cd0b5dcc30269c2f0b0955ebbf153e4594f8
Shlomi Fish <shlomif@shlomifish.org>
2014-03-28 21:06:12 +0300

    Add more.

--- fc-solve/presets/soft-threads/meta-moves/auto-gen/retime-existing-scans.pl
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/retime-existing-scans.pl
@@ -3,20 +3,29 @@
 use strict;
 use warnings;
 
-use MyInput;
-
-my $id = shift(@ARGV);
+use AI::Pathfinding::OptimizeMultiple::DataInputObj;
 
 my $start_board = 1;
 my $num_boards = 32_000;
 
-my $input_obj = MyInput->new(
+my $input_obj = AI::Pathfinding::OptimizeMultiple::DataInputObj->new(
     {
         start_board => $start_board,
         num_boards => $num_boards,
     }
 );
 
+my %params;
+if (exists($ENV{FC_NUM}))
+{
+    $params{freecells_num} = $ENV{FC_NUM};
+}
+if (exists($ENV{FC_VARIANT}))
+{
+    $params{variant} = $ENV{FC_VARIANT};
+}
+
+SCANS_LOOP:
 foreach my $scan_rec (@{$input_obj->selected_scans()})
 {
     my $id = $scan_rec->id();
@@ -25,8 +34,8 @@ foreach my $scan_rec (@{$input_obj->selected_scans()})
     # Scan was already done.
     if (-e "data/$id.fcs.moves.txt")
     {
-        next;
+        next SCANS_LOOP;
     }
-    $input_obj->time_scan({ id => $id, argv => \@cmd_line, });
+    $input_obj->time_scan({ %params, id => $id, argv => \@cmd_line, });
 }
 
