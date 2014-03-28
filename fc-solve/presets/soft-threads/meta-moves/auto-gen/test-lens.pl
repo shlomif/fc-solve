7829a4ae487ca929a3f58b9202fb3dbfa662a8d5
Shlomi Fish <shlomif@shlomifish.org>
2014-03-28 17:22:23 +0300

    Convert test-lens.pl away from MyInput.

--- fc-solve/presets/soft-threads/meta-moves/auto-gen/test-lens.pl
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/test-lens.pl
@@ -3,13 +3,27 @@
 use strict;
 use warnings;
 
-use MyInput;
-
-use vars qw($scans_lens_data);
+use AI::Pathfinding::OptimizeMultiple::DataInputObj;
 
 my @params = (1,100);
-$scans_lens_data = MyInput::get_scans_lens_data(@params,
-    MyInput::get_selected_scan_list(@params));
+
+my $start_board = 1;
+my $num_boards = 32_000;
+
+my $input_obj = AI::Pathfinding::OptimizeMultiple::DataInputObj->new(
+    {
+        start_board => $start_board,
+        num_boards => $num_boards,
+    }
+);
+
+my $data_hash_ref = $input_obj->get_scans_lens_iters_pdls();
+
+my @scan_ids = map { $_->id() } @{$input_obj->selected_scans};
+
+my %scan_ids_to_indexes = (map { $scan_ids[$_] => $_ } keys @scan_ids);
+
+my $scans_lens_data = PDL::cat( @{$data_hash_ref}{@scan_ids} )->xchg(1,3)->clump(2..3);
 
 my $iters = $scans_lens_data->slice(":,:,0");
 my $iters_quota = 500;
@@ -28,14 +42,8 @@ my $solved_moves_sums = my_sum_over($solved_moves);
 my $solved_moves_counts = my_sum_over($solved);
 my $solved_moves_avgs = $solved_moves_sums / $solved_moves_counts;
 
-print "$solved\n";
-print "$solved_moves\n";
+# print "$solved\n";
+# print "$solved_moves\n";
 print "SUMS = $solved_moves_sums\n";
 print "COUNTS = $solved_moves_counts\n";
 print "AVGS = $solved_moves_avgs\n";
-
-print "5\n";
-# while(1)
-# {
-#    sleep(1);
-# }
