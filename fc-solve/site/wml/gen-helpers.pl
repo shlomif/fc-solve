db568c702155653b21a479316a76081dc0541181
Shlomi Fish <shlomif@shlomifish.org>
2014-05-17 14:21:49 +0300

    Do not build the temporary leftover ipp.* files.
    Sometimes wml leaves them and they should not be registered and built.

--- fc-solve/site/wml/gen-helpers.pl
+++ fc-solve/site/wml/gen-helpers.pl
@@ -6,6 +6,10 @@ use warnings;
 use HTML::Latemp::GenMakeHelpers;
 use File::Find::Object::Rule;
 
+require IO::All;
+
+IO::All->import('io');
+
 my $generator =
     HTML::Latemp::GenMakeHelpers->new(
         'hosts' =>
@@ -33,6 +37,10 @@ if ($E)
 {
     print "$E\n";
 }
+
+my $text = io("include.mak")->slurp();
+$text =~ s!^((?:T2_DOCS|T2_DIRS) = )([^\n]*)!my ($prefix, $files) = ($1,$2); $prefix . ($files =~ s# +ipp\.\S*##gr)!ems;
+io("include.mak")->print($text);
 
 1;
 
