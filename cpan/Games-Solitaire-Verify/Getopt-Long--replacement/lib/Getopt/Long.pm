c040120e1320404573cc8a65ccb10c54f52e13cf
Shlomi Fish <shlomif@shlomifish.org>
2014-11-01 18:40:01 +0200

    Add the Getopt-Long replacement.

---
+++ cpan/Games-Solitaire-Verify/Getopt-Long--replacement/lib/Getopt/Long.pm
@@ -0,0 +1,55 @@
+package Getopt::Long;
+
+use strict;
+use warnings;
+
+use parent 'Exporter';
+
+our @EXPORT_OK = (qw( GetOptionsFromArray ));
+
+sub GetOptionsFromArray
+{
+    my $cmd_line_args = shift;
+
+    my @rules = @_;
+
+    my $i = 0;
+
+    while (@$cmd_line_args)
+    {
+        my $arg = $cmd_line_args->[0];
+        if ($arg !~ s/\A-//)
+        {
+            return 1;
+        }
+        shift(@$cmd_line_args);
+
+        # Double dash
+        $arg =~ s/\A-//;
+
+        if (! @$cmd_line_args)
+        {
+            die "Missing param to $arg";
+        }
+
+        my $param = shift(@$cmd_line_args);
+
+        RULES:
+        for (my $r_idx = 0; $r_idx < @rules; $r_idx += 2)
+        {
+            my $spec = $rules[$r_idx];
+            my $cb = $rules[$r_idx+1];
+
+            $spec =~ s/=.*//ms;
+            my %h = (map { $_ => 1 } split(/\|/, $spec));
+            if (exists ($h{$arg}))
+            {
+                $cb->($arg, $param);
+                last RULES;
+            }
+        }
+    }
+}
+
+1;
+
