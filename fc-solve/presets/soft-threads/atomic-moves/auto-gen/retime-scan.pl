9864361956e77f306dff1bf94af20203684af086
Shlomi Fish <shlomif@shlomifish.org>
2015-03-03 21:03:36 +0200

    Add for the updated iters count.
    Found a different problem though:
    
    http://groups.yahoo.com/neo/groups/fc-solve-discuss/conversations/messages/1399
    
    Patsolve appears to solve the same initial Freecell layout differently
    based on the order of the columns.
    
    <<<<
    The problem is that it [= patsolve] yeilds different results based on the
    permutations of the order of columns in Freecell layouts (including those from
    the initial Microsoft Freecell deals):
    >>>>

--- fc-solve/presets/soft-threads/atomic-moves/auto-gen/retime-scan.pl
+++ fc-solve/presets/soft-threads/atomic-moves/auto-gen/retime-scan.pl
@@ -3,5 +3,7 @@
 use strict;
 use warnings;
 
+local $ENV{FC_SOLVE_RANGE_ITERS_LIMIT} = '100000000';
+
 my $dir = "../../meta-moves/auto-gen/";
 exec($^X, "-I$dir", "$dir/retime-scan.pl", @ARGV);
--- fc-solve/presets/soft-threads/atomic-moves/auto-gen/time-scan.pl
+++ fc-solve/presets/soft-threads/atomic-moves/auto-gen/time-scan.pl
@@ -3,5 +3,7 @@
 use strict;
 use warnings;
 
+local $ENV{FC_SOLVE_RANGE_ITERS_LIMIT} = '5000000';
+
 my $dir = "../../meta-moves/auto-gen/";
 exec($^X, "-I$dir", "$dir/time-scan.pl", @ARGV);
