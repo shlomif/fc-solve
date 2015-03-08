521f62f05699f9ac3b23c2d471106de11258f230
Shlomi Fish <shlomif@shlomifish.org>
2015-03-08 18:36:20 +0200

    Remove trailing space.

--- fc-solve/presets/soft-threads/atomic-moves/auto-gen/__Time_Patsolve_Scan.pl
+++ fc-solve/presets/soft-threads/atomic-moves/auto-gen/__Time_Patsolve_Scan.pl
@@ -7,7 +7,7 @@ my @x_params = (qw(4 1 8 -1 7 11 4 2 2 1 2));
 
 my @y_params = (0.0032, 0.32, -3.0);
 
-exec($^X, "time-scan.pl", 
+exec($^X, "time-scan.pl",
     "--method", "patsolve",
     (map { ("--patsolve-x-param", "$_,$x_params[$_]") } keys @x_params),
     (map { ("--patsolve-y-param", "$_,$y_params[$_]") } keys @y_params),
