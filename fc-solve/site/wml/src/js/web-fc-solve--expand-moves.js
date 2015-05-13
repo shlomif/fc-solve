49c8783de7364f03aaeab794a3ebc74274cc8567
Shlomi Fish <shlomif@shlomifish.org>
2015-05-13 23:02:36 +0300

    Add more to the expand cards JS routine.

--- fc-solve/site/wml/src/js/web-fc-solve--expand-moves.js
+++ fc-solve/site/wml/src/js/web-fc-solve--expand-moves.js
@@ -2,7 +2,7 @@
 
 function fc_solve_expand_move (num_stacks, num_freecells, initial_src_state_str, initial_move, initial_dest_state_str) {
 
-    var matched = initial_move.match(/^Move ([0-9]+) cards from stack ([0-9]+) to stack ([0-9]+)$/);
+    var matched = initial_move.str.match(/^Move ([0-9]+) cards from stack ([0-9]+) to stack ([0-9]+)$/);
 
     if (! matched) {
         return [initial_move];
@@ -23,6 +23,8 @@ function fc_solve_expand_move (num_stacks, num_freecells, initial_src_state_str,
     var empty_fc_indexes = [];
     var empty_stack_indexes = [];
 
+    var modified_state = {f:[],c:[]};
+
     var freecell_match = initial_src_state_str.match(/\nFreecells:([^\n]*)\n/);
     if (!freecell_match) {
         throw "Cannot match freecell exception.";
@@ -35,8 +37,12 @@ function fc_solve_expand_move (num_stacks, num_freecells, initial_src_state_str,
     }
 
     for (var idx = 0 ; idx < num_freecells ; idx++) {
-        if (freecell_string.substring(idx*4, (idx+1)*4) == "    ") {
+        var fc_s = freecell_string.substring(idx*4, (idx+1)*4);
+        if (fc_s == "    ") {
+            modified_state.f[idx] = null;
             empty_fc_indexes.push(idx);
+        } else {
+            modified_state.f[idx] = {t: 's', s: fc_s};
         }
     }
 
@@ -47,11 +53,22 @@ function fc_solve_expand_move (num_stacks, num_freecells, initial_src_state_str,
     }
 
     for (var idx = 0 ; idx < num_stacks ; idx++) {
+        var cards = col_matches[idx].match(/\w{2}/g);
+
+        modified_state.c[idx] = cards;
         if ((idx != ultimate_dest) && (idx != ultimate_source) &&
-            col_matches[idx].match(/^\n:\s*$/) {
+            (card.length == 0)) {
                 empty_stack_indexes.push(idx);
         }
     }
+
+    var foundations_match = initial_src_state_str.match(/^(Foundations:[^\n]*\n)/);
+
+    if (! foundations_match) {
+        throw "Cannot find foundations.";
+    }
+
+    var foundations_str = foundations_match[1];
 
     var num_cards_moved_at_each_stage = [];
 
@@ -69,4 +86,25 @@ function fc_solve_expand_move (num_stacks, num_freecells, initial_src_state_str,
     }
     num_cards_moved_at_each_stage.push( num_cards );
 
+    var ret_array = [];
+
+
+    var output_state_promise = function() { return; };
+
+    var past_first_output_state_promise = function() {
+        var state_string = foundations_str +
+            "Freecells: " + (modified_state.f.map(function (fc) {
+            return ((!fc) ? '    ' : (fc.t == 's') ? fc.s : ("  " + fc.c));
+        }).join("")) + "\n" + (modified_state.c.map(function(col) {
+            return ": " + col.join(" ") + "\n";
+        }).join(""));
+        ret_array.push(
+            {
+                type: 's',
+                str: state_string,
+            }
+        );
+    };
+
+    return ret_array;
 }
