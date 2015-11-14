82cb5ac29a8efceaf973364ceb8f19381b549c4f
Shlomi Fish <shlomif@shlomifish.org>
2015-11-14 23:52:22 +0200

    Fix ../Tatzer --rcs.
    I was too overzealous with applying C99 struct initialisers.

--- fc-solve/source/instance_for_lib.h
+++ fc-solve/source/instance_for_lib.h
@@ -335,16 +335,10 @@ static GCC_INLINE void fc_solve_init_instance(fc_solve_instance_t * const instan
     {
         fcs_lru_cache_t * cache = &(instance->rcs_states_cache);
 
-        *cache = (typeof(*cache))
-        {
-            .lowest_pri = NULL,
-            .highest_pri = NULL,
-            .recycle_bin = NULL,
-            .count_elements_in_cache = 0,
 #if (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_JUDY)
-            .states_values_to_keys_map = ((Pvoid_t) NULL);
+        cache->states_values_to_keys_map = ((Pvoid_t) NULL);
 #elif (FCS_RCS_CACHE_STORAGE == FCS_RCS_CACHE_STORAGE_KAZ_TREE)
-            .kaz_tree = fc_solve_kaz_tree_create(fc_solve_compare_lru_cache_keys, NULL, instance->meta_alloc);
+        cache->kaz_tree = fc_solve_kaz_tree_create(fc_solve_compare_lru_cache_keys, NULL, instance->meta_alloc);
 #else
 #error Unknown FCS_RCS_CACHE_STORAGE
 #endif
@@ -353,6 +347,10 @@ static GCC_INLINE void fc_solve_init_instance(fc_solve_instance_t * const instan
             &(cache->states_values_to_keys_allocator),
             instance->meta_alloc
         );
+        cache->lowest_pri = NULL;
+        cache->highest_pri = NULL;
+        cache->recycle_bin = NULL;
+        cache->count_elements_in_cache = 0;
     }
 
 #endif
