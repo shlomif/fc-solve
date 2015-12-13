3e364d758ed39791909c2ab99f890c506d2a3e5f
Shlomi Fish <shlomif@shlomifish.org>
2015-12-14 00:21:14 +0200

    Fix old.bash as well.

--- fc-solve/scripts/TEST_OPTIMIZATIONS/old.bash
+++ fc-solve/scripts/TEST_OPTIMIZATIONS/old.bash
@@ -1,1 +1,1 @@
-export SRC_DIR="$(pwd)/../source/" ARGS="--worker-step 16 -l as" num_cpus="$(cat /proc/cpuinfo | grep -P '^processor\s*:' | wc -l)"; bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash "$num_cpus" "$num_cpus"
+export SRC_DIR="$(pwd)/../../source/" ARGS="--worker-step 16 -l as" num_cpus="$(cat /proc/cpuinfo | grep -P '^processor\s*:' | wc -l)"; bash "${SRC_DIR:-.}"/scripts/time-threads-num.bash "$num_cpus" "$num_cpus"
