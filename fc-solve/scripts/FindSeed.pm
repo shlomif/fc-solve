c2d3a3671271ea6366f0b7b97ad333f6793407e4
Shlomi Fish <shlomif@shlomifish.org>
2015-12-14 16:35:48 +0200

    FindSeed accepts an array-ref of scans.
    For better convenience.

--- fc-solve/scripts/FindSeed.pm
+++ fc-solve/scripts/FindSeed.pm
@@ -5,17 +5,20 @@ use warnings;
 use autodie;
 
 use List::Util qw/max/;
+use List::UtilsBy qw/min_by/;
 use bytes;
 use integer;
 
-sub f { return $_[0] =~ /Iters: ([0-9]+)/ ? $1 : 1e6 }
+sub f { return $_[0] =~ /Solved.*?Iters: ([0-9]+)/ ? $1 : 1e6 }
 
 sub find
 {
     my ($self, $args) = @_;
 
     my @deals = @{$args->{deals}};
-    my $scan = $args->{scan};
+    my $scan_arg = $args->{scan};
+
+    my @scans = ((ref($scan_arg) eq '') ? ($scan_arg) : @$scan_arg);
 
     # my @deals = (14249, 10692);
     # my @deals = (14249);
@@ -43,15 +46,23 @@ sub find
 
     my $handle = sub {
         my ($seed) = @_;
-        my @new_line = map { "$seed " . `pi-make-microsoft-freecell-board -t $_ | ../../source/scripts/summarize-fc-solve -- $scan -seed "$seed" -sp r:tf -mi "$iters"` } @deals;
-        my $new = max ( map { f($_) } @new_line);
+
+        my @new_ = (map {
+                my $scan = $_;
+                my @l = map { "$seed " . `pi-make-microsoft-freecell-board -t $_ | ../../source/scripts/summarize-fc-solve -- $scan -seed "$seed" -sp r:tf -mi "$iters"` =~ s/\n+\z//r } @deals;
+                my $v = max ( map { f($_) } @l);
+                [$scan,\@l, $v ]
+            } @scans
+        );
+        my $new_scan = min_by { $_->[2] } @new_;
+        my $new = $new_scan->[2];
 
         if ($new < $iters)
         {
-            @old_line = @new_line;
+            @old_line = @$new_scan;
             $iters = $new;
         }
-        print "$seed @old_line";
+        print "$seed @{$old_line[1]} ; $old_line[0]\n";
 
         return;
     };
