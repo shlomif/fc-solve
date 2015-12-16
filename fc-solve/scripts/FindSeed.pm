01799c9a6d09285a4ed35f5a24e5ed2916509590
Shlomi Fish <shlomif@shlomifish.org>
2015-12-17 00:45:49 +0200

    Start adding the parallelisation.
    Currently crashing.

--- fc-solve/scripts/FindSeed.pm
+++ fc-solve/scripts/FindSeed.pm
@@ -4,6 +4,21 @@ use strict;
 use warnings;
 use autodie;
 
+use Storable qw/nfreeze thaw/;
+use MIME::Base64 qw/encode_base64 decode_base64/;
+
+sub enc
+{
+    my ($item) = @_;
+    return encode_base64(nfreeze($item));
+}
+
+sub dec
+{
+    my ($text) = @_;
+    return thaw(decode_base64($text));
+}
+
 package FindSeed::DealAndSeedResult;
 
 use MooX qw/late/;
@@ -188,6 +203,92 @@ sub run_scans
     return;
 }
 
+sub par_handle_seed
+{
+    my ($iters_agg, $seed) = @_;
+
+    $iters_agg->run_scans(
+        {
+            seed => $seed,
+            scans => FindSeed::Globals->obj->scans,
+            deals => FindSeed::Globals->obj->deals,
+            MAX_TH => FindSeed::Globals->obj->max_th,
+        }
+    );
+
+    return;
+}
+
+{ # This is the instance held by the parent process for communicating with the child
+	package FindSeed::Parallel::Child;
+	use parent qw(Process::Async::Child);
+	use POSIX qw(strftime);
+
+	sub finished { $_[0]->{finished} ||= $_[0]->loop->new_future }
+
+	sub cmd_done {
+        my $self = shift;
+        my $args = FindSeed::dec(shift);
+
+        if (ref ( $args ) eq 'HASH')
+        {
+            my $start = $args->{'s'};
+            my $end = $args->{'e'};
+            FindSeed::Globals->obj->agg->merge_from($args->{agg});
+
+            if (FindSeed::Globals->obj->remove_from_pq($end))
+            {
+                FindSeed::Globals->obj->agg->output_to_fh(
+                    FindSeed::Globals->obj->max_th,
+                    $end,
+                    \*STDOUT,
+                );
+            }
+        }
+
+        my ($s, $e) = FindSeed::Globals->obj->get_next_range;
+
+        FindSeed::Globals->obj->add_to_pq($e);
+        $self->send_command(calc => FindSeed::enc({s => $s, e => $e, agg => FindSeed::Globals->obj->agg}));
+	}
+}
+
+{ # This is the code which runs in the fork
+	package FindSeed::Parallel::Worker;
+	use parent qw(Process::Async::Worker);
+
+	sub completion { $_[0]->{completion} ||= $_[0]->loop->new_future }
+
+	sub cmd_calc {
+        my $self = shift;
+        my $args = FindSeed::dec(shift);
+
+        my $agg = $args->{agg};
+        my $start = $args->{'s'};
+        my $end = $args->{'e'};
+
+        for my $seed ($start .. $end)
+        {
+            $agg->par_handle_seed($seed);
+        }
+
+        $self->send_command(done => FindSeed::enc($args));
+
+        return;
+	}
+
+	sub run {
+		my ($self, $loop) = @_;
+		$self->debug_printf("Worker started");
+		$self->send_command(done => FindSeed::enc([]))->then(sub {
+			$self->completion
+		})->get;
+		$self->debug_printf("Worker finished");
+
+		# our exitcode
+		return 0;
+	}
+}
 
 package FindSeed;
 
@@ -290,6 +391,167 @@ sub find
     {
         $handle->($seed);
     }
+}
+
+sub parallel_find
+{
+    my ($self, $args) = @_;
+
+    $ENV{PATH} .= ":" . Cwd::getcwd();
+
+    my @deals = @{$args->{deals}};
+    my $scan_arg = $args->{scan};
+
+    my @scans = ((ref($scan_arg) eq '') ? ($scan_arg) : @$scan_arg);
+
+    my $MAX_THRESHOLD = $args->{threshold};
+
+    if ($MAX_THRESHOLD > @scans)
+    {
+        $MAX_THRESHOLD = @scans;
+    }
+
+    my $MAX_TH = $MAX_THRESHOLD - 1;
+
+    # my @deals = (14249, 10692);
+    # my @deals = (14249);
+
+    my $first_seed = $ENV{FIRST_SEED} || 1;
+    my $check_seed__str = $ENV{CHECK_SEED} || $first_seed;
+
+    my @check_seeds = split/\s+/, $check_seed__str;
+
+    {
+        my $prev = shift(@ARGV);
+        if (defined($prev))
+        {
+            my $seeds_str;
+
+            if (($first_seed, $seeds_str) = ($prev =~ /\ASUMMARY\[([0-9]+)\] = ((?:\[[0-9]+=[0-9]+\@seed=[0-9]+\]\s*)+)\z/))
+            {
+                @check_seeds = map { s#\Aseed=##r } ($seeds_str =~ /(seed=[0-9]+)/g);
+            }
+            else
+            {
+                die "Argument is malformatted.";
+            }
+        }
+    }
+
+    if (@check_seeds != $MAX_THRESHOLD)
+    {
+        die "Number of checked seeds is wrong.";
+    }
+
+    my $LAST_SEED = ((1 << 31)-1);
+    my $iters_agg = FindSeed::ThresholdAgg->from_lines(
+        {
+            seed => -1,
+            scan => '',
+            lines => [ map { "$_ = " } @deals],
+        }
+    );
+
+    my $old_line;
+
+    # 4086 923 Verdict: Solved ; Iters: 170 ; Length: 142
+    # my $scan = q#--method random-dfs -to "01[2345789]"#;
+    # 2463 694 Verdict: Solved ; Iters: 283 ; Length: 122
+    # my $scan = q#--method random-dfs -to "01[234579]"#;
+    # my $scan = q#--method random-dfs -to "01[234579]"#;
+
+    FindSeed::Globals->obj->scans(\@scans);
+    FindSeed::Globals->obj->deals(\@deals);
+    FindSeed::Globals->obj->max_th($MAX_TH);
+    FindSeed::Globals->obj->agg($iters_agg);
+    FindSeed::Globals->obj->_next_seed($first_seed);
+
+    for my $seed (@check_seeds)
+    {
+        $iters_agg->par_handle_seed($seed);
+        STDERR->print("Handled seed '$seed'.\n");
+    }
+
+    use Process::Async;
+    use IO::Async::Loop;
+
+    my $NUM_JOBS = $ENV{NUM_JOBS} || 4;
+
+    my $loop = IO::Async::Loop->new;
+
+    my @children;
+    for my $job (1 .. $NUM_JOBS)
+    {
+        $loop->add(
+            my $pm = Process::Async::Manager->new(
+                worker => 'FindSeed::Parallel::Worker',
+                child => 'FindSeed::Parallel::Child',
+            )
+        );
+        push @children, $pm->spawn;
+    }
+    $children[0]->finished->get;
+
+    return;
+}
+
+1;
+
+package FindSeed::Globals;
+
+use MooX qw/late/;
+
+has 'agg' => (is => 'rw');
+has 'scans' => (is => 'rw');
+has 'deals' => (is => 'rw');
+has 'max_th' => (is => 'rw');
+has '_next_seed' => (is => 'rw');
+has 'range_size' => (is => 'rw', default => sub { return 10; });
+has 'pq' => (is => 'rw', default => sub { return []; });
+
+# A singleton.
+my $obj = FindSeed::Globals->new;
+
+sub obj
+{
+    return $obj;
+}
+
+sub get_next_range
+{
+    my ($self) = @_;
+
+    my $start = $self->_next_seed;
+
+    my $end = $start + $self->range_size;
+
+    $self->_next_seed($end);
+
+    return ($start, $end-1);
+}
+
+sub remove_from_pq
+{
+    my ($self, $pivot) = @_;
+
+    my $ret = ($self->pq->[0] == $pivot);
+
+    $self->pq(
+        [ grep {$_ != $pivot } @{$self->pq}]
+    );
+
+    return $ret;
+}
+
+sub add_to_pq
+{
+    my ($self, $pivot) = @_;
+
+    $self->pq(
+        [ sort {$a <=> $b} @{$self->pq}, $pivot]
+    );
+
+    return;
 }
 
 1;
---
+++ fc-solve/scripts/TEST_OPTIMIZATIONS/find-min-seed-for-as-10-scans--parallel.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindSeed ();
+
+my $x = <<'EOF';
+EOF
+
+my @deals = (<<'EOF' =~ /^ *([0-9]+)/gms);
+   617	7992
+ 19677	8037
+ 13583	8166
+ 16768	8362
+ 26557	8468
+   718	8484
+ 30887	8580
+ 12421	8702
+ 16837	8736
+ 28920	8738
+ 30898	8771
+ 13765	8806
+  8858	10113
+ 13304	10165
+ 25599	10565
+ 27188	10573
+ 25315	10604
+  2288	10870
+  6182	10975
+  9182	12110
+ 17323	12435
+ 24106	13454
+ 17355	13959
+ 15592	14758
+ 31302	15413
+  1941	19934
+EOF
+
+my @scans =
+(
+    q#--method random-dfs -to "01[2345789]"#,
+    q#--method random-dfs -to "01[234579]"#,
+    q#--method random-dfs -to "01[234589]"#,
+    q#--method random-dfs -to "01[234567]" -dto2 "6,0132[456789]"#,
+    q#--method random-dfs -to "01[234567]" -dto2 "5,01[2345789]"#,
+    q#--method random-dfs -to "[01][23457]"#,
+    q#--method random-dfs -to "[0123457]"#,
+    q#--method random-dfs -to "[01][23457]" -dto2 "7,[0123][456789]"#,
+    q#--method random-dfs -to "[01][23457]" -dto2 "7,[0123][4567]"#,
+    q#--method random-dfs -to "[01][23457]" -dto2 "5,[0123][4567]"#,
+    q#--method random-dfs -to "[01][23457]" -dto2 "10,[0123][4567]"#,
+    q#--method random-dfs -to "[01][23457]" -dto2 "8,[0123][4567]"#,
+);
+
+FindSeed->parallel_find(
+    {
+        scan => \@scans,
+        deals => \@deals,
+        threshold => 10,
+    },
+);
