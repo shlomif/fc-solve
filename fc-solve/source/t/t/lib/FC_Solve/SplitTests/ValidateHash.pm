d3336f8884fac1e474810069523fe1e33eeaacc7
Shlomi Fish <shlomif@shlomifish.org>
2016-01-26 16:01:45 +0200

    validate the hash for duplicate keys.

---
+++ fc-solve/source/t/t/lib/FC_Solve/SplitTests/ValidateHash.pm
@@ -0,0 +1,35 @@
+package FC_Solve::SplitTests::ValidateHash;
+
+use strict;
+use warnings;
+
+use Carp qw/confess/;
+
+use parent 'Test::Data::Split::Backend::Hash';
+
+sub populate
+{
+    my ($self, $array_ref) = @_;
+
+    my $l = @$array_ref;
+
+    my $tests = $self->get_hash;
+
+    if ($l & 0x1)
+    {
+        confess("Input length is not even.");
+    }
+    foreach my $i (grep { $_ & 0x1 == 0 } 0 .. $l)
+    {
+        my ($key, $val) = @$array_ref[$i,$i+1];
+        if (exists($tests->{$key}))
+        {
+            confess("Duplicate key '$key'!");
+        }
+        $tests->{$key} = $val;
+    }
+
+    return;
+}
+
+1;
