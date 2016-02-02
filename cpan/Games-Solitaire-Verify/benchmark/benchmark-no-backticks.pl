b7da41263234a4d16a914addb5745cfbaa4b1510
Shlomi Fish <shlomif@shlomifish.org>
2016-02-02 17:58:17 +0200

    cleanups.

--- cpan/Games-Solitaire-Verify/benchmark/benchmark-no-backticks.pl
+++ cpan/Games-Solitaire-Verify/benchmark/benchmark-no-backticks.pl
@@ -2,32 +2,16 @@
 
 use strict;
 use warnings;
+use autodie;
 
 use lib '../Games-Solitaire-Verify/lib';
 
 use FC_Solve::Solve (qw/fc_solve_init solve/);
 
-use autodie;
-
 use List::MoreUtils qw(first_index true);
 
 use Games::Solitaire::Verify::Solution;
 use Games::Solitaire::Verify::App::CmdLine;
-
-if ($ARGV[0] =~ m{\A-h|--help\z})
-{
-    print <<'EOF';
-summarize-fc-solve [Game Params] -- [Extra fc-solve Args]
-
-Display the solution status, the iterations count and the solution length
-of the Freecell Solver invocation specified by the arguments. Also verifies
-that the solution is true.
-
-The [Game Params] flags (e.g: -g , --freecells-num, --stacks-num, etc.) are also
-used to determine the variant used by the solution verifier.
-EOF
-    exit (0);
-}
 
 my @args = @ARGV;
 
