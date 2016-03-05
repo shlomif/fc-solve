c1329715aa20a54fe20096bba7dd5f777ac844ce
Shlomi Fish <shlomif@shlomifish.org>
2016-03-05 19:34:35 +0200

    Generate a ninja build

--- cpan/Games-Solitaire-Verify/benchmark/gen-par-mak.pl
+++ cpan/Games-Solitaire-Verify/benchmark/gen-par-mak.pl
@@ -10,9 +10,17 @@ my $MIN = 1;
 my $MAX = 32_000;
 my $STEP = 100;
 
+my $ARGS = +{
+    min => $MIN,
+    max => ($MAX / $STEP),
+    step => $STEP,
+    res => sub {
+        return "Results/" . shift . ".res";
+    },
+};
+
 my $template = Template->new(
     {
-            INTERPOLATE  => 1,               # expand "$var" in plain text
             EVAL_PERL    => 1,               # evaluate Perl code blocks
     }
 );
@@ -27,14 +35,23 @@ all:[% FOREACH i = [min .. max] %] [% res(i) %][% END %]
 EOF
 
 $template->process(\$TEXT,
-    {
-        min => $MIN,
-        max => ($MAX / $STEP),
-        step => $STEP,
-        res => sub {
-            return "Results/" . shift . ".res";
-        },
-    },
+    $ARGS,
     'par2.mak',
+) or die $template->error;
+
+my $NINJA_TEXT = <<'EOF';
+rule b
+  command = F=$f L=$l perl benchmark-no-backticks.pl -- -l as -ni -l fg > $out
+
+[% FOREACH i = [min .. max] %]
+build [% res(i) %]: b
+  f=[% min + (i - 1) * step %]
+  l=[% min + i * step - 1 %]
+[% END %]
+EOF
+
+$template->process(\$NINJA_TEXT,
+    $ARGS,
+    'build.ninja',
 ) or die $template->error;
 
