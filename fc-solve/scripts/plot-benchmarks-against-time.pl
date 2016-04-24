0e4b650445ec042262862c771b7d965d94f22941
Shlomi Fish <shlomif@shlomifish.org>
2016-04-24 21:37:08 +0300

    Enhance plot-benchmarks w rate+headers.

--- fc-solve/scripts/plot-benchmarks-against-time.pl
+++ fc-solve/scripts/plot-benchmarks-against-time.pl
@@ -11,11 +11,12 @@ my $repository = Git::Repository->new();
 # Get the git blame information.
 my $blame_lines = $repository->blame( 'fc-solve/benchmarks/shlomif-core-i3-desktop-machine.txt' );
 
+print "Epoch time\tBenchmark duration\tDeals Per Second\n";
 foreach my $line (@$blame_lines)
 {
     if (my ($timing) = $line->get_line =~ /\A([0-9]+\.[0-9]+)s\z/)
     {
-        printf "%d\t%s\n", $line->get_commit_attributes->{'author-time'}, $timing;
+        printf "%d\t%s\t%f\n", $line->get_commit_attributes->{'author-time'}, $timing, (32_000/$timing);
     }
 }
 
