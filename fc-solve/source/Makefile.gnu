dac005b4c5ab5bab0ccab8dca2a59122ab120c78
Shlomi Fish <shlomif@shlomifish.org>
2016-04-24 17:17:57 +0300

    Revert "Revert "Optimization: moved hash_insert and inlined it.""
    This reverts commit 8a3b4961c02bb384f8d356513ec285f40d629ac2.

--- fc-solve/source/Makefile.gnu
+++ fc-solve/source/Makefile.gnu
@@ -188,7 +188,6 @@ OBJECTS :=                     \
           card.o              \
           check_and_add_state.o \
           cmd_line.o          \
-          fcs_hash.o          \
           freecell.o          \
           instance.o          \
           lib.o               \
--- fc-solve/source/check_and_add_state.c
+++ fc-solve/source/check_and_add_state.c
@@ -37,6 +37,201 @@
 
 #include "move_stack_compact_alloc.h"
 
+#if ((FCS_STATE_STORAGE == FCS_STATE_STORAGE_INTERNAL_HASH) || (FCS_STACK_STORAGE == FCS_STATE_STORAGE_INTERNAL_HASH))
+/*
+    This function "rehashes" a hash. I.e: it increases the size of its
+    hash table, allowing for smaller chains, and faster lookup.
+  */
+static GCC_INLINE void fc_solve_hash_rehash(
+    fc_solve_hash_t * const hash
+    )
+{
+    const int old_size = hash->size;
+
+    const int new_size = old_size << 1;
+
+    /* Check for overflow. */
+    if (new_size < old_size)
+    {
+        hash->max_num_elems_before_resize = FCS_INT_LIMIT_MAX;
+        return;
+    }
+
+    const int new_size_bitmask = new_size - 1;
+
+    fc_solve_hash_symlink_t * const new_entries = calloc(new_size, sizeof(new_entries[0]));
+
+    /* Copy the items to the new hash while not allocating them again */
+    for (int i=0 ; i < old_size ; i++)
+    {
+        fc_solve_hash_symlink_item_t * item = hash->entries[i].first_item;
+        /* traverse the chain item by item */
+        while(item != NULL)
+        {
+            /* The place in the new hash table */
+            const int place = item->hash_value & new_size_bitmask;
+
+            /* Store the next item in the linked list in a safe place,
+               so we can retrieve it after the assignment */
+            fc_solve_hash_symlink_item_t * const next_item = item->next;
+            /* It is placed in front of the first element in the chain,
+               so it should link to it */
+            item->next = new_entries[place].first_item;
+
+            /* Make it the first item in its chain */
+            new_entries[place].first_item = item;
+
+            /* Move to the next item this one. */
+            item = next_item;
+        }
+    };
+
+    /* Free the entries of the old hash */
+    free(hash->entries);
+
+    /* Copy the new hash to the old one */
+    hash->entries = new_entries;
+    hash->size = new_size;
+    hash->size_bitmask = new_size_bitmask;
+    fcs_hash_set_max_num_elems(hash, new_size);
+}
+
+/*
+ * Returns NULL if the key is new and the key/val pair was inserted.
+ * Returns the existing key if the key is not new (= a truthy pointer).
+ */
+static GCC_INLINE void * const fc_solve_hash_insert(
+    fc_solve_hash_t * const hash,
+    void * const key,
+#ifdef FCS_RCS_STATES
+    void * const key_id,
+#endif
+    const fc_solve_hash_value_t hash_value
+#ifdef FCS_ENABLE_SECONDARY_HASH_VALUE
+    , const fc_solve_hash_value_t secondary_hash_value
+#endif
+    )
+{
+#if defined(FCS_INLINED_HASH_COMPARISON) && defined(INDIRECT_STACK_STATES)
+    const_SLOT(hash_type, hash);
+#endif
+    /* Get the index of the appropriate chain in the hash table */
+#define PLACE() (hash_value & (hash->size_bitmask))
+
+    typeof(hash->entries[0]) * const list = (hash->entries + PLACE());
+
+#undef PLACE
+
+    fc_solve_hash_symlink_item_t * * item_placeholder;
+    /* If first_item is non-existent */
+    if (list->first_item == NULL)
+    {
+        /* Allocate a first item with that key/val pair */
+        item_placeholder = &(list->first_item);
+    }
+    else
+    {
+        /* Initialize item to the chain's first_item */
+        fc_solve_hash_symlink_item_t * item = list->first_item;
+        fc_solve_hash_symlink_item_t * last_item = NULL;
+
+#ifdef FCS_WITH_CONTEXT_VARIABLE
+#define MY_HASH_CONTEXT_VAR    , hash->context
+#else
+#define MY_HASH_CONTEXT_VAR
+#endif
+
+/*
+ * MY_HASH_COMPARE_PROTO() returns -1/0/+1 depending on the compared
+ * states order. We need to negate it for the desired condition of equality.
+ * */
+#define MY_HASH_COMPARE() (! MY_HASH_COMPARE_PROTO())
+
+/* Define MY_HASH_COMPARE_PROTO() */
+#if defined(FCS_RCS_STATES)
+
+#define MY_HASH_COMPARE_PROTO() (fc_solve_state_compare(key_id, fc_solve_lookup_state_key_from_val(hash->instance, item->key)))
+
+#elif !defined(INDIRECT_STACK_STATES)
+
+#define MY_HASH_COMPARE_PROTO() (fc_solve_state_compare(item->key, key))
+
+#elif defined(FCS_INLINED_HASH_COMPARISON)
+
+#define MY_HASH_COMPARE_PROTO() \
+        ((hash_type == FCS_INLINED_HASH__COLUMNS) \
+            ? fc_solve_stack_compare_for_comparison(item->key, key) \
+            : fc_solve_state_compare(item->key, key) \
+        )
+
+#else
+
+#define MY_HASH_COMPARE_PROTO() (hash->compare_function(item->key, key MY_HASH_CONTEXT_VAR))
+
+#endif
+/* End of MY_HASH_COMPARE_PROTO() */
+
+        while (item != NULL)
+        {
+            /*
+                We first compare the hash values, because it is faster than
+                comparing the entire data structure.
+            */
+            if (
+                (item->hash_value == hash_value)
+#ifdef FCS_ENABLE_SECONDARY_HASH_VALUE
+                && (item->secondary_hash_value == secondary_hash_value)
+#endif
+                && MY_HASH_COMPARE()
+               )
+            {
+                return item->key;
+            }
+            /* Cache the item before the current in last_item */
+            last_item = item;
+            /* Move to the next item */
+            item = item->next;
+        }
+
+        item_placeholder = &(last_item->next);
+    }
+
+#define ITEM_ALLOC() fcs_compact_alloc_ptr(&(hash->allocator), sizeof(*item))
+#ifdef FCS_WITHOUT_TRIM_MAX_STORED_STATES
+    fc_solve_hash_symlink_item_t * const item = ITEM_ALLOC();
+#else
+    fc_solve_hash_symlink_item_t * item;
+
+    if ((item = hash->list_of_vacant_items))
+    {
+        hash->list_of_vacant_items = item->next;
+    }
+    else
+    {
+        item = ITEM_ALLOC();
+    }
+#endif
+
+    *(*(item_placeholder) = item) =
+    (typeof(*item)){
+        .key = key,
+        .hash_value = hash_value,
+        .next = NULL,
+#ifdef FCS_ENABLE_SECONDARY_HASH_VALUE
+        .secondary_hash_value = secondary_hash_value,
+#endif
+    };
+
+    if ((++(hash->num_elems)) > hash->max_num_elems_before_resize)
+    {
+        fc_solve_hash_rehash(hash);
+    }
+
+    return NULL;
+}
+
+#endif
+
 typedef  unsigned long  int  ub4;   /* unsigned 4-byte quantities */
 typedef  unsigned       char ub1;
 
