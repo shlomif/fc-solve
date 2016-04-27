e9daa162d670102457b41a5a462ef9d7fc4e2dea
Shlomi Fish <shlomif@shlomifish.org>
2016-04-27 20:42:36 +0300

    pep8 cleanup of by-depth-tests-order.

--- fc-solve/source/t/t/by-depth-tests-order.py
+++ fc-solve/source/t/t/by-depth-tests-order.py
@@ -6,23 +6,23 @@ from FC_Solve import FC_Solve
 
 plan(7)
 
+
 def test_1():
     testname = "test_1"
 
     fcs = FC_Solve()
 
     # TEST*$input_cmd_line
-    fcs.input_cmd_line("dto 1",
-        ["-to", "0123456789",
-            "-dto", "1,0132456789",
-        ]
+    fcs.input_cmd_line(
+        "dto 1",
+        ["-to", "0123456789", "-dto", "1,0132456789"]
     )
 
     # TEST
     fcs.num_by_depth_tests_order_is(testname, 2)
 
     # TEST
-    fcs.by_depth_max_depth_of_depth_idx_is(testname, 0, 1);
+    fcs.by_depth_max_depth_of_depth_idx_is(testname, 0, 1)
 
 
 def test_2():
@@ -31,30 +31,26 @@ def test_2():
     fcs = FC_Solve()
 
     # TEST*$input_cmd_line
-    fcs.input_cmd_line("dto 1",
-        ["-to", "0123456789",
-            "-dto", "1,0132456789",
-            "-dto", "50,056234",
-        ]
+    fcs.input_cmd_line(
+        "dto 1",
+        ["-to", "0123456789", "-dto", "1,0132456789", "-dto", "50,056234"]
     )
 
     # TEST
     fcs.num_by_depth_tests_order_is(testname, 3)
 
     # TEST
-    fcs.by_depth_max_depth_of_depth_idx_is(testname, 0, 1);
+    fcs.by_depth_max_depth_of_depth_idx_is(testname, 0, 1)
 
     # TEST
-    fcs.by_depth_max_depth_of_depth_idx_is(testname, 1, 50);
+    fcs.by_depth_max_depth_of_depth_idx_is(testname, 1, 50)
+
 
 def main():
-
     test_1()
-
     test_2()
 
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 if __name__ == "__main__":
     main()
-
