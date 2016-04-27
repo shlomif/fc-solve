1b0231821a7610c00242b73ed822cbbb7ce1b6ef
Shlomi Fish <shlomif@shlomifish.org>
2016-04-27 20:53:04 +0300

    pep8 for set-befs-weights.py

--- fc-solve/source/t/t/set-befs-weights.py
+++ fc-solve/source/t/t/set-befs-weights.py
@@ -7,30 +7,35 @@ from FC_Solve import FC_Solve
 # Should be 24.
 plan(24)
 
+
 def main():
     fcs = FC_Solve()
 
     # TEST*$test_befs
-    fcs.test_befs_weights("Simple - all integers",
-            "5,4,3,0,2",
-            [5.0, 4.0, 3.0, 0.0, 2.0])
+    fcs.test_befs_weights(
+        "Simple - all integers",
+        "5,4,3,0,2",
+        [5.0, 4.0, 3.0, 0.0, 2.0])
 
     # TEST*$test_befs
-    fcs.test_befs_weights("Simple - fractions",
-            "0.2,0.3,0.4,0.5,0.6",
-            [0.2,0.3,0.4,0.5,0.6])
+    fcs.test_befs_weights(
+        "Simple - fractions",
+        "0.2,0.3,0.4,0.5,0.6",
+        [0.2, 0.3, 0.4, 0.5, 0.6])
 
     # TEST*$test_befs
-    fcs.test_befs_weights("Testing end after three numbers",
-            "0.2,0.35,0.4\x00500\x00600.7\x00",
-            [0.2,0.35,0.4,0,0])
+    fcs.test_befs_weights(
+        "Testing end after three numbers",
+        "0.2,0.35,0.4\x00500\x00600.7\x00",
+        [0.2, 0.35, 0.4, 0, 0])
 
     # TEST*$test_befs
-    fcs.test_befs_weights("One number with a trailing comma",
-            "100,\x002,3,4,5,6,7,8,9,100\x00",
-            [100,0,0,0,0])
+    fcs.test_befs_weights(
+        "One number with a trailing comma",
+        "100,\x002,3,4,5,6,7,8,9,100\x00",
+        [100, 0, 0, 0, 0])
 
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 if __name__ == "__main__":
     main()
