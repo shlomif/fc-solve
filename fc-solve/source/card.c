c1ba7bbbf4846f597f1c21fc6916db9d140448f2
Shlomi Fish <shlomif@shlomifish.org>
2016-06-29 23:16:54 +0300

    More formatting.

--- fc-solve/source/card.c
+++ fc-solve/source/card.c
@@ -43,51 +43,51 @@ DEFINE_fc_solve_empty_card();
  * the program.
  * */
 
-int fc_solve_u2p_rank(const char * string)
+int fc_solve_u2p_rank(const char *string)
 {
     while (1)
     {
         switch (FC_SOLVE_MAP_CHAR(*string))
         {
-            case '\0':
-            case ' ':
-            case '\t':
-                return 0;
-            case 'A':
-                return 1;
-            case 'J':
-                return 11;
-            case 'Q':
-                return 12;
-            case 'K':
-                return 13;
+        case '\0':
+        case ' ':
+        case '\t':
+            return 0;
+        case 'A':
+            return 1;
+        case 'J':
+            return 11;
+        case 'Q':
+            return 12;
+        case 'K':
+            return 13;
 #ifndef FC_SOLVE__STRICTER_BOARD_PARSING
-            case '1':
-                return (string[1] == '0')?10:1;
+        case '1':
+            return (string[1] == '0') ? 10 : 1;
 #endif
-            case 'T':
+        case 'T':
 #ifndef FC_SOLVE__STRICTER_BOARD_PARSING
-            case '0':
+        case '0':
 #endif
-                return 10;
-            case '2':
-                return 2;
-            case '3':
-                return 3;
-            case '4':
-                return 4;
-            case '5':
-                return 5;
-            case '6':
-                return 6;
-            case '7':
-                return 7;
-            case '8':
-                return 8;
-            case '9':
-                return 9;
-            default:
-                string++;
+            return 10;
+        case '2':
+            return 2;
+        case '3':
+            return 3;
+        case '4':
+            return 4;
+        case '5':
+            return 5;
+        case '6':
+            return 6;
+        case '7':
+            return 7;
+        case '8':
+            return 8;
+        case '9':
+            return 9;
+        default:
+            string++;
         }
     }
 }
@@ -96,10 +96,7 @@ int fc_solve_u2p_rank(const char * string)
  * Converts a suit to its user representation.
  *
  * */
-static GCC_INLINE void fc_solve_p2u_suit(
-        const int suit,
-        char * str
-        )
+static GCC_INLINE void fc_solve_p2u_suit(const int suit, char *str)
 {
     *(str++) = "HCDS"[suit];
     *(str) = '\0';
@@ -109,17 +106,9 @@ static GCC_INLINE void fc_solve_p2u_suit(
  * Convert an entire card to its user representation.
  *
  * */
-void fc_solve_card_perl2user(const fcs_card_t card, char * const str
-    PASS_T(const fcs_bool_t t)
-)
+void fc_solve_card_perl2user(const fcs_card_t card,
+                             char *const str PASS_T(const fcs_bool_t t))
 {
-    fc_solve_p2u_rank(
-        fcs_card_rank(card),
-        str
-        PASS_T(t)
-        );
-    fc_solve_p2u_suit(
-        fcs_card_suit(card),
-        str+strlen(str)
-        );
+    fc_solve_p2u_rank(fcs_card_rank(card), str PASS_T(t));
+    fc_solve_p2u_suit(fcs_card_suit(card), str + strlen(str));
 }
