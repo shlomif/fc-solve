92eaacc4a2d4c49708078af653ebdf1f078b9c5e
Shlomi Fish <shlomif@shlomifish.org>
2016-07-20 20:55:39 +0300

    Rename for coding standards.

--- fc-solve/source/pruner-main.c
+++ fc-solve/source/pruner-main.c
@@ -30,7 +30,7 @@ static void my_iter_handler(void *const user_instance,
     const fcs_int_limit_t iter_num, const int depth, void *const ptr_state,
     const fcs_int_limit_t parent_iter_num, void *const lp_context)
 {
-    fc_solve_display_information_context_t *const dc =
+    fc_solve_display_information_context_t *const display_context =
         (fc_solve_display_information_context_t *)lp_context;
 
     fprintf(stdout, "Iteration: %li\n", (long)iter_num);
@@ -40,13 +40,13 @@ static void my_iter_handler(void *const user_instance,
                 user_instance));
     fprintf(stdout, "Scan: %s\n",
         freecell_solver_user_get_current_soft_thread_name(user_instance));
-    if (dc->display_parent_iter_num)
+    if (display_context->display_parent_iter_num)
     {
         fprintf(stdout, "Parent Iteration: %li\n", (long)parent_iter_num);
     }
     fprintf(stdout, "\n");
 
-    if (dc->debug_iter_state_output)
+    if (display_context->debug_iter_state_output)
     {
         char state_string[1000];
         freecell_solver_user_iter_state_stringify(user_instance, state_string,
