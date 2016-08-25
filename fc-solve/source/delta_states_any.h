9f102851b6f27dbacc812fe5bc6f5408cdbec1d4
Shlomi Fish <shlomif@shlomifish.org>
2016-08-25 22:38:11 +0300

    Extract common code into a header.
    compare_enc_states().

--- fc-solve/source/delta_states_any.h
+++ fc-solve/source/delta_states_any.h
@@ -66,6 +66,38 @@ static GCC_INLINE void fc_solve_delta_stater_init(
     fc_solve_debondt_delta_stater_release(a)
 #endif
 
+/****************************************************/
+/* compare_enc_states */
+
+#ifdef FCS_DEBONDT_DELTA_STATES
+
+static GCC_INLINE int compare_enc_states(
+    const fcs_encoded_state_buffer_t *a, const fcs_encoded_state_buffer_t *b)
+{
+    return memcmp(a, b, sizeof(*a));
+}
+
+#else
+
+static GCC_INLINE int compare_enc_states(
+    const fcs_encoded_state_buffer_t *a, const fcs_encoded_state_buffer_t *b)
+{
+    if (a->s[0] < b->s[0])
+    {
+        return -1;
+    }
+    else if (a->s[0] > b->s[0])
+    {
+        return 1;
+    }
+    else
+    {
+        return memcmp(a->s, b->s, a->s[0] + 1);
+    }
+}
+
+#endif
+
 #ifdef __cplusplus
 }
 #endif
