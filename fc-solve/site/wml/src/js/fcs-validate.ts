9507ee29236c7e9e28fb0fb830222b6f291edb3f
Shlomi Fish <shlomif@shlomifish.org>
2016-08-29 21:31:18 +0300

    Add more tests and added the missing src file.

---
+++ fc-solve/site/wml/src/js/fcs-validate.ts
@@ -0,0 +1,69 @@
+// Adapted from http://www.inventpartners.com/javascript_is_int - thanks.
+function is_int(input: number): boolean {
+    var value:string = "" + input;
+    if((parseFloat(value) == parseInt(value)) && !isNaN(input)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+class Card {
+    private rank: number;
+    private suit: number;
+
+    constructor(rank: number, suit: number) {
+        if (! is_int(rank)) {
+            throw "rank is not an integer.";
+        }
+        if (! is_int(suit)) {
+            throw "suit is not an integer.";
+        }
+        if (rank < 1) {
+            throw "rank is too low.";
+        }
+        if (rank > 13) {
+            throw "rank is too high.";
+        }
+        if (suit < 0) {
+            throw "suit is negative.";
+        }
+        if (suit > 3) {
+            throw "suit is too high.";
+        }
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    getRank(): number {
+        return this.rank;
+    }
+
+    getSuit(): number {
+        return this.suit;
+    }
+}
+
+var _ranks__int_to_str:string = "0A23456789TJQK";
+var _ranks__str_to_int = {};
+function _perl_range(start: number, end: number): Array<number> {
+    var ret: Array<number> = [];
+
+    for (var i = start; i <= end; i++) {
+        ret.push(i);
+    }
+
+    return ret;
+}
+
+_perl_range(1,13).forEach(function (rank) {
+    _ranks__str_to_int[_ranks__int_to_str.substring(rank, rank+1)] = rank;
+});
+
+function fcs_js__card_from_string(s: string): Card {
+    var m = s.match(/^([A23456789TJQK])([HCDS])$/);
+    if (! m) {
+        throw "Invalid format for a card - \"" + s + "\"";
+    }
+    return new Card(_ranks__str_to_int[m[1]], 0);
+}
--- fc-solve/site/wml/src/js/web-fc-solve-tests.js
+++ fc-solve/site/wml/src/js/web-fc-solve-tests.js
@@ -10204,7 +10204,7 @@ function test_js_fc_solve_class()
             "get_num_freecells() returns 2 after command line.");
     });
     test("verify_state Card class tests", function() {
-        expect(2);
+        expect(4);
 
         {
             var c = fcs_js__card_from_string('TH');
@@ -10214,6 +10214,16 @@ function test_js_fc_solve_class()
 
             // TEST
             equal(c.getSuit(), 0, "Card(TH).getSuit() is fine.");
+        }
+
+        {
+            var c = fcs_js__card_from_string('AH');
+
+            // TEST
+            equal(c.getRank(), 1, "Card(AH).getRank() is fine.");
+
+            // TEST
+            equal(c.getSuit(), 0, "Card(AH).getSuit() is fine.");
         }
     });
 
