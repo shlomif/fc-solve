790b82dd1d3161537fa2d48c878a443b3bb14fc9
Shlomi Fish <shlomif@shlomifish.org>
2016-09-03 05:33:39 +0300

    Optimisation: consolidate calls.

--- fc-solve/source/iter_handler_base.h
+++ fc-solve/source/iter_handler_base.h
@@ -3,16 +3,14 @@ static GCC_INLINE void my_iter_handler_base(const fcs_int_limit_t iter_num,
     const fc_solve_display_information_context_t *const display_context,
     const fcs_int_limit_t parent_iter_num)
 {
-    fprintf(stdout, "Iteration: %li\n", (long)iter_num);
-    fprintf(stdout, "Depth: %i\n", depth);
-    fprintf(stdout, "Stored-States: %li\n",
+    printf("Iteration: %li\nDepth: %i\nStored-States: %li\nScan: %s\n",
+        (long)iter_num, depth,
         (long)freecell_solver_user_get_num_states_in_collection_long(
-                user_instance));
-    fprintf(stdout, "Scan: %s\n",
+               user_instance),
         freecell_solver_user_get_current_soft_thread_name(user_instance));
     if (display_context->display_parent_iter_num)
     {
-        fprintf(stdout, "Parent Iteration: %li\n", (long)parent_iter_num);
+        printf("Parent Iteration: %li\n", (long)parent_iter_num);
     }
-    fprintf(stdout, "\n");
+    putchar('\n');
 }
