8c3331328284861aee995d52ab3b7049eaa67650
Shlomi Fish <shlomif@shlomifish.org>
2016-09-03 05:26:46 +0300

    Refactoring: extract a method.

--- fc-solve/source/pruner-main.c
+++ fc-solve/source/pruner-main.c
@@ -25,27 +25,16 @@
  * pruner-main.c : pruning solver.
  */
 #include "cl_callback.h"
+#include "iter_handler_base.h"
 
 static void my_iter_handler(void *const user_instance,
     const fcs_int_limit_t iter_num, const int depth, void *const ptr_state,
     const fcs_int_limit_t parent_iter_num, void *const lp_context)
 {
-    fc_solve_display_information_context_t *const display_context =
-        (fc_solve_display_information_context_t *)lp_context;
-
-    fprintf(stdout, "Iteration: %li\n", (long)iter_num);
-    fprintf(stdout, "Depth: %i\n", depth);
-    fprintf(stdout, "Stored-States: %li\n",
-        (long)freecell_solver_user_get_num_states_in_collection_long(
-                user_instance));
-    fprintf(stdout, "Scan: %s\n",
-        freecell_solver_user_get_current_soft_thread_name(user_instance));
-    if (display_context->display_parent_iter_num)
-    {
-        fprintf(stdout, "Parent Iteration: %li\n", (long)parent_iter_num);
-    }
-    fprintf(stdout, "\n");
-
+    const fc_solve_display_information_context_t *const display_context =
+        (const fc_solve_display_information_context_t *const)lp_context;
+    my_iter_handler_base(
+        iter_num, depth, user_instance, display_context, parent_iter_num);
     if (display_context->debug_iter_state_output)
     {
         char state_string[1000];
@@ -53,25 +42,6 @@ static void my_iter_handler(void *const user_instance,
             ptr_state FC_SOLVE__PASS_PARSABLE(TRUE),
             FALSE FC_SOLVE__PASS_T(TRUE));
         printf("%s\n---------------\n\n\n", state_string);
-
-        /* Process the string in-place to make it available as input
-         * to fc-solve again.
-         * */
-
-        {
-            char *s, *d;
-            char c;
-            s = d = state_string;
-
-            while ((c = *(d++) = *(s++)))
-            {
-                if ((c == '\n') && (s[0] == ':') && (s[1] = ' '))
-                {
-                    s += 2;
-                }
-            }
-        }
-
         /* Now pass it to a secondary user_instance prune it. */
         {
             void *const pruner = freecell_solver_user_alloc();
