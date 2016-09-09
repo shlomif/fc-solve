81e71b66bca2311b72e96fb6c09814eca24aae17
Shlomi Fish <shlomif@shlomifish.org>
2016-09-09 20:10:27 +0300

    Start adding fcs_js__column_from_string.
    Needs more tests.

--- fc-solve/site/wml/src/js/fcs-validate.ts
+++ fc-solve/site/wml/src/js/fcs-validate.ts
@@ -44,6 +44,30 @@ class Card {
     }
 }
 
+class Column {
+    private cards: Array<Card>;
+
+    constructor(cards: Array<Card>) {
+        this.cards = cards;
+    }
+
+    getLen(): number {
+        return this.cards.length;
+    }
+
+    getCard(idx: number) {
+        var that = this;
+        if (idx < 0) {
+            throw "idx is below zero.";
+        }
+        if (idx >= that.getLen()) {
+            throw "idx exceeds the length of the column.";
+        }
+        return that.cards[idx];
+    }
+}
+
+
 var _ranks__int_to_str:string = "0A23456789TJQK";
 var _ranks__str_to_int = {};
 function _perl_range(start: number, end: number): Array<number> {
@@ -65,11 +89,47 @@ _perl_range(0,3).forEach(function (suit) {
     _suits__str_to_int[_suits__int_to_str.substring(suit, suit+1)] = suit;
 });
 
-
+var card_re:string = '([A23456789TJQK])([HCDS])';
 function fcs_js__card_from_string(s: string): Card {
-    var m = s.match(/^([A23456789TJQK])([HCDS])$/);
+    var m = s.match('^' + card_re + '$');
     if (! m) {
         throw "Invalid format for a card - \"" + s + "\"";
     }
     return new Card(_ranks__str_to_int[m[1]], _suits__str_to_int[m[2]]);
+}
+
+class ColumnParseResult {
+    public col: Column;
+    public is_correct: boolean;
+    public problem_found_at_char_idx: number;
+    public error: string;
+
+    constructor(is_correct: boolean, problem_found_at_char_idx: number, error: string, cards: Array<Card>) {
+        this.is_correct = is_correct;
+        this.problem_found_at_char_idx = problem_found_at_char_idx;
+        this.error = error;
+        this.col = new Column(cards);
+    }
+}
+
+function fcs_js__column_from_string(s: string): ColumnParseResult {
+    var cards:Array<Card> = [];
+    var is_start:boolean = true;
+    var consumed:number = 0;
+    while (s.length > 0) {
+        if (s.match('^ +$')) {
+            break;
+        }
+        var m = s.match('^(' + (is_start ? '' : ' +') + '(' + card_re + ')' + ')');
+        if (! m) {
+            return new ColumnParseResult(false, consumed, 'Wrong card format - should be [Rank][Suit]', []);
+        }
+
+        var len_match = m[1].length;
+        consumed += len_match;
+        cards.push(fcs_js__card_from_string(m[2]));
+        s = s.substring(len_match);
+        is_start = false;
+    }
+    return new ColumnParseResult(true, -1, '', cards);
 }
