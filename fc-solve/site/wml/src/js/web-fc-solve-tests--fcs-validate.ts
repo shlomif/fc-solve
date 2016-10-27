5361f51d96afc25c8a6a96f4e9878303c561b8c1
Shlomi Fish <shlomif@shlomifish.org>
2016-10-27 13:07:22 +0300

    Add tests for trailing newline.

--- fc-solve/site/wml/src/js/web-fc-solve-tests--fcs-validate.ts
+++ fc-solve/site/wml/src/js/web-fc-solve-tests--fcs-validate.ts
@@ -145,7 +145,7 @@ export function test_fcs_validate()
         }
     });
     QUnit.test("verify_state Freecells class tests", function(a: Assert) {
-        a.expect(25);
+        a.expect(30);
 
         {
             var start_char_idx = 10;
@@ -265,6 +265,31 @@ export function test_fcs_validate()
 
             // TEST
             a.equal(result.num_consumed_chars, str.length, "Trailing Empty FC - fc.consumed is right on success.");
+        }
+
+        {
+            const start_char_idx = 200;
+            const str = "Freecells: - JC  - 9H\n";
+            const num_freecells = 4;
+            const test_name = "With trailing newline. ";
+
+            var result = fcs_js__freecells_from_string(num_freecells, start_char_idx, str);
+
+            // TEST
+            a.ok (result.is_correct, test_name + "Column was parsed correctly.");
+
+            // TEST
+            a.equal(result.start_char_idx, start_char_idx, test_name + "start_char_idx is correct.");
+
+            // TEST
+            a.equal(result.getEnd(), start_char_idx + str.length);
+
+            // TEST
+            a.deepEqual(result.freecells.getArrOfStrs(), ['-', 'JC', '-', '9H'],
+                        test_name + "freecell contents is fine.");
+
+            // TEST
+            a.equal(result.num_consumed_chars, str.length, test_name + "fc.consumed is right on success.");
         }
     });
 }
