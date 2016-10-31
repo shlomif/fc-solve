a32e6112dfdb5f254f294bd2c53005bf8985ec23
Shlomi Fish <shlomif@shlomifish.org>
2016-10-31 10:33:51 +0200

    Remove a generated file.

--- fc-solve/source/scripts/gen-cmd-line-radix-tree.pl
+++ fc-solve/source/scripts/gen-cmd-line-radix-tree.pl
@@ -5,6 +5,7 @@ use autodie;
 
 use List::Util qw(first);
 use Data::Dumper;
+use FindBin;
 use Path::Tiny qw/path/;
 
 my %strings_to_opts_map;
@@ -33,7 +34,7 @@ EOF
 my $ws = " " x 4;
 my @enum = ($UNREC);
 
-open my $module, "<", "cmd_line.c";
+open my $module, "<", "$FindBin::Bin/../cmd_line.c";
 SEARCH_FOR_SWITCH: while (my $line = <$module>)
 {
     if ($line =~ m{\A(\s*)/\* OPT-PARSE-START \*/})
@@ -62,29 +63,6 @@ gen_radix_tree();
 
 path($enum_fn)->spew_utf8(
     <<"EOF",
-/* Copyright (c) 2000 Shlomi Fish
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
 /*
  * $enum_fn - the ANSI C enum (= enumeration) for the command line
  * arguments. Partially auto-generated.
