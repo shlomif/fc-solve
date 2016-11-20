f2e5ebeaf4b8245b79b1448c4df525444b6f738e
Shlomi Fish <shlomif@shlomifish.org>
2016-11-20 17:35:52 +0200

    add commas for deals.

--- fc-solve/site/wml/lib/FreecellSolver/GenDealsCharts1.pm
+++ fc-solve/site/wml/lib/FreecellSolver/GenDealsCharts1.pm
@@ -2,15 +2,29 @@ use strict;
 use warnings;
 use IO::All qw/ io /;
 
-foreach my $deal (
+sub format_num
+{
+    my $s = shift;
+    my @d;
+    while (length $s)
+    {
+        unshift @d, substr($s,-3);
+        $s = substr($s,0,-3);
+    }
+    return join",",@d;
+}
+
+foreach my $deal
+(
     grep { /\A[12]/ }
     sort {$a <=> $b }
     map { s#.*/##mrs =~ s#\.dump\.txt\z##mrs }
-    glob("charts/fc-pro--4fc-intractable-deals--report/data/*.dump.txt"))
+    glob("charts/fc-pro--4fc-intractable-deals--report/data/*.dump.txt")
+)
 {
     my $data_id = "queue-items-$deal-data";
     my $chart_id = "queue-items-$deal-chart";
-    print qq#<h4 id="queue-items-$deal">Deal $deal</h4>\n#;
+    print qq#<h4 id="queue-items-$deal">Deal @{[format_num($deal)]}</h4>\n#;
     print <<"EOF";
     <div class="demo-container">
         <div id="$chart_id" class="demo-placeholder"></div>
