de8016b5045ab8bbdd49bfcc3751cac728b2048f
Shlomi Fish <shlomif@shlomifish.org>
2016-11-21 19:40:56 +0200

    extract into a method.

--- fc-solve/site/wml/lib/FreecellSolver/GenDealsCharts1.pm
+++ fc-solve/site/wml/lib/FreecellSolver/GenDealsCharts1.pm
@@ -1,3 +1,5 @@
+package FreecellSolver::GenDealsCharts1;
+
 use strict;
 use warnings;
 use IO::All qw/ io /;
@@ -14,26 +16,29 @@ sub format_num
     return join",",@d;
 }
 
-foreach my $deal
-(
-    sort { $a <=> $b }
-    map { s#.*/##mrs =~ s#\.dump\.txt\z##mrs }
-    glob("charts/fc-pro--4fc-intractable-deals--report/data/*.dump.txt")
-)
+sub gen_progress_charts
 {
-    my $data_id = "queue-items-$deal-data";
-    my $chart_id = "queue-items-$deal-chart";
-    print qq#<h4 id="queue-items-$deal">Deal @{[format_num($deal)]}</h4>\n#;
-    print <<"EOF";
+    foreach my $deal
+    (
+        sort { $a <=> $b }
+        map { s#.*/##mrs =~ s#\.dump\.txt\z##mrs }
+        glob("charts/fc-pro--4fc-intractable-deals--report/data/*.dump.txt")
+    )
+    {
+        my $data_id = "queue-items-$deal-data";
+        my $chart_id = "queue-items-$deal-chart";
+        print qq#<h4 id="queue-items-$deal">Deal @{[format_num($deal)]}</h4>\n#;
+        print <<"EOF";
     <div class="demo-container">
         <div id="$chart_id" class="demo-placeholder"></div>
     </div>
     <script type="text/javascript">\$(function() { chart_data("#$data_id", "#$chart_id"); })</script>
     <br />
 EOF
-    print qq#<textarea id="$data_id" cols="40" rows="20" readonly="readonly" class="fcs_data">\n#;
-    print io->file("../dest/charts/fc-pro--4fc-intractable-deals--report/data/$deal" . ($deal eq '6825625742' ? ".filtered" : '') . ".tsv")->all;
-    print qq#</textarea>\n<br />\n#;
+        print qq#<textarea id="$data_id" cols="40" rows="20" readonly="readonly" class="fcs_data">\n#;
+        print io->file("../dest/charts/fc-pro--4fc-intractable-deals--report/data/$deal" . ($deal eq '6825625742' ? ".filtered" : '') . ".tsv")->all;
+        print qq#</textarea>\n<br />\n#;
+    }
 }
 
 1;
