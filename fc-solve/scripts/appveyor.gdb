dd48688646e23042d7fb34e1187fe984243d2750
Shlomi Fish <shlomif@shlomifish.org>
2016-12-18 20:01:44 +0200

    Revert pure-perl queue - now time for debug.

--- fc-solve/scripts/appveyor.gdb
+++ fc-solve/scripts/appveyor.gdb
@@ -1,6 +1,6 @@
 set breakpoint pending on
 # b freecell_solver_user_alloc
 # r -d:Trace "..\\..\\source\\t\t\\delta-states-perl.t"
-r "..\\..\\source\\t\t\\delta-states-perl.t"
+r "..\\..\\source\\t\t\\depth-based-multi-queue.t"
 bt full
 q
--- fc-solve/source/t/lib/FC_Solve/DepthMultiQueuePrototype.pm
+++ fc-solve/source/t/lib/FC_Solve/DepthMultiQueuePrototype.pm
@@ -2,9 +2,7 @@ package FC_Solve::DepthMultiQueuePrototype;
 
 use strict;
 use warnings;
-
-# TODO : Convert to QueueInC
-use FC_Solve::QueuePrototype;
+use FC_Solve::QueueInC;
 
 use parent 'Games::Solitaire::Verify::Base';
 
@@ -80,7 +78,7 @@ sub _new_queue
 {
     my ($self) = @_;
 
-    return FC_Solve::QueuePrototype->new(
+    return FC_Solve::QueueInC->new(
         {
             num_items_per_page => $self->_num_items_per_page,
             offload_dir_path   => $self->_offload_dir_path,
