92bd6ffd998914514af7a20bb97d960873e1d150
Shlomi Fish <shlomif@shlomifish.org>
2016-12-18 15:57:43 +0200

    Tweak to avoid breakage on windows.

--- fc-solve/source/scripts/perl-remove-trailing-newlines.pl
+++ fc-solve/source/scripts/perl-remove-trailing-newlines.pl
@@ -1,6 +1,38 @@
-#!/usr/bin/perl -0777 -p
+#!/usr/bin/perl
 
 use strict;
 use warnings;
+use autodie;
 
-s{[\n\r]+\z}{};
+use Getopt::Long qw/ GetOptions /;
+
+my $output;
+GetOptions( '-o=s' => \$output, )
+    or die "Failed - $!";
+
+sub _slurp
+{
+    my $filename = shift;
+
+    open my $in, '<', $filename
+        or die "Cannot open '$filename' for slurping - $!";
+
+    local $/;
+    my $contents = <$in>;
+
+    close($in);
+
+    return $contents;
+}
+
+my $t = '';
+foreach my $fn (@ARGV)
+{
+    $t .= _slurp($fn);
+}
+
+$t =~ s{[\n\r]+\z}{};
+
+open my $o, '>', $output;
+print {$o} $t;
+close $o;
