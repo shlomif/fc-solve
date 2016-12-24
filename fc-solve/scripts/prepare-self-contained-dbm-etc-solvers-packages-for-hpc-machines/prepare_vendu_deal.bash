3d75981985b1b275f7efef24e604319ef439ac46
Shlomi Fish <shlomif@shlomifish.org>
2016-12-24 22:52:45 +0200

    The vendu depth-dbm source distro.

---
+++ fc-solve/scripts/prepare-self-contained-dbm-etc-solvers-packages-for-hpc-machines/prepare_vendu_deal.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+deal="$1"
+shift
+output="$1"
+shift
+
+target="dbm_fc_solver"
+
+board="$deal.board"
+
+offload_dir="$HOME/2fc-dbm_fc_solver-offload/deal-$deal"
+final_out="$HOME/2fc-dbm_fc_solver-results/deal-$deal"
+base_dir="`pwd`"
+
+echo '#!/bin/bash' > "$output"
+echo  >> "$output"
+# shar "$board" "$target" | grep -vE '^ *exit *0 *$' >> "$output"
+echo >> "$output"
+echo "mkdir -p '$offload_dir' '$final_out'" >> "$output"
+echo "$base_dir/$target --batch-size 20 --num-threads ${THREADS:-16} --offload-dir-path '$offload_dir' '$base_dir/$board' | perl -lnE 'use Time::HiRes qw/time/; printf qq#%.9f\\t%s\\n#, time(), \$_; STDOUT->flush;' | tee '$final_out/$deal.dump' '$final_out/$deal.dump.backup'" >> "$output"
