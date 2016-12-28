a1cc319d59c4e55ad80cfae10bfd724d2dbb994f
Shlomi Fish <shlomif@shlomifish.org>
2016-12-28 20:42:59 +0200

    Extract more into PrepareCommon.

--- fc-solve/scripts/prepare-self-contained-dbm-etc-solvers-packages-for-hpc-machines/PrepareCommon.pm
+++ fc-solve/scripts/prepare-self-contained-dbm-etc-solvers-packages-for-hpc-machines/PrepareCommon.pm
@@ -3,9 +3,17 @@ package PrepareCommon;
 use strict;
 use warnings;
 
+use File::Temp qw / tempdir /;
+use Cwd qw / getcwd /;
+use File::Path qw/ mkpath /;
+use IO::All qw/ io /;
+
 use Moose;
 
 has 'depth_dbm' => (is => 'ro', isa => 'Bool', required => 1);
+has ['dest_dir_base', 'march_flag'] => (is => 'ro', isa => 'Str', required => 1);
+has 'flto' => (is => 'ro', isa => 'Bool', required => 1);
+has ['mem', 'num_hours', 'num_threads'] => (is => 'ro', isa => 'Int', required => 1);
 
 sub main_base
 {
@@ -98,4 +106,140 @@ sub modules
         'state.o',
     ];
 }
+
+sub run
+{
+    my $self = shift;
+
+    my $BIN_DIR = File::Spec->rel2abs(File::Basename::dirname($0));
+    my $num_freecells = $ENV{NUM_FC} || 4;
+    my $temp_dir = tempdir( CLEANUP => 1 );
+    my $dest_dir_base = $self->dest_dir_base;
+    my $dest_dir = File::Spec->catdir($temp_dir, $dest_dir_base);
+    my $build_dir = File::Spec->catdir($temp_dir, 'build');
+
+    my $src_path = getcwd();
+
+    mkpath("$dest_dir");
+    mkpath("$dest_dir/libavl");
+    mkpath("$dest_dir/pthread");
+    mkpath($build_dir);
+
+    chdir ($build_dir);
+    system(File::Spec->catdir($src_path, "Tatzer"),
+        qw{-l x64b}, "--nfc=$num_freecells", qw{--states-type=COMPACT_STATES --dbm=kaztree},
+        $src_path
+    );
+
+    foreach my $fn (@{$self->src_filenames()})
+    {
+        io("$src_path/$fn") > io("$dest_dir/$fn");
+    }
+
+    foreach my $fn ('dbm_common.h')
+    {
+        io("$dest_dir/$fn")->print(io("$src_path/$fn")->slurp() =~ s{^(#define FCS_DBM_FREECELLS_NUM\s*)\d+(\s*)$}{$1$num_freecells$2}mrs);
+    }
+
+    foreach my $fn ('config.h', 'fcs_back_compat.h',)
+    {
+        io("./$fn") > io("$dest_dir/$fn");
+    }
+
+    foreach my $fn ('rwlock.c', 'queue.c', 'pthread/rwlock_fcfs.h', 'pthread/rwlock_fcfs_queue.h')
+    {
+        io("/home/shlomif/progs/C/pthreads/rwlock/fcfs-rwlock/pthreads/$fn") > io("$dest_dir/$fn")
+    }
+
+    for my $fn ("prepare_vendu_deal.bash")
+    {
+        io("$BIN_DIR/$fn") > io("$dest_dir/$fn")
+    }
+
+my @deals =
+(qw/
+    504179225
+    8203007169
+    /);
+
+foreach my $deal_idx (@deals)
+{
+    if (system(qq{pi-make-microsoft-freecell-board -t $deal_idx > $dest_dir/$deal_idx.board}) != 0)
+    {
+        die "Could not generate deal no. $deal_idx.";
+    }
+}
+
+my @modules = @{ $self->modules };
+
+my $more_cflags = $self->flto ? " -flto " : '';
+
+if (0)
+{
+    $more_cflags .= ' -DFCS_DBM_USE_RWLOCK=1 ';
+}
+
+my $num_threads = $self->num_threads;
+my $num_hours = $self->num_hours;
+my $num_cpus = $num_threads;
+my $mem = $self->mem;
+my $march_flag = $self->march_flag;
+
+io("$dest_dir/Makefile")->print(<<"EOF");
+TARGET = dbm_fc_solver
+DEALS = @deals
+
+DEALS_DUMPS = \$(patsubst %,%.dump,\$(DEALS))
+DEALS_BOARDS = \$(patsubst %,%.board,\$(DEALS))
+
+THREADS = $num_threads
+MEM = $mem
+CPUS = $num_cpus
+HOURS = $num_hours
+
+CFLAGS = -std=gnu99 -O3 $march_flag -fomit-frame-pointer $more_cflags -DFCS_DBM_WITHOUT_CACHES=1 -DFCS_DBM_USE_LIBAVL=1 -DFCS_LIBAVL_STORE_WHOLE_KEYS=1 -DFCS_DBM_RECORD_POINTER_REPR=1 -DFCS_DEBONDT_DELTA_STATES=1 -I. -I./libavl
+MODULES = @modules
+
+JOBS = \$(patsubst %,jobs/%.job.sh,\$(DEALS))
+JOBS_STAMP = jobs/STAMP
+
+all: \$(TARGET) \$(JOBS)
+
+\$(TARGET): \$(MODULES)
+\tgcc \$(CFLAGS) -fwhole-program -o \$\@ \$(MODULES) -Bstatic -lm -lpthread -ltcmalloc
+\tstrip \$\@
+
+\$(MODULES): %.o: %.c
+\tgcc -c \$(CFLAGS) -o \$\@ \$<
+
+\$(JOBS_STAMP):
+\tmkdir -p jobs
+\ttouch \$\@
+
+\$(JOBS): %: \$(JOBS_STAMP) \$(RESULT)
+\tTHREADS="\$(THREADS)" MEM="\$(MEM)" CPUS="\$(CPUS)" HOURS="\$(HOURS)" bash prepare_vendu_deal.bash "\$(patsubst jobs/%.job.sh,%,\$\@)" "\$\@"
+
+%.show:
+\t\@echo "\$* = \$(\$*)"
+EOF
+
+chdir($temp_dir);
+system("tar", "-cavf", "$src_path/$dest_dir_base.tar.xz", "$dest_dir_base/");
+chdir($src_path);
+}
+
 1;
+
+__END__
+
+=head1 COPYRIGHT AND LICENSE
+
+This file is part of Freecell Solver. It is subject to the license terms in
+the COPYING.txt file found in the top-level directory of this distribution
+and at http://fc-solve.shlomifish.org/docs/distro/COPYING.html . No part of
+Freecell Solver, including this file, may be copied, modified, propagated,
+or distributed except according to the terms contained in the COPYING file.
+
+Copyright (c) 2012 Shlomi Fish
+
+=cut
