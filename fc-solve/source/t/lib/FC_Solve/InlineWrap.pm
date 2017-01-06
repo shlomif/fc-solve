a32c8b4a54c316c9749a3e40639cae4843b9a683
Shlomi Fish <shlomif@shlomifish.org>
2017-01-06 18:34:20 +0200

    [Win32] Try to remove win-specific workarounds.

--- fc-solve/source/t/lib/FC_Solve/InlineWrap.pm
+++ fc-solve/source/t/lib/FC_Solve/InlineWrap.pm
@@ -6,25 +6,15 @@ use warnings;
 use Config;
 use Inline;
 
-use FC_Solve::Paths qw( $IS_WIN );
-
 sub import
 {
     my ( $self, %args ) = @_;
 
     my ($pkg) = caller(0);
 
-    my $src = delete( $args{C} );
-    my $libs = delete( $args{l} ) // '';
-
-    my @workaround_for_a_heisenbug =
-        ( $IS_WIN ? ( optimize => '-g' ) : () );
-
+    my $src     = delete( $args{C} );
+    my $libs    = delete( $args{l} ) // '';
     my $ccflags = "$Config{ccflags} -std=gnu99";
-    if ($IS_WIN)
-    {
-        $ccflags =~ s#(^|\s)-[Of][a-zA-Z0-9_\-]*#$1#gms;
-    }
 
     my @inline_params = (
         C                 => $src,
@@ -34,7 +24,6 @@ sub import
         CCFLAGS           => $ccflags,
         CLEAN_AFTER_BUILD => 0,
         LIBS              => "-L$ENV{FCS_PATH} $libs",
-        @workaround_for_a_heisenbug,
         %args,
     );
 
