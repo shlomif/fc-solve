7e29ba60af22aefda84a3331c9ed4466feeaec99
Shlomi Fish <shlomif@shlomifish.org>
2017-01-18 10:13:02 +0200

    reduce more

--- fc-solve/source/main.c
+++ fc-solve/source/main.c
@@ -15,7 +15,38 @@
  * Freecell Solver distribution from http://fc-solve.shlomifish.org/ .
  */
 #include "rinutils.h"
-#include "preset.h"
+#include "config.h"
+
+#include "instance.h"
+#include "move_funcs_maps.h"
+
+typedef struct
+{
+    int preset_id;
+
+    fcs_game_type_params_t game_params;
+
+    char tests_order[FCS_MOVE_FUNCS_NUM * 3 + 1];
+    char allowed_tests[FCS_MOVE_FUNCS_NUM * 3 + 1];
+} fcs_preset_t;
+
+extern fc_solve_preset_ret_code_t fc_solve_apply_preset_by_ptr(
+    fc_solve_instance_t *const instance, const fcs_preset_t *const preset_ptr);
+
+extern fc_solve_preset_ret_code_t fc_solve_get_preset_by_name(
+    const char *const name, const fcs_preset_t **const preset_ptr);
+
+static inline fc_solve_preset_ret_code_t fc_solve_apply_preset_by_name(
+    fc_solve_instance_t *const instance, const char *const name)
+{
+    const fcs_preset_t *preset_ptr;
+    const_AUTO(ret, fc_solve_get_preset_by_name(name, &preset_ptr));
+    if (ret != FCS_PRESET_CODE_OK)
+    {
+        return ret;
+    }
+    return fc_solve_apply_preset_by_ptr(instance, preset_ptr);
+}
 
 int main(int argc, char *argv[])
 {
