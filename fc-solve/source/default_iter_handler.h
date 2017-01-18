6100163e0cdf9ec2bf624896f9a5ccf87db56dba
Shlomi Fish <shlomif@shlomifish.org>
2017-01-18 09:29:25 +0200

    Reduce more.
    Still crashes on https://github.com/shlomif/fc-solve/issues/11 .

--- fc-solve/source/default_iter_handler.h
+++ fc-solve/source/default_iter_handler.h
@@ -16,18 +16,4 @@ static void my_iter_handler(void *const user_instance,
     const fcs_int_limit_t iter_num, const int depth, void *const ptr_state,
     const fcs_int_limit_t parent_iter_num, void *const context)
 {
-    const fc_solve_display_information_context_t *const display_context =
-        (const fc_solve_display_information_context_t *const)context;
-    my_iter_handler_base(
-        iter_num, depth, user_instance, display_context, parent_iter_num);
-    if (display_context->debug_iter_state_output)
-    {
-        char state_string[1000];
-        freecell_solver_user_iter_state_stringify(user_instance, state_string,
-            ptr_state FC_SOLVE__PASS_PARSABLE(
-                display_context->parseable_output),
-            display_context->canonized_order_output FC_SOLVE__PASS_T(
-                display_context->display_10_as_t));
-        printf("%s\n---------------\n\n\n", state_string);
-    }
 }
