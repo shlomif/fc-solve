140e5c022e728d220328dca1b662330b423d67a5
Shlomi Fish <shlomif@shlomifish.org>
2017-01-18 10:29:00 +0200

    reduce more

--- fc-solve/source/main.c
+++ fc-solve/source/main.c
@@ -189,7 +189,249 @@ enum
             (seqs_build_by | (empty_stacks_fill << 2) | (seq_move << 4))       \
     }
 
-#include "fcs_presets_generated.h"
+static const fcs_preset_t fcs_presets[16] =
+{
+    {
+        FCS_PRESET_BAKERS_DOZEN,
+        MAKE_GAME_PARAMS(
+            0,
+            13,
+            1,
+
+            FCS_SEQ_BUILT_BY_RANK,
+            0,
+            FCS_ES_FILLED_BY_NONE
+        ),
+
+        "0123456789",
+        "0123456789"
+    },
+    {
+        FCS_PRESET_BAKERS_GAME,
+        MAKE_GAME_PARAMS(
+            4,
+            8,
+            1,
+
+            FCS_SEQ_BUILT_BY_SUIT,
+            0,
+            FCS_ES_FILLED_BY_ANY_CARD
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_BELEAGUERED_CASTLE,
+        MAKE_GAME_PARAMS(
+            0,
+            8,
+            1,
+
+            FCS_SEQ_BUILT_BY_RANK,
+            0,
+            FCS_ES_FILLED_BY_ANY_CARD
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_CRUEL,
+        MAKE_GAME_PARAMS(
+            0,
+            12,
+            1,
+
+            FCS_SEQ_BUILT_BY_SUIT,
+            0,
+            FCS_ES_FILLED_BY_NONE
+        ),
+
+        "0123456789",
+        "0123456789"
+    },
+    {
+        FCS_PRESET_DER_KATZENSCHWANZ,
+        MAKE_GAME_PARAMS(
+            8,
+            9,
+            2,
+
+            FCS_SEQ_BUILT_BY_ALTERNATE_COLOR,
+            1,
+            FCS_ES_FILLED_BY_NONE
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_DIE_SCHLANGE,
+        MAKE_GAME_PARAMS(
+            8,
+            9,
+            2,
+
+            FCS_SEQ_BUILT_BY_ALTERNATE_COLOR,
+            0,
+            FCS_ES_FILLED_BY_NONE
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_EIGHT_OFF,
+        MAKE_GAME_PARAMS(
+            8,
+            8,
+            1,
+
+            FCS_SEQ_BUILT_BY_SUIT,
+            0,
+            FCS_ES_FILLED_BY_KINGS_ONLY
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_FAN,
+        MAKE_GAME_PARAMS(
+            0,
+            18,
+            1,
+
+            FCS_SEQ_BUILT_BY_SUIT,
+            0,
+            FCS_ES_FILLED_BY_KINGS_ONLY
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_FORECELL,
+        MAKE_GAME_PARAMS(
+            4,
+            8,
+            1,
+
+            FCS_SEQ_BUILT_BY_ALTERNATE_COLOR,
+            0,
+            FCS_ES_FILLED_BY_KINGS_ONLY
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_FREECELL,
+        MAKE_GAME_PARAMS(
+            4,
+            8,
+            1,
+
+            FCS_SEQ_BUILT_BY_ALTERNATE_COLOR,
+            0,
+            FCS_ES_FILLED_BY_ANY_CARD
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_GOOD_MEASURE,
+        MAKE_GAME_PARAMS(
+            0,
+            10,
+            1,
+
+            FCS_SEQ_BUILT_BY_RANK,
+            0,
+            FCS_ES_FILLED_BY_NONE
+        ),
+
+        "0123456789",
+        "0123456789"
+    },
+    {
+        FCS_PRESET_KINGS_ONLY_BAKERS_GAME,
+        MAKE_GAME_PARAMS(
+            4,
+            8,
+            1,
+
+            FCS_SEQ_BUILT_BY_SUIT,
+            0,
+            FCS_ES_FILLED_BY_KINGS_ONLY
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_RELAXED_FREECELL,
+        MAKE_GAME_PARAMS(
+            4,
+            8,
+            1,
+
+            FCS_SEQ_BUILT_BY_ALTERNATE_COLOR,
+            1,
+            FCS_ES_FILLED_BY_ANY_CARD
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_RELAXED_SEAHAVEN_TOWERS,
+        MAKE_GAME_PARAMS(
+            4,
+            10,
+            1,
+
+            FCS_SEQ_BUILT_BY_SUIT,
+            1,
+            FCS_ES_FILLED_BY_KINGS_ONLY
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_SEAHAVEN_TOWERS,
+        MAKE_GAME_PARAMS(
+            4,
+            10,
+            1,
+
+            FCS_SEQ_BUILT_BY_SUIT,
+            0,
+            FCS_ES_FILLED_BY_KINGS_ONLY
+        ),
+
+        "[01][23456789]",
+        "0123456789ABCDE"
+    },
+    {
+        FCS_PRESET_SIMPLE_SIMON,
+        MAKE_GAME_PARAMS(
+            0,
+            10,
+            1,
+
+            FCS_SEQ_BUILT_BY_SUIT,
+            0,
+            FCS_ES_FILLED_BY_ANY_CARD
+        ),
+
+        "abcdefgh",
+        "abcdefghi"
+    }
+};
 
 typedef struct
 {
