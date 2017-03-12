73b3834bee49740650d67cba596779c638b10130
Shlomi Fish <shlomif@shlomifish.org>
2017-03-12 19:03:53 +0200

    more state.

--- fc-solve/scripts/generate-summary-length-improvements/summarizer-v2.py
+++ fc-solve/scripts/generate-summary-length-improvements/summarizer-v2.py
@@ -21,7 +21,7 @@ with open(basedir + "/FC_SOLVE_SUMMARIZE_RESULTS--fif-10.cat.txt") as fh:
 NUM = 32000
 assert len(state['start_nums']) == NUM
 
-run_nums = [x for x in state['start_nums']]
+state['run_nums'] = [x for x in state['start_nums']]
 mysum = 0
 my_num_improved = 0
 my_max = 0
@@ -34,16 +34,16 @@ for seed in xrange(1, 4840):
         for l in fh:
             new = extract(l)
             if new >= 0:
-                delta = run_nums[i] - new
+                delta = state['run_nums'][i] - new
                 if delta > 0:
                     orig = state['start_nums'][i]
-                    if run_nums[i] == orig:
+                    if state['run_nums'][i] == orig:
                         my_num_improved += 1
                     mysum += delta
                     init_delta = orig - new
                     if init_delta > my_max:
                         my_max = init_delta
-                    run_nums[i] = new
+                    state['run_nums'][i] = new
             i += 1
     print( "%d\t%d\t%d\t%d" % (seed, my_num_improved, mysum, my_max))
 
