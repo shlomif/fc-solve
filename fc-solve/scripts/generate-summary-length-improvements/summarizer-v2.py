250b63dd7189b7f4189601898d52edba8576e8a7
Shlomi Fish <shlomif@shlomifish.org>
2017-03-13 09:22:30 +0200

    Add persistence / caching.

--- fc-solve/scripts/generate-summary-length-improvements/summarizer-v2.py
+++ fc-solve/scripts/generate-summary-length-improvements/summarizer-v2.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 import re
-from os.path import expanduser
+import sys
+from os.path import expanduser, isfile
+import json
 
 home = expanduser("~")
 
 basedir = home + "/Backup/Arcs"
 
+STATE_FN = basedir + "/FC_SOLVE_SUMMARIZE_RESULTS--state.json"
 
 regex = re.compile(r'(?<=Length: )(-?[0-9]+)')
 
@@ -16,7 +19,7 @@ def extract(l):
 NUM = 32000
 
 def calc_init_state():
-    state = {'start_nums' : [], 'mysum': 0, 'my_num_improved': 0, 'my_max': 0, }
+    state = {'start_nums' : [], 'reached_seed': 1, 'mysum': 0, 'my_num_improved': 0, 'my_max': 0, 'output': '', }
     with open(basedir + "/FC_SOLVE_SUMMARIZE_RESULTS--fif-10.cat.txt") as fh:
         for l in fh:
             state['start_nums'].append(extract(l))
@@ -26,11 +29,22 @@ def calc_init_state():
 
     return state
 
-state = calc_init_state()
+def write_state(state):
+    with open(STATE_FN, 'w') as fh:
+        fh.write(json.dumps(state))
+    return
 
-MAX_SEED = 4839
+if not isfile(STATE_FN):
+    write_state(calc_init_state())
+
+state = None
+with open(STATE_FN) as fh:
+    state = json.loads(fh.read())
+
+MAX_SEED = 5302
 print( "Seed\tN\tSum\tMax" )
-for seed in xrange(1, MAX_SEED + 1):
+sys.stdout.write(state['output'])
+for seed in xrange(state['reached_seed'], MAX_SEED + 1):
     with open(basedir + "/fcs-summary-len-seed/lens-theme1--seed=%d.txt" % seed) as fh:
         i = 0
         for l in fh:
@@ -47,6 +61,9 @@ for seed in xrange(1, MAX_SEED + 1):
                         state['my_max'] = init_delta
                     state['run_nums'][i] = new
             i += 1
-    print( "%d\t%d\t%d\t%d" % (seed, state['my_num_improved'], state['mysum'], state['my_max']))
+    out_line = ("%d\t%d\t%d\t%d" % (seed, state['my_num_improved'], state['mysum'], state['my_max']))
+    state['output'] += out_line + "\n"
+    print out_line
 
 state['reached_seed'] = MAX_SEED
+write_state(state)
