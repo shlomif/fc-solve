5b91ecda957607fb8ca83a46f9070fc0e0cb4293
Shlomi Fish <shlomif@shlomifish.org>
2017-06-09 23:20:17 +0300

    Correct handling of over 31-bits deals.
    On 32-bit system and/or windows at least.

--- fc-solve/source/fc_pro_range_solver.c
+++ fc-solve/source/fc_pro_range_solver.c
@@ -19,11 +19,11 @@
 #include "help_err.h"
 #include "try_param.h"
 
-static inline void fc_pro_get_board(long deal_idx,
+static inline void fc_pro_get_board(const long long deal_idx,
     fcs_state_string_t state_string,
-    fcs_state_keyval_pair_t *pos IND_BUF_T_PARAM(indirect_stacks_buffer))
+    fcs_state_keyval_pair_t *const pos IND_BUF_T_PARAM(indirect_stacks_buffer))
 {
-    get_board(deal_idx, state_string);
+    get_board_l(deal_idx, state_string);
     fc_solve_initial_user_state_to_c(
         state_string, pos, 4, 8, 1, indirect_stacks_buffer);
 }
