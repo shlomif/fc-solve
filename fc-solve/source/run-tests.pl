9c52157842605385002b69b2a811cc844526a111
Shlomi Fish <shlomif@shlomifish.org>
2017-07-04 23:19:04 +0300

    Cleanups.

--- fc-solve/source/run-tests.pl
+++ fc-solve/source/run-tests.pl
@@ -71,49 +71,29 @@ GetOptions(
         ( path( File::Spec->catdir( File::Spec->curdir(), "t", "TAGS.txt" ) )
             ->slurp_utf8 =~ /([a-zA-Z_0-9]+)/g );
 
-    my $testing_preset_rc;
+    my $preset_dest =
+        File::Spec->catdir( File::Spec->curdir(), "t", "Presets" );
+    my $preset_src = File::Spec->catfile( $abs_bindir, "Presets" );
+    my $pset_files_dest = File::Spec->catdir( $preset_dest, "presets" );
+    my $pset_files_src  = File::Spec->catdir( $preset_src,  "presets" );
+    mkpath($pset_files_dest);
+    foreach my $f ( path($pset_files_src)->children(qr{\.sh\z}) )
     {
-        my $preset_dest =
-            File::Spec->catdir( File::Spec->curdir(), "t", "Presets" );
-        my $preset_src = File::Spec->catfile( $abs_bindir, "Presets" );
-        my $pset_files_dest = File::Spec->catdir( $preset_dest, "presets" );
-        my $pset_files_src  = File::Spec->catdir( $preset_src,  "presets" );
-        mkpath($pset_files_dest);
-        opendir( my $d, $pset_files_src );
-        while ( defined( my $f = readdir($d) ) )
-        {
-
-            if ( $f =~ m{\.sh\z} )
-            {
-                copy(
-                    File::Spec->catfile( $pset_files_src,  $f ),
-                    File::Spec->catfile( $pset_files_dest, $f )
-                );
-            }
-        }
-        closedir($d);
-
-        my $files_dest_abs =
-            File::Spec->rel2abs(
-            File::Spec->catfile( $pset_files_dest, "STUB.TXT" ) );
-
-        $files_dest_abs =~ s{STUB\.TXT\z}{};
-
-        $testing_preset_rc =
-            File::Spec->rel2abs(
-            File::Spec->catfile( $preset_dest, "presetrc" ) );
-
-        open my $in, "<",
-            File::Spec->catfile( $fcs_path, "Presets", "presetrc" );
-        open my $out, ">", $testing_preset_rc;
-        while ( my $line = <$in> )
-        {
-            $line =~ s{\A(dir=)(.*)}{$1$files_dest_abs\n}ms;
-            print {$out} $line;
-        }
-        close($in);
-        close($out);
+        $f->copy( File::Spec->catfile( $pset_files_dest, $f->basename ) );
     }
+
+    my $files_dest_abs =
+        File::Spec->rel2abs(
+        File::Spec->catfile( $pset_files_dest, "STUB.TXT" ) );
+
+    $files_dest_abs =~ s{STUB\.TXT\z}{};
+
+    my $testing_preset_rc =
+        File::Spec->rel2abs( File::Spec->catfile( $preset_dest, "presetrc" ) );
+    path($testing_preset_rc)
+        ->spew_utf8(
+        path( File::Spec->catfile( $fcs_path, "Presets", "presetrc" ) )
+            ->slurp_utf8 =~ s{^(dir=)([^\n]*)}{$1$files_dest_abs}gmrs );
 
     local $ENV{FREECELL_SOLVER_PRESETRC} = $testing_preset_rc;
     local $ENV{FREECELL_SOLVER_QUIET}    = 1;
