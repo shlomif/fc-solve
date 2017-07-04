300d16f925157680a1db8c3d5302eae82f0a11a8
Shlomi Fish <shlomif@shlomifish.org>
2017-07-05 01:04:31 +0300

    cleanup and update the perl scripts.

--- fc-solve/source/scripts/perl-remove-trailing-newlines.pl
+++ fc-solve/source/scripts/perl-remove-trailing-newlines.pl
@@ -1,38 +1,16 @@
-#!/usr/bin/perl
-
 use strict;
 use warnings;
-use autodie;
-
+use Path::Tiny qw/ path /;
 use Getopt::Long qw/ GetOptions /;
 
 my $output;
 GetOptions( '-o=s' => \$output, )
     or die "Failed - $!";
 
-sub _slurp
-{
-    my $filename = shift;
-
-    open my $in, '<', $filename
-        or die "Cannot open '$filename' for slurping - $!";
-
-    local $/;
-    my $contents = <$in>;
-
-    close($in);
-
-    return $contents;
-}
-
 my $t = '';
 foreach my $fn (@ARGV)
 {
-    $t .= _slurp($fn);
+    $t .= path($fn)->slurp_utf8;
 }
 
-$t =~ s{[\n\r]+\z}{};
-
-open my $o, '>', $output;
-print {$o} $t;
-close $o;
+path($output)->spew_utf8( $t =~ s{[\n\r]+\z}{}r );
