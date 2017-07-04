32e46611455f853d8e767e57a80e60ad3e827ce2
Shlomi Fish <shlomif@shlomifish.org>
2017-07-04 17:35:15 +0300

    Revert slowdown

--- fc-solve/source/help_err.h
+++ fc-solve/source/help_err.h
@@ -1,10 +1,14 @@
 #ifndef FCS_WITHOUT_CMD_LINE_HELP
 static void print_help(void);
 #endif
-static inline __attribute__((noreturn)) void help_err(const char *const msg)
+static inline __attribute__((noreturn)) void help_err(
+    const char *const msg, ...)
 {
 #ifndef FCS_WITHOUT_CMD_LINE_HELP
-    fputs(msg, stderr);
+    va_list ap;
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
     print_help();
 #endif
     exit(-1);
