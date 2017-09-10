d5222ca9fc9d32de164d011ea6953ba10539ed2c
Shlomi Fish <shlomif@shlomifish.org>
2017-09-10 22:23:15 +0300

    Implement more functionality.

--- fc-solve/scripts/analyse-deals-range-prelude-stage-execution.py
+++ fc-solve/scripts/analyse-deals-range-prelude-stage-execution.py
@@ -28,9 +28,46 @@ def iters_count_iterator(fn):
                     last = long(m.group(1))
 
 
+class PreludeAnalyzer(object):
+    """docstring for PreludeAnalyzer"""
+    def __init__(self, fn):
+        with open(fn, 'r') as f:
+            for l in f:
+                m = re.match(r'^\s*--prelude "([^"]+)"', l)
+                if m:
+                    s = m.group(1)
+                    lst = s.split(',')
+                    _stages = []
+                    for stage in lst:
+                        m = re.match(r'^([0-9]+)@([0-9A-Za-z_]+)$', stage)
+                        if not m:
+                            raise BaseException('Invalid stage: ' + stage)
+                        _stages.append({'quota': long(m.group(1)),
+                                        'id': m.group(2), 'data': []})
+                    _stages.append({'quota': long('1000000000000'),
+                                    'id': 'AFTER', 'data': []})
+                    self._stages = _stages
+                    break
+            else:
+                raise BaseException('Cannot find prelude.')
+
+    def insert(self, i):
+        for s in self._stages:
+            quota = s['quota']
+            if i <= quota:
+                s['data'].append(i)
+                break
+            else:
+                i -= quota
+        else:
+            raise BaseException('Cannot fit in prelude.')
+
+
 def main():
+    p = PreludeAnalyzer('../scripts/TEST_OPTIMIZATIONS/obf-mod4.sh')
     for i in iters_count_iterator('serial.dump'):
         print_(i)
+        p.insert(i)
 
 
 if __name__ == "__main__":
