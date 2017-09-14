40f8a83326a45448d833abc2f0f4e248df406479
Shlomi Fish <shlomif@shlomifish.org>
2017-09-15 02:24:20 +0300

    merge two headers.

--- fc-solve/source/range_solvers.h
+++ fc-solve/source/range_solvers.h
@@ -15,7 +15,19 @@
 #include "fcs_cl.h"
 #include "range_solvers_gen_ms_boards.h"
 #include "handle_parsing.h"
-#include "help_err.h"
+
+#ifndef FCS_WITHOUT_CMD_LINE_HELP
+static void print_help(void);
+static __attribute__((noreturn)) void help_err(const char *const msg, ...)
+{
+    va_list ap;
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
+    print_help();
+    exit(-1);
+}
+#endif
 
 static inline fcs_bool_t range_solvers__solve(void *const instance,
     const long long board_num, long long *const total_num_iters_temp)
