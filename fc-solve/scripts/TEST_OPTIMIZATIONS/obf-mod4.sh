d19a87cafdc97df1cb0ebc79f498eee1766f0b48
Shlomi Fish <shlomif@shlomifish.org>
2017-09-19 21:18:04 +0300

    optimize down to 3.68073487281799 .
    By removing a soft thread.

--- fc-solve/scripts/TEST_OPTIMIZATIONS/obf-mod4.sh
+++ fc-solve/scripts/TEST_OPTIMIZATIONS/obf-mod4.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # This scan is derived from the obf scan.
 freecell-solver-range-parallel-solve 1 32000 1 \
---method random-dfs -seed 2 -to 0[01][23456j89] -step 500 -sp r:tf --st-name 3 -nst \
 --method random-dfs -seed 3 -to "0[124]=asw(1)j[36]=asw(1)" -step 500 -sp r:tf --st-name 5 -nst \
 --method random-dfs -seed 6 -to 0j26[34] -step 500 -sp r:tf --st-name 9 -nst \
 --method random-dfs -to [01][23456j89] -seed 8 -step 500 -sp r:tf --st-name 10 -nst \
@@ -19,4 +18,4 @@ freecell-solver-range-parallel-solve 1 32000 1 \
 --method random-dfs -to "[01][2345j]" -dto2 "5,[0123][456j]" -dto2 "13,[0123456j]=asw(1)" -seed 33627 -step 500 -sp r:tf --st-name sG -nst \
 --method random-dfs -to "[01][2345j]" -dto2 "5,[0123][456j]" -dto2 "13,[0123456j]=asw(1)" -seed 18296 -step 500 -sp r:tf --st-name sH -nst \
 --method random-dfs -to "012j[35]" -dto2 "9,[012]j[468]" -dto2 "15,[012346j8]=asw(1)" -seed 10468 -step 500 -sp r:tf --st-name sI \
---prelude "295@24,230@5,280@9,412@sI,400@sF,490@sH,428@sG,760@32,380@12,281@24,380@10,76@5,200@new,222@sE,225@sD,266@sB,380@3,380@9,380@24,760@20,380@11,760@24,380@11"
+--prelude "295@24,230@5,280@9,412@sI,400@sF,490@sH,428@sG,760@32,380@12,281@24,380@10,76@5,200@new,222@sE,225@sD,266@sB,380@24,760@20,380@11,760@24,380@11"
