6ee8906e550dab605ffe684e545dc923bc723914
Shlomi Fish <shlomif@shlomifish.org>
2017-09-19 16:18:39 +0300

    Optimise down to 3.76258683204651

--- fc-solve/scripts/TEST_OPTIMIZATIONS/obf-mod4.sh
+++ fc-solve/scripts/TEST_OPTIMIZATIONS/obf-mod4.sh
@@ -3,7 +3,7 @@
 freecell-solver-range-parallel-solve 1 32000 1 \
 --method random-dfs -seed 2 -to 0[01][23456j89] -step 500 -sp r:tf --st-name 3 -nst \
 --method random-dfs -seed 3 -to "0[124]=asw(1)j[36]=asw(1)" -step 500 -sp r:tf --st-name 5 -nst \
---method random-dfs -seed 4 -to 0[2346j] -step 500 -sp r:tf --st-name 9 -nst \
+--method random-dfs -seed 4 -to 0j[2346] -step 500 -sp r:tf --st-name 9 -nst \
 --method random-dfs -to [01][23456j89] -seed 8 -step 500 -sp r:tf --st-name 10 -nst \
 --method a-star -asw 0.2,0.8,0,0,0 -step 500 -sp r:tf --st-name 11 -nst \
 --method random-dfs -to "[01]=asw(1)[23456j89]=asw(1)" -step 500 -sp r:tf --st-name 12 -nst \
@@ -19,5 +19,5 @@ freecell-solver-range-parallel-solve 1 32000 1 \
 --method random-dfs -to "01[2345j]" -dto2 "9,012[456j]" -seed 44250 -step 500 -sp r:tf --st-name sF -nst \
 --method random-dfs -to "[01][2345j]" -dto2 "5,[0123][456j]" -dto2 "13,[0123456j]=asw(1)" -seed 33627 -step 500 -sp r:tf --st-name sG -nst \
 --method random-dfs -to "[01][2345j]" -dto2 "5,[0123][456j]" -dto2 "13,[0123456j]=asw(1)" -seed 18296 -step 500 -sp r:tf --st-name sH -nst \
---method random-dfs -to "01j[235]" -dto2 "9,[012]j[468]" -dto2 "15,[012346j8]=asw(1)" -seed 10468 -step 500 -sp r:tf --st-name sI \
+--method random-dfs -to "012j[35]" -dto2 "9,[012]j[468]" -dto2 "15,[012346j8]=asw(1)" -seed 10468 -step 500 -sp r:tf --st-name sI \
 --prelude "295@24,230@5,280@9,414@sI,400@sF,490@sH,428@sG,760@32,380@12,281@24,380@10,76@5,192@s1,200@new,222@sE,225@sD,266@sB,380@3,380@9,380@24,760@20,380@11,760@24,380@11"
