b52e358bd87bb5c3a3d753ad9f7a05c91f5f57d4
Shlomi Fish <shlomif@shlomifish.org>
2017-09-30 23:03:28 +0300

    Update script.

--- fc-solve/scripts/try-to-solve-board-with-range-of-seeds.bash
+++ fc-solve/scripts/try-to-solve-board-with-range-of-seeds.bash
@@ -3,13 +3,13 @@
 board="$1"
 shift
 
-seed=4400
+seed=1
 found=0
 while test "$found" = 0 ; do
     echo "Trying seed = $seed"
-    if ./fc-solve --method random-dfs -sp r:tf -to '[01234556789ABCDE]' -seed "$seed" -mi 3000000 "$board" | grep -E '^This game is solv' ; then
+    if ./fc-solve --method random-dfs -sp r:tf -to '[0123456789ABCDE]' -seed "$seed" -mi 3000000 "$board" | grep -E '^This game is solv' ; then
         found=1
-        echo "Found seed=$seed"
+        echo "Found seed = $seed"
     else
         let seed++
     fi
