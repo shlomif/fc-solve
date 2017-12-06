abfb8a7e8ff4103b7004c3c960be058394d32d84
Shlomi Fish <shlomif@shlomifish.org>
2017-12-06 12:21:45 +0200

    Refactoring: avoid old cruft.

--- fc-solve/source/rate_state.h
+++ fc-solve/source/rate_state.h
@@ -14,18 +14,11 @@
 extern "C" {
 #endif
 
-#define FCS_BEFS_SEQS_OVER_RENEGADE_CARDS_EXPONENT 1.3
-
 typedef double fc_solve_seq_cards_power_type_t;
 extern const fc_solve_seq_cards_power_type_t
     fc_solve_seqs_over_cards_lookup[2 * 13 * 4 + 1];
 
-#if 0
-#define FCS_SEQS_OVER_RENEGADE_POWER(n)                                        \
-    pow(n, FCS_BEFS_SEQS_OVER_RENEGADE_CARDS_EXPONENT)
-#else
 #define FCS_SEQS_OVER_RENEGADE_POWER(n) fc_solve_seqs_over_cards_lookup[(n)]
-#endif
 
 #ifdef __cplusplus
 };
