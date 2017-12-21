1ba7015fa30a7b5a87c382bbeec66ecfca09f825
Shlomi Fish <shlomif@shlomifish.org>
2017-12-21 22:21:11 +0200

    Code cleanup. Remove pointless cmts and macros.

--- fc-solve/source/rank2str.h
+++ fc-solve/source/rank2str.h
@@ -14,27 +14,13 @@ extern "C" {
 
 #include "config.h"
 #include "rinutils.h"
-/*
- * Those strings contain the string representations of the different cards.
- * If CARD_DEBUG_PRES is defined then an asterisk is printed as an empty card.
- *
- * Notice that there are two of them: one prints 10 and one prints T for the
- * 10 card.
- *
- * */
+
 #ifdef CARD_DEBUG_PRES
 #define CARD_ZERO() "*"
 #else
 #define CARD_ZERO() " "
 #endif
 
-/*
- * Converts a rank from its internal representation to a string.
- *
- * rank_idx - the rank
- * str - the string to output to.
- * t - whether 10 should be printed as T or not.
- * */
 #define GEN_CARD_MAP(t_card)                                                   \
     {                                                                          \
         CARD_ZERO()                                                            \
@@ -50,14 +36,12 @@ static const char cards_char_map[15] = (CARD_ZERO() "A23456789TJQK");
 #endif
 
 static inline void rank2str(
-    const int rank_idx, char *const str PASS_T(const fcs_bool_t t))
+    const int rank_idx, char *const str PASS_T(const fcs_bool_t display_T))
 {
-#define INDEX() (rank_idx)
 #ifdef FC_SOLVE_IMPLICIT_T_RANK
-    str[0] = cards_char_map[INDEX()];
+    str[0] = cards_char_map[rank_idx];
     str[1] = '\0';
 #else
-    strcpy(str, (t ? card_map_3_T : card_map_3_10)[INDEX()]);
+    strcpy(str, (display_T ? card_map_3_T : card_map_3_10)[rank_idx]);
 #endif
-#undef INDEX
 }
