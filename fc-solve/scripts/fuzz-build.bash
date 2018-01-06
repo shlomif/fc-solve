5924b17be9cecfe00cf91877d1ba01474b08ed2f
Shlomi Fish <shlomif@shlomifish.org>
2018-01-06 19:39:10 +0200

    Add the build fuzzer.
    It allows to randomize command line arguments and see if they cause
    warnings on clang.

---
+++ fc-solve/scripts/fuzz-build.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+export CC=/usr/bin/clang CXX=/usr/bin/clang++ FCS_CLANG=1 CFLAGS="-Werror"
+seed=1
+while true
+do
+    echo "Checking seed=$seed"
+    export FCS_THEME_RAND="$seed"
+    if (../source/Tatzer && make)
+    then
+        let seed++
+    else
+        echo "seed=$seed failed"
+        exit 1
+    fi
+done
