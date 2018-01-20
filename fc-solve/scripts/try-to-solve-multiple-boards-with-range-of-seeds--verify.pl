df7ce733adcaacbb55d68ef27d88a5d6caf46d9e
Shlomi Fish <shlomif@shlomifish.org>
2018-01-20 16:48:29 +0200

    Add verify.

--- fc-solve/scripts/try-to-solve-multiple-boards-with-range-of-seeds--verify.pl
+++ fc-solve/scripts/try-to-solve-multiple-boards-with-range-of-seeds--verify.pl
@@ -3,13 +3,20 @@
 use strict;
 use warnings;
 
-while (my $l = <>)
+while ( my $l = <> )
 {
-    chomp$l;
-    if (my ($seed, $deal) = ($l =~ /\AFound seed = ([0-9]+) for deal = ([0-9]+)\z/))
+    chomp $l;
+    if ( my ( $seed, $deal ) =
+        ( $l =~ /\AFound seed = ([0-9]+) for deal = ([0-9]+)\z/ ) )
     {
         print "=== $l\n";
-        system("pi-make-microsoft-freecell-board -t $deal | ../scripts/summarize-fc-solve --freecells-num 3 -- --method random-dfs -sp r:tf -to '[0123456789ABCDE]' -seed $seed") and die "failed '$l'";
+        my $verdict =
+`pi-make-microsoft-freecell-board -t $deal | ../scripts/summarize-fc-solve --freecells-num 3 -- --method random-dfs -sp r:tf -to '[0123456789ABCDE]' -seed $seed`;
+        if ( $? or $verdict !~ /\AVerdict: Solved / )
+        {
+            die "failed '$l'";
+        }
+        print "\t$verdict";
     }
     else
     {
