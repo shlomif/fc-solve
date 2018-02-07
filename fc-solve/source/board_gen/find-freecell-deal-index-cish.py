633339b0f098a529d7186db5271e8644709c5b1c
Shlomi Fish <shlomif@shlomifish.org>
2018-02-07 16:50:15 +0200

    Got rid of some GPL deps in the cish code.

--- fc-solve/source/board_gen/find-freecell-deal-index-cish.py
+++ fc-solve/source/board_gen/find-freecell-deal-index-cish.py
@@ -10,12 +10,40 @@
 # imports
 import sys
 import re
-from make_pysol_freecell_board import createCards
 from find_index_lib_py import ffi, lib
 
 if sys.version_info > (3,):
     long = int
     xrange = range
+
+
+class Card(object):
+    def __init__(self, id, rank, suit, print_ts):
+        self.id, self.rank, self.suit, self.print_ts = id, rank, suit, print_ts
+
+    def rank_s(self):
+        ret = "0A23456789TJQK"[self.rank]
+        if ((not self.print_ts) and ret == 'T'):
+            ret = '10'
+        return ret
+
+    def suit_s(self):
+        return 'CSHD'[self.suit]
+
+    def to_s(self):
+        ret = self.rank_s() + self.suit_s()
+        return ret
+
+
+def createCards(num_decks, print_ts):
+    id = 0
+    ret = []
+    for d in range(num_decks):
+        for s in range(4):
+            for r in range(13):
+                ret.append(Card(id, r+1, s, print_ts))
+                id += 1
+    return ret
 
 
 def shlomif_main(args):
