420df0e69ccd4b4c08de2a136ce84446ea9fa1bf
Shlomi Fish <shlomif@shlomifish.org>
2018-04-25 13:50:08 +0300

    update index.

--- fc-solve/scripts/0fcpar.bash
+++ fc-solve/scripts/0fcpar.bash
@@ -1,9 +1,9 @@
 #! /bin/bash
 #
 # 0fcpar.bash
-# Copyright (C) 2018 shlomif <shlomif@telaviv1.shlomifish.org>
+# Copyright (C) 2018 shlomif <shlomif@shlomifish.org>
 #
-# Distributed under terms of the MIT license.
+# Distributed under the terms of the MIT license.
 #
 mkdir -p results
-seq 26000 85000 | parallel -u -j4 "$HOME"/Download/unpack/prog/python/pypy2-v5.7.1-src/pypy/goal/pypy-c ../scripts/0fc_sol.py
+seq 30000 85000 | parallel -u -j4 "$HOME"/Download/unpack/prog/python/pypy2-v5.7.1-src/pypy/goal/pypy-c ../scripts/0fc_sol.py
