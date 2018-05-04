0113192aff9b6198ebd37e123650b0ee0c680592
Shlomi Fish <shlomif@shlomifish.org>
2018-05-04 20:26:12 +0300

    remove unneeded code

--- fc-solve/scripts/install-git-cmakey-program-system-wide.bash
+++ fc-solve/scripts/install-git-cmakey-program-system-wide.bash
@@ -13,8 +13,6 @@ clone_dir='mygit'
 "$vcs_tool" clone "$url" "$clone_dir"
 (cd "$clone_dir/$d"
 
-( a="$(pwd)"; cmdir="$a/cmake"; mkdir -p "$cmdir" ; mkdir B2 && cd B2 && hg clone https://bitbucket.org/shlomif/shlomif-cmake-modules && cd shlomif-cmake-modules/shlomif-cmake-modules && cp -f "$(pwd)"/Shlomif_Common.cmake "$cmdir" )
-
 ( mkdir b && cd b && cmake .. && make && sudo make install ) || exit -1
 ) || exit -1
 # Cleanup to avoid trailing space problems.
