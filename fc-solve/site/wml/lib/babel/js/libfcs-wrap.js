fe53dca6e849b0f708a59b5d6a0f53c7e597ff83
Shlomi Fish <shlomif@shlomifish.org>
2018-05-14 23:55:28 +0300

    emcc / asm.js fixes.

--- fc-solve/site/wml/lib/babel/js/libfcs-wrap.js
+++ fc-solve/site/wml/lib/babel/js/libfcs-wrap.js
@@ -6,7 +6,26 @@ function mydef(x, y) {
         return define(x, y);
     }
 }
-mydef([((typeof WebAssembly) ? "libfreecell-solver.min" :
+// Taken from https://stackoverflow.com/questions/47879864/
+// thanks!
+const supported = (() => {
+    try {
+        if (typeof WebAssembly === "object"
+            && typeof WebAssembly.instantiate === "function") {
+            const module = new WebAssembly.Module(
+                Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
+            if (module instanceof WebAssembly.Module) {
+                return (new WebAssembly.Instance(module) instanceof
+                    WebAssembly.Instance);
+            }
+        }
+    } catch (e) {
+    }
+    return false;
+})();
+
+mydef([(supported ? "libfreecell-solver.min" :
+//mydef([(false ? "libfreecell-solver.min" :
     "libfreecell-solver-asm")], function(M) {
     return M;
 });
