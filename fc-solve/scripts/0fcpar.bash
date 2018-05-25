fff3936cb4b8cfd009f3575a0b9afd09cd5ee99e
Shlomi Fish <shlomif@shlomifish.org>
2018-05-25 15:22:43 +0300

    update seeds.

--- fc-solve/scripts/0fcpar.bash
+++ fc-solve/scripts/0fcpar.bash
@@ -6,4 +6,4 @@
 # Distributed under the terms of the MIT license.
 #
 mkdir -p results
-seq 78300 85899 | parallel -u -j4 "$HOME"/Download/unpack/prog/python/pypy2-v5.7.1-src/pypy/goal/pypy-c ../scripts/0fc_sol.py
+seq 82300 85899 | parallel -u -j4 "$HOME"/Download/unpack/prog/python/pypy2-v5.7.1-src/pypy/goal/pypy-c ../scripts/0fc_sol.py
