a76e7947dae3634cd173eb193765c404eaedb0cd
Shlomi Fish <shlomif@shlomifish.org>
2018-06-06 22:05:15 +0300

    Add the new task for multi_config.pl.

---
+++ cpan/Task-FreecellSolver-Testing-MultiConfig/lib/Task/FreecellSolver/Testing/MultiConfig.pm
@@ -0,0 +1,39 @@
+package Task::FreecellSolver::Testing::MultiConfig;
+
+use warnings;
+use strict;
+
+use 5.012;
+
+=head1 NAME
+
+Task::FreecellSolver::Testing::MultiConfig - install the CPAN dependencies of
+the Freecell Solver multi-config test suite.
+
+=pkgroup Freecell Solver deps
+
+=pkg Code::TidyAll::Plugin::ESLint
+
+=pkg Dist::Zilla::Plugin::Test::Kwalitee
+
+=pkg Dist::Zilla::Plugin::Test::TidyAll
+
+=pkg Dist::Zilla::PluginBundle::SHLOMIF
+
+=pkg Pod::Weaver::Section::Support
+
+=pkg HTML::Spelling::Site::Whitelist
+
+=pkg Test::Pod::Coverage
+
+=pkg Test::HTML::Recursive::DeprecatedTags
+
+=pkg Task::FreecellSolver::Testing
+
+=head1 AUTHOR
+
+Shlomi Fish, L<http://www.shlomifish.org/>
+
+=cut
+
+1;
