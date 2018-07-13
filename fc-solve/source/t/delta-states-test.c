f9b5c04a7c9aa5cffa587765e7e29e7a884463c5
Shlomi Fish <shlomif@shlomifish.org>
2018-07-13 13:47:24 +0300

    merge some delta_states code

--- fc-solve/source/t/delta-states-test.c
+++ fc-solve/source/t/delta-states-test.c
@@ -8,66 +8,14 @@
  * Copyright (c) 2011 Shlomi Fish
  */
 // A test for the delta states routines.
-#include <string.h>
-#include <tap.h>
-
-#ifndef FCS_COMPILE_DEBUG_FUNCTIONS
-#define FCS_COMPILE_DEBUG_FUNCTIONS
-#endif
-
-#include "card.c"
-#include "state.c"
-#include "delta_states_any.h"
-#include "indirect_buffer.h"
-#include "trim_trailing_whitespace.h"
-#include "render_state.h"
-#ifdef FCS_FREECELL_ONLY
-#include "is_parent.c"
-#endif
-
-static bool test_encode_and_decode(const fcs_dbm_variant_type local_variant, fcs_delta_stater *const delta, fcs_state_keyval_pair *const state, const char *const expected_str, const char *const blurb)
-{
-    fcs_state_keyval_pair new_derived_state;
-    fcs_encoded_state_buffer enc_state;
-    DECLARE_IND_BUF_T(new_derived_indirect_stacks_buffer)
-    fcs_state_locs_struct locs;
-    fc_solve_init_locs(&locs);
-
-    fcs_init_and_encode_state(
-        delta,
-        local_variant,
-        state,
-        &enc_state
-    );
-
-    fc_solve_delta_stater_decode_into_state(
-        delta,
-        (unsigned char *)&enc_state,
-        &(new_derived_state),
-        new_derived_indirect_stacks_buffer
-    );
-
-    char as_str[1000];
-    FCS__RENDER_STATE(as_str, &(new_derived_state.s), &locs);
-    trim_trailing_whitespace(as_str);
-
-    const bool verdict = ok(!strcmp(as_str, expected_str), "%s", blurb);
-    if (!verdict)
-    {
-        diag("got == <<<\n%s\n>>> ; expected == <<<\n%s\n>>>\n",
-                as_str,
-                expected_str
-            );
-    }
-    return verdict;
-}
+#include "delta_states_test_common.h"
 
 static fcs_card make_card(int rank, int suit)
 {
     return fcs_make_card(rank, suit);
 }
 
-static int main_tests(void)
+static void main_tests(void)
 {
     const fcs_dbm_variant_type local_variant = FCS_DBM_VARIANT_2FC_FREECELL;
     {
@@ -691,7 +639,6 @@ static int main_tests(void)
 
         fc_solve_delta_stater_release (&delta);
     }
-    return 0;
 }
 
 int main(void)
