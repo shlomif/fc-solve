a580a9476c989bbdeacf0198208bab69e966da65
Shlomi Fish <shlomif@shlomifish.org>
2018-08-23 01:48:02 +0300

    Remove unused deps.

--- fc-solve/site/wml/bin/install-npm-deps.sh
+++ fc-solve/site/wml/bin/install-npm-deps.sh
@@ -5,5 +5,5 @@
 # Distributed under terms of the MIT license.
 #
 npm install \
-    amdefine babel-cli babel-preset-env babel-preset-stage-2 big-integer bower browserify camel-case coffeescript eslint eslint-config-google html-minifier qunit qunit-cli requirejs tslint typescript uglify-es \
-    @types/big-integer @types/jquery @types/qunit \
+    amdefine babel-cli babel-preset-env babel-preset-stage-2 big-integer browserify camel-case coffeescript eslint eslint-config-google html-minifier qunit qunit-cli requirejs tslint typescript uglify-es \
+    @types/jquery @types/qunit \
