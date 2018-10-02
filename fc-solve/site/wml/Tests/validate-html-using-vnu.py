4cd3a3347dc087bd4e20441af46bf8d2aa35efb8
Shlomi Fish <shlomif@shlomifish.org>
2018-10-02 23:38:32 +0300

    fix bugs.

--- fc-solve/site/wml/Tests/validate-html-using-vnu.py
+++ fc-solve/site/wml/Tests/validate-html-using-vnu.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
+"""
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2018 by Shlomi Fish
+
+This program is distributed under the MIT / Expat License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+=cut
+"""
 
 import os
 from os.path import join
@@ -22,14 +54,15 @@ class VnuValidate:
             dn = join(t.name, dirpath)
             os.makedirs(dn)
             for fn in fns:
+                print(fn)
                 path = join(dirpath, fn)
-                html = re.match(r'\.html?$', fn)
-                if re.match(r'\.xhtml$', fn) or (
-                        html and not re.match(self.non_xhtml_re, path)):
-                    open(join(dn, fn), 'w').write(
-                        open(join(dirpath,
-                                  re.sub('\.[^\.]*$', '.xhtml',
-                                         fn)).read()))
+                html = re.match(r'.*\.html?$', fn)
+                if re.match('.*\\.xhtml$', fn) or (
+                        html and not re.search(self.non_xhtml_re, path)):
+                    print(fn)
+                    open(join(dn, re.sub('\.[^\.]*$', '.xhtml',
+                                         fn)), 'w').write(
+                        open(join(dirpath, fn)).read())
                 elif html:
                     open(join(dn, fn), 'w').write(
                         open(path).read())
