7407f2ef3bd02c4e582747fd7f810986b0b6a2e7
Shlomi Fish <shlomif@shlomifish.org>
2018-10-03 20:55:50 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- fc-solve/site/wml/bin/gen-web-fc-solve-tests--texts-dictionary.pl
+++ fc-solve/site/wml/bin/gen-web-fc-solve-tests--texts-dictionary.pl
@@ -13,4 +13,4 @@ my %TEXTS = ( map { $_ => path("$dir/texts/$_")->slurp_utf8 }
         path("$dir/texts-lists.txt")->lines_utf8( { chomp => 1 } ) );
 
 path("src/js/web-fcs-tests-strings.ts")->spew_utf8( "export const dict = ",
-    JSON::MaybeXS->new->utf8->canonical(1)->encode( \%TEXTS ), ";" );
+    JSON::MaybeXS->new->ascii->canonical(1)->encode( \%TEXTS ), ";" );
