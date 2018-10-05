5e4e8c5d2732cb8a1ec1765d721f95eece182bba
Shlomi Fish <shlomif@shlomifish.org>
2018-10-05 23:37:54 +0300

    generalise

--- fc-solve/scripts/validate-html-using-vnu.bash
+++ fc-solve/scripts/validate-html-using-vnu.bash
@@ -3,12 +3,12 @@ set -e
 echo "1..1"
 dir=../dest-xh
 rm -fr "$dir"
-cp -R ../wml/dest "$dir"
+cp -R "$1" "$dir"
 find "$dir" -name '*.html' | grep -v jquery-ui | (while read fn
 do
     mv "$fn" "${fn%%.html}.xhtml"
 done)
-if java -jar build/dist/vnu.jar --Werror --skip-non-html "$dir"
+if java -jar ~/Download/unpack/net/www/validator/build/dist/vnu.jar --Werror --skip-non-html "$dir"
 then
     echo "ok 1"
 else
