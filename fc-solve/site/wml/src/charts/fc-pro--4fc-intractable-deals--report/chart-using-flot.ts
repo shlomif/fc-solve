faf6944f7f1cdc105b0d76463e333b1a6d3201d9
Shlomi Fish <shlomif@shlomifish.org>
2018-10-17 20:20:58 +0300

    reformat per prettier.

--- fc-solve/site/wml/src/charts/fc-pro--4fc-intractable-deals--report/chart-using-flot.ts
+++ fc-solve/site/wml/src/charts/fc-pro--4fc-intractable-deals--report/chart-using-flot.ts
@@ -29,28 +29,29 @@ function chart_data(data_selector: string, chart_selector: string) {
 
     $("<div id='tooltip'></div>")
         .css({
-            'background-color': "#fee",
-            'border': "1px solid #fdd",
-            'display': "none",
-            'opacity': 0.80,
-            'padding': "2px",
-            'position': "absolute" })
+            "background-color": "#fee",
+            border: "1px solid #fdd",
+            display: "none",
+            opacity: 0.8,
+            padding: "2px",
+            position: "absolute",
+        })
         .appendTo("body");
 
     $(chart_selector).bind("plothover", (event, pos, item) => {
-
         {
-            const str = "(" + pos.x.toFixed(2) + ", " + pos.y.toFixed(2) + ")";
+            const str = `(${pos.x.toFixed(2)},${pos.y.toFixed(2)})`;
             $("#hoverdata").text(str);
         }
 
         {
             if (item) {
-                const x = (item.datapoint[0] / 1000000);
+                const x = item.datapoint[0] / 1000000;
                 const y = item.datapoint[1].toFixed(2);
 
-                $("#tooltip").html(item.series.label + " of " + x.toFixed(1) + "M = " + y + "sec")
-                    .css({top: item.pageY + 5, left: item.pageX + 5})
+                $("#tooltip")
+                    .html(`${item.series.label} of ${x.toFixed(1)} M = ${y}sec`)
+                    .css({ top: item.pageY + 5, left: item.pageX + 5 })
                     .fadeIn(200);
             } else {
                 $("#tooltip").hide();
@@ -61,7 +62,9 @@ function chart_data(data_selector: string, chart_selector: string) {
 
     $(chart_selector).bind("plotclick", (event, pos, item) => {
         if (item) {
-            $("#clickdata").text(" - click point " + item.dataIndex + " in " + item.series.label);
+            $("#clickdata").text(
+                ` - click point ${item.dataIndex} in ${item.series.label}`,
+            );
             plot.highlight(item.series, item.datapoint);
         }
     });
