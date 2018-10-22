d8a18ac9237927adb2911b979844c9fdb6044abb
Shlomi Fish <shlomif@shlomifish.org>
2018-10-23 01:57:37 +0300

    Add fc-solve-multi.
    It is an experimental solver that solves more than one file at a time
    - useful in combination with
    fc-solve/source/board_gen/gen-multiple-pysol-layouts.py and with xargs
    or https://en.wikipedia.org/wiki/GNU_parallel or
    https://github.com/mmstick/parallel .

---
+++ fc-solve/source/multi_fc_solve_main.c
@@ -0,0 +1,15 @@
+/*
+ * This file is part of Freecell Solver. It is subject to the license terms in
+ * the COPYING.txt file found in the top-level directory of this distribution
+ * and at http://fc-solve.shlomifish.org/docs/distro/COPYING.html . No part of
+ * Freecell Solver, including this file, may be copied, modified, propagated,
+ * or distributed except according to the terms contained in the COPYING file.
+ *
+ * Copyright (c) 2000 Shlomi Fish
+ */
+// main.c - implement the "fc-solve" command line executable.
+//
+// It is documented in the documents "README", "USAGE", etc. in the
+// Freecell Solver distribution from http://fc-solve.shlomifish.org/ .
+#include "multi_cl_callback.h"
+#include "default_iter_handler.h"
