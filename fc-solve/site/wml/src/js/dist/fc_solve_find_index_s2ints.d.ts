4c6dfed7da8abdd9cdfb526582cbdcd8297ce881
Shlomi Fish <shlomif@shlomifish.org>
2018-11-01 12:50:09 +0200

    remove unused gen code.

--- fc-solve/site/wml/src/js/dist/fc_solve_find_index_s2ints.d.ts
+++ fc-solve/site/wml/src/js/dist/fc_solve_find_index_s2ints.d.ts
@@ -9,28 +9,3 @@ export as namespace myLib;
 /*~ If this module has methods, declare them as functions like so.
  */
 export function find_index__board_string_to_ints(ms_deal_24: string): [number];
-export function myOtherMethod(a: number): number;
-
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
-}
-
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    export function foo(): void;
-}
