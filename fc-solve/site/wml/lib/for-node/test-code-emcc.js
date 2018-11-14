c79b9c4f8bcbcf135e83e78c70281e9ccfa3e962
Shlomi Fish <shlomif@shlomifish.org>
2018-11-14 17:05:02 +0200

    Extract a script.
    To please qunit -  it started the tap output again.

---
+++ fc-solve/site/wml/lib/for-node/test-code-emcc.js
@@ -0,0 +1,12 @@
+if (process.env.SKIP_EMCC != '1') {
+    const test_code = require('web-fc-solve-tests');
+    test_code.test_js_fc_solve_class(QUnit, function() {
+        return;
+    });
+} else {
+    QUnit.test((a) => {
+        a.expect(1);
+
+        a.ok(true, 'skipped');
+    });
+}
--- fc-solve/site/wml/lib/for-node/test-code.js
+++ fc-solve/site/wml/lib/for-node/test-code.js
@@ -1,18 +1,2 @@
-var requirejs = require('requirejs');
-
-requirejs.config({
-    //Pass the top-level main.js/index.js require
-    //function to requirejs so that node modules
-    //are loaded relative to the top-level JS file.
-    nodeRequire: require
-});
-
 var test_valid = require('web-fc-solve-tests--fcs-validate');
 test_valid.test_fcs_validate(QUnit);
-// define = requirejs.define;
-if (process.env.SKIP_EMCC != '1') {
-    var test_code = require('web-fc-solve-tests');
-    test_code.test_js_fc_solve_class(QUnit, function () {
-        return;
-    });
-}
