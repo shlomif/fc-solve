fdafcba655f6f08fceacc3e067ae9f113cc11a80
Shlomi Fish <shlomif@shlomifish.org>
2019-05-06 21:18:12 +0300

    continue to convert site from wml to jinja2

--- fc-solve/site/wml/bin/jinja-render.py
+++ fc-solve/site/wml/bin/jinja-render.py
@@ -18,8 +18,10 @@ env = Environment(
 for line in open('lib/make/jinja.txt', 'rt'):
     fn = line.strip()
     template = env.get_template(fn+'.jinja')
+    base_path = "../"*len([x for x in fn if x == '/'])
     open('dest/'+fn, 'wt').write(
         template.render(
+            base_path=base_path,
             common_keywords="Freecell, Freecell Solver, solvers, " +
             "AI, artificial intelligence, solitaire, Simple Simon, " +
             "Baker's Game, Seahaven Towers, Shlomi Fish, games",
@@ -28,5 +30,9 @@ for line in open('lib/make/jinja.txt', 'rt'):
             " Solving Games of Freecell and similar Solitaire Variants",
             filename=fn,
             host='fc-solve',
-            base_path="../"*len([x for x in fn if x == '/'])
+            solitairey='<a href="https://foss-card-games.github.io/' +
+            'Solitairey/">Solitairey</a>',
+            presentation_url="http://www.shlomifish.org/" +
+            "lecture/Freecell-Solver/",
+            doxygen_url=base_path + "michael_mann/",
             ))
