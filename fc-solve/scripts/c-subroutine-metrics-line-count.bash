cd82719d2e32211e02cd1b1d25b90150599702da
Shlomi Fish <shlomif@shlomifish.org>
2019-05-11 18:50:33 +0300

    list from all files

--- fc-solve/scripts/c-subroutine-metrics-line-count.bash
+++ fc-solve/scripts/c-subroutine-metrics-line-count.bash
@@ -2,4 +2,4 @@ metrix=~/Download/unpack/prog/mainline/metrix++.py
 pym="python2 $metrix"
 db=~/mpp.db
 $pym collect --db-file="$db"
-$pym export --db-file="$db" | perl -lnaF/,/ -E 'if ($F[0] eq "./lib.c"){say $F[-1]-$F[-2], "\t", $F[1];}' | sort -n
+$pym export --db-file="$db" | perl -lnaF/,/ -E 'if ($F[0] and $F[1] and $F[1] ne "__global__"){say $F[-1]-$F[-2], "\t", $F[1], "\t", $F[0];}' | sort -n
