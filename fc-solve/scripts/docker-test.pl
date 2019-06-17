28520d028aabf0bb1a1949c4303c04ec093d3344
Shlomi Fish <shlomif@shlomifish.org>
2019-06-17 20:22:46 +0300

    more progress with the fedora build-process run.

--- fc-solve/scripts/docker-test.pl
+++ fc-solve/scripts/docker-test.pl
@@ -18,7 +18,7 @@ sub do_system
     my ($args) = @_;
 
     my $cmd = $args->{cmd};
-    print "Running [@$cmd]";
+    print "Running [@$cmd]\n";
     if ( system(@$cmd) )
     {
         die "Running [@$cmd] failed!";
@@ -29,14 +29,18 @@ my @deps = map { /^BuildRequires:\s*(\S+)/ ? ("'$1'") : () }
     path("freecell-solver.spec.in")->lines_utf8;
 my $SYS = "fedora:31";
 do_system( { cmd => [ 'docker', 'pull', $SYS ] } );
+do_system(
+    { cmd => [ 'docker', 'run', "-t", "-d", "--name", "fcsfed", $SYS, ] } );
+do_system( { cmd => [ 'docker', 'cp', "../..", "fcsfed:fc-solve", ] } );
 my $script = <<"EOSCRIPTTTTTTT";
 set -e -x
 sudo dnf -y install cmake gcc gcc-c++ git glibc-devel libcmocka-devel make perl-autodie perl-Path-Tiny @deps
-git clone https://github.com/shlomif/fc-solve.git
 mkdir b
 cd b
 perl ../fc-solve/fc-solve/scripts/Tatzer
 make
-FCS_TEST_BUILD=1 perl ../fc-solve/fc-solve/source/run-tests.pl --glob='build-process.t'
+FCS_TEST_BUILD=1 perl ../fc-solve/fc-solve/source/run-tests.pl --glob='build*.t'
 EOSCRIPTTTTTTT
-do_system( { cmd => [ 'docker', 'run', $SYS, 'bash', '-c', $script, ] } );
+
+# do_system( { cmd => [ 'docker', 'start', "fcsfed", ] } );
+do_system( { cmd => [ 'docker', 'exec', "fcsfed", 'bash', '-c', $script, ] } );
