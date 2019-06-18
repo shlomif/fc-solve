b79f71268561a9cf77958758bc42c2aec83d493e
Shlomi Fish <shlomif@shlomifish.org>
2019-06-18 11:32:21 +0300

    add docker fedora build proc test

---
+++ fc-solve/scripts/ci-docker-test--install.bash
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# ci-before-install.bash
+# Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under terms of the MIT license.
+#
+set -e -x
+cpanm local::lib
+eval "$(perl -Mlocal::lib=$HOME/perl_modules)"
+cpanm --notest Path::Tiny
