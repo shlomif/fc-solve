77e0764287a96d93d93a91211926192d1a0a094b
Shlomi Fish <shlomif@shlomifish.org>
2019-07-22 22:02:50 +0300

    travis debug #2: remove temp debug

--- fc-solve/source/run-tests.pl
+++ fc-solve/source/run-tests.pl
@@ -49,10 +49,13 @@ sub run_tests
     my $tests = shift;
 
     my @cmd = ( ( $use_prove ? @{ _calc_prove() } : 'runprove' ), @$tests );
-    @cmd = (
-        ( $use_prove ? @{ _calc_prove() } : 'runprove' ),
-        '-v', grep { /build-proc/ || /\.py\z/ } @$tests
-    );
+    if (0)
+    {
+        @cmd = (
+            ( $use_prove ? @{ _calc_prove() } : 'runprove' ),
+            '-v', grep { /build-proc/ || /\.py\z/ } @$tests
+        );
+    }
     if ( $ENV{RUN_TESTS_VERBOSE} )
     {
         print "Running [@cmd]\n";
