175688ef7653ab3e2f1943820f5348638b546cec
Shlomi Fish <shlomif@shlomifish.org>
2019-12-11 14:27:39 +0200

    debug emcc

--- fc-solve/site/wml/run2.bash
+++ fc-solve/site/wml/run2.bash
@@ -1,1 +1,4 @@
-gmake ; env NODE_PATH="`pwd`"/lib/for-node/js NODE_NO_READLINE=1 node-inspect ./qunit.js lib/for-node/test-code-emcc.js < /home/shlomif/in.txt
+set -e -x
+gmake
+export NODE_PATH="`pwd`"/lib/for-node/js NODE_NO_READLINE=1
+node-inspect ./qunit.js lib/for-node/test-code-emcc.js < "./in.txt"
