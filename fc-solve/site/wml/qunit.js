492e1d74f008acb99182cd8ae9de8ecf8c1456a2
Shlomi Fish <shlomif@shlomifish.org>
2019-12-11 11:55:25 +0200

    debug emcc

--- fc-solve/site/wml/qunit.js
+++ fc-solve/site/wml/qunit.js
@@ -7,31 +7,10 @@ const path = require( "path" );
 const resolve = require( "resolve" );
 
 function requireQUnit() {
-	try {
-
 		// First we attempt to find QUnit relative to the current working directory.
 		const localQUnitPath = resolve.sync( "qunit", { basedir: process.cwd() } );
 		delete require.cache[ localQUnitPath ];
 		return require( localQUnitPath );
-	} catch ( e ) {
-		try {
-
-			// Second, we use the globally installed QUnit
-			delete require.cache[ resolve.sync( "../../qunit/qunit" ) ];
-			// eslint-disable-next-line node/no-missing-require, node/no-unpublished-require
-			return require( "../../qunit/qunit" );
-		} catch ( e ) {
-			if ( e.code === "MODULE_NOT_FOUND" ) {
-
-				// Finally, we use the local development version of QUnit
-				delete require.cache[ resolve.sync( "../../dist/qunit" ) ];
-				// eslint-disable-next-line node/no-missing-require, node/no-unpublished-require
-				return require( "../../dist/qunit" );
-			}
-
-			throw e;
-		}
-	}
 };
 
 function run( args, reporter ) {
