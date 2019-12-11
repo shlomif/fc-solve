fcf8b88be1554827ce046f0736b61fa198151b37
Shlomi Fish <shlomif@shlomifish.org>
2019-12-11 11:18:11 +0200

    debug emcc

--- fc-solve/site/wml/qunit.js
+++ fc-solve/site/wml/qunit.js
@@ -7,32 +7,8 @@ const run = require( "qunit/src/cli/run" );
 const JSReporters = require( "js-reporters" );
 const utils = require( "qunit/src/cli/utils" );
 
-function findReporter( reporterName ) {
-	if ( !reporterName ) {
-		return JSReporters.TapReporter;
-	}
-
-	// First, check if the reporter is one of the standard js-reporters ones
-
-	const capitalizedName = utils.capitalize( reporterName );
-	const jsReporterName = `${capitalizedName}Reporter`;
-
-	if ( JSReporters[ jsReporterName ] ) {
-		return JSReporters[ jsReporterName ];
-	}
-
-	// Second, check if the reporter is an npm package
-	try {
-		return require( reporterName );
-	} catch ( e ) {
-		if ( e.code !== "MODULE_NOT_FOUND" ) {
-			throw e;
-		}
-	}
-}
-
 const options = {
-	reporter: findReporter( program.reporter ),
+	reporter: JSReporters.TapReporter,
 	requires: [],
 };
 
