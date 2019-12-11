87850acc724a958ba9ec823b32e39519f0f171bf
Shlomi Fish <shlomif@shlomifish.org>
2019-12-11 15:14:42 +0200

    debug emcc

--- fc-solve/site/wml/RUN_ME.bash
+++ fc-solve/site/wml/RUN_ME.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e -x
 gmake
-NODE_PATH="`pwd`"/lib/for-node/js ./qunit.js lib/for-node/test-code-emcc.js
+NODE_PATH="`pwd`"/lib/for-node/js strace -e openat -f -o q.strace ./qunit.js lib/for-node/test-code-emcc.js
