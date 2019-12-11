7f328f9d6b53a89c1d66870679177be5a637f4d0
Shlomi Fish <shlomif@shlomifish.org>
2019-12-11 11:18:53 +0200

    debug emcc

--- fc-solve/site/wml/qunit.js
+++ fc-solve/site/wml/qunit.js
@@ -5,7 +5,6 @@
 const program = require( "commander" );
 const run = require( "qunit/src/cli/run" );
 const JSReporters = require( "js-reporters" );
-const utils = require( "qunit/src/cli/utils" );
 
 const options = {
 	reporter: JSReporters.TapReporter,
