f91cb82a18eff430e654e3d6059be35e83c82551
Shlomi Fish <shlomif@shlomifish.org>
2019-12-17 20:12:59 +0200

    add the new scripts. see:
    https://github.com/nodejs/node-inspect/blob/af55a32c0f91c7e7214ad816313c6ea4ed2e50e2/test/cli/exec.test.js#L14-L16
    
    https://github.com/nodejs/node-inspect/issues/74 .

---
+++ fc-solve/site/wml/bin/node_inspect_debugger_exec.js
@@ -0,0 +1,55 @@
+'use strict';
+const { test } = require('tap');
+
+const startCLI = require('./start-cli');
+
+test('examples/alive.js', (t) => {
+    const cli = startCLI(['node_modules/.bin/qunit', 'lib/for-node/test-code-emcc.js']);
+
+  function onFatal(error) {
+    cli.quit();
+    throw error;
+  }
+
+  return cli.waitForInitialBreak()
+    .then(() => cli.waitForPrompt())
+/*    .then(() => cli.command('exec [typeof heartbeat, typeof process.exit]'))
+    .then(() => {
+      t.match(cli.output, '[ \'function\', \'function\' ]', 'works w/o paren');
+    }) */
+    /*.then(() => cli.command('repl'))
+    .then(() => {
+      t.match(
+        cli.output,
+        'Press Ctrl + C to leave debug repl\n> ',
+        'shows hint for how to leave repl');
+      t.notMatch(cli.output, 'debug>', 'changes the repl style');
+    })*/
+    .then(() => cli.command('run'))
+    // .then(() => cli.waitFor(/function/))
+    .then(() => cli.waitForPrompt())
+    /*.then(() => {
+      t.match(
+        cli.output,
+        '[ \'function\', \'function\' ]', 'can evaluate in the repl');
+      t.match(cli.output, /> $/);
+    })
+    .then(() => cli.ctrlC())
+    .then(() => cli.waitFor(/debug> $/))
+    .then(() => cli.command('exec("[typeof heartbeat, typeof process.exit]")'))
+    .then(() => {
+      t.match(cli.output, '[ \'function\', \'function\' ]', 'works w/ paren');
+    })
+    .then(() => cli.command('cont'))
+    .then(() => cli.command('exec [typeof heartbeat, typeof process.exit]'))
+    .then(() => {
+      t.match(
+        cli.output,
+        '[ \'undefined\', \'function\' ]',
+        'non-paused exec can see global but not module-scope values');
+    })
+    */
+    .then(() => cli.quit())
+    .then(null, onFatal);
+});
+
