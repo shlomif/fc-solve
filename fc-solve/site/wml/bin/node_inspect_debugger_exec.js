a0040b2e5c13c84867333a3f4e754414f9366916
Shlomi Fish <shlomif@shlomifish.org>
2019-12-22 19:05:39 +0200

    Extract a common class/struct.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://en.wikipedia.org/wiki/God_object
    
    * https://en.wikipedia.org/wiki/Extract_class
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- fc-solve/site/wml/bin/node_inspect_debugger_exec.js
+++ fc-solve/site/wml/bin/node_inspect_debugger_exec.js
@@ -6,66 +6,34 @@ const startCLI = require('./start-cli');
 test('qunit-emcc-fcs', (t) => {
     const cli = startCLI(['node_modules/.bin/qunit', 'lib/for-node/test-code-emcc.js']);
 
-  function onFatal(error) {
-    cli.quit();
-    throw error;
-  }
+    function onFatal(error) {
+        cli.quit();
+        throw error;
+    }
     const timeout = 20*1000;
 
-  return cli.waitForInitialBreak(timeout)
-    .then(() => cli.waitForPrompt(timeout))
-/*    .then(() => cli.command('exec [typeof heartbeat, typeof process.exit]'))
-    .then(() => {
-      t.match(cli.output, '[ \'function\', \'function\' ]', 'works w/o paren');
-    }) */
-    /*.then(() => cli.command('repl'))
-    .then(() => {
-      t.match(
-        cli.output,
-        'Press Ctrl + C to leave debug repl\n> ',
-        'shows hint for how to leave repl');
-      t.notMatch(cli.output, 'debug>', 'changes the repl style');
-    })*/
-    .then(() => cli.command('sb("lib/for-node/js/web-fc-solve-tests.js", 2)'))
-    .then(() => cli.command('sb("lib/for-node/js/libfreecell-solver.min.js", 1833)'))
-    .then(() => cli.command('cont'))
-    .then(() => {console.log(cli.output);})
-    // .then(() => cli.waitFor(/debug> $/))
-    // .then(() => cli.command('cont'))
-    // .then(() => cli.command('cont'))
-    // .then(() => cli.waitFor(/function/))
-    .then(() => cli.waitForPrompt(timeout))
-    .then(() => cli.command('cont'))
-    .then(() => cli.waitForPrompt(timeout))
-    // .then(() => cli.waitFor(/No|throw/, timeout))
-    .then(() => {
-        console.log(cli.output);
-        // console.log(cli);
-    })
-    .then(() => cli.waitForPrompt(timeout))
-    /*.then(() => {
-      t.match(
-        cli.output,
-        '[ \'function\', \'function\' ]', 'can evaluate in the repl');
-      t.match(cli.output, /> $/);
-    })
-    .then(() => cli.ctrlC())
-    .then(() => cli.waitFor(/debug> $/))
-    .then(() => cli.command('exec("[typeof heartbeat, typeof process.exit]")'))
-    .then(() => {
-      t.match(cli.output, '[ \'function\', \'function\' ]', 'works w/ paren');
-    })
-    .then(() => cli.command('cont'))
-    .then(() => cli.command('exec [typeof heartbeat, typeof process.exit]'))
-    .then(() => {
-      t.match(
-        cli.output,
-        '[ \'undefined\', \'function\' ]',
-        'non-paused exec can see global but not module-scope values');
-    })
-    */
-    .then(() => {console.log(cli.output);})
-    .then(() => cli.quit())
-    .then(null, onFatal);
+    class Queue {
+        constructor() {
+            const that = this;
+            that.idx = 0;
+            that.cmds =['sb("lib/for-node/js/web-fc-solve-tests.js", 2)',
+'sb("lib/for-node/js/libfreecell-solver.min.js", 1833)',
+            'cont',
+        ];
+        };
+        run() {
+            const that = this;
+            cli.command(that.cmds[that.idx++]) .then(() => cli.waitForPrompt(timeout)) .then(() => {console.log(cli.output);}) .then(() => {that.run()});
+        }
+    }
+
+    const queue = new Queue();
+
+    return cli.waitForInitialBreak(timeout)
+        .then(() => cli.waitForPrompt(timeout))
+//        .then(() => {queue.run();})
+        .then(() => {console.log(cli.output);})
+        .then(() => cli.quit())
+        .then(null, onFatal);
 });
 
