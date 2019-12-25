f157df2432bcbd3ab697a950189c1d797f259731
Shlomi Fish <shlomif@shlomifish.org>
2019-12-25 17:28:23 +0200

    Try to fix valgrind arch instructions mismatch
    See: https://travis-ci.org/shlomif/fc-solve/jobs/629396105 .
    
    We get:
    
    <<<<
    LAST    # vex amd64->IR: unhandled instruction bytes: 0x62 0xF1 0xFE 0x8
    0x6F 0x0 0xC5 0xF8 0x11 0x83
    LAST    # vex amd64->IR:   REX=0 REX.W=0 REX.R=0 REX.X=0 REX.B=0
    LAST    # vex amd64->IR:   VEX=0 VEX.L=0 VEX.nVVVV=0x0 ESC=NONE
    LAST    # vex amd64->IR:   PFX.66=0 PFX.F2=0 PFX.F3=0
    LAST    # ==17533== valgrind: Unrecognised instruction at address
    0x110102.
    LAST    # ==17533==    at 0x110102: user_initialize (lib.c:2376)
    LAST    # ==17533==    by 0x110102: freecell_solver_user_alloc
    (lib.c:2414)
    LAST    # ==17533==    by 0x10A726: alloc_instance_and_parse
    (handle_parsing.h:36)
    LAST    # ==17533==    by 0x10A726: main (cl_callback.h:53)
    LAST    # ==17533== Your program just tried to execute an instruction
    that Valgrind
    LAST    # ==17533== did not recognise.  There are two possible reasons
    for this.
    LAST    # ==17533== 1. Your program has a bug and erroneously jumped to
    a non-code
    LAST    # ==17533==    location.  If you are running Memcheck and you
    just saw a
    LAST    # ==17533==    warning about a bad jump, it's probably your
    program's fault.
    LAST    # ==17533== 2. The instruction is legitimate but Valgrind
    doesn't handle it,
    LAST    # ==17533==    i.e. it's Valgrind's fault.  If you think this is
    the case or
    LAST    # ==17533==    you are not sure, please let us know and we'll
    try to fix it.
    LAST    # ==17533== Either way, Valgrind will now raise a SIGILL signal
    which will
    LAST    # ==17533== probably kill your program.
    LAST    # ==17533==
    LAST    # ==17533== Process terminating with default action of signal 4
    (SIGILL)
    LAST    # ==17533==  Illegal opcode at address 0x110102
    LAST    # ==17533==    at 0x110102: user_initialize (lib.c:2376)
    LAST    # ==17533==    by 0x110102: freecell_solver_user_alloc
    (lib.c:2414)
    LAST    # ==17533==    by 0x10A726: alloc_instance_and_parse
    (handle_parsing.h:36)
    LAST    # ==17533==    by 0x10A726: main (cl_callback.h:53)
    LAST    # ==17533==
    >>>

--- fc-solve/scripts/run-ci-tests.bash
+++ fc-solve/scripts/run-ci-tests.bash
@@ -38,4 +38,10 @@ enable_reproducible_builds
 export FC_SOLVE__TESTS_RERUNS_CACHE_DATA_DIR="$HOME/fc-solve-tests-reruns-cache"
 cd fc-solve/source
 echo "\$HOME = < $HOME > ; PWD = < $PWD > ; TRAVIS_BUILD_DIR = < $TRAVIS_BUILD_DIR >"
+# For avoiding valgrind issues on travis:
+avoid_valgrind_issues_on_travis()
+{
+    export FC_SOLVE__MULT_CONFIG_TESTS__GCC_ARCH="x64"
+}
+avoid_valgrind_issues_on_travis
 perl ../scripts/multi_config_tests.pl 2>&1 | zenfilter --count-step=500 --last=500 --filter='^Running:\s*\{' --suppress-last-on="All tests successful\\.\\n*\\Z"
