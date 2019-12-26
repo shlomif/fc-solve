a9c493800950fdf5517f68fa3a96d0af52fb376e
Shlomi Fish <shlomif@shlomifish.org>
2019-12-26 23:02:18 +0200

    taking an approach of using expect.

--- fc-solve/site/wml/run-me.bash
+++ fc-solve/site/wml/run-me.bash
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 export NODE_PATH="`pwd`/lib/for-node/js"
-node bin/node_inspect_debugger_exec.js
+# node bin/node_inspect_debugger_exec.js
+expect -f bin/expect--node-inspect--auto.expect
