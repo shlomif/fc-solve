46bc1a05ce0882f230141c11ce99cb115f41326a
Shlomi Fish <shlomif@shlomifish.org>
2019-12-27 12:51:26 +0200

    Merged qunit/emcc fix/workaround.

--- fc-solve/site/wml/lib/for-node/test-code-emcc.js
+++ fc-solve/site/wml/lib/for-node/test-code-emcc.js
@@ -1,6 +1,6 @@
 if (process.env.SKIP_EMCC != '1') {
-    // QUnit.config.autostart = false;
-    QUnit.config.notrycatch = 1;
+    QUnit.config.autostart = false;
+    // QUnit.config.notrycatch = 1;
     const test_code = require('web-fc-solve-tests');
     test_code.test_js_fc_solve_class(QUnit, function() {
         return;
