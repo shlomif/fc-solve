aa2b87ab18927a0a13be48aedd94695c849443d9
Shlomi Fish <shlomif@shlomifish.org>
2019-12-31 12:56:26 +0200

    package.json - avoid args to npm i

--- fc-solve/site/wml/bin/install-npm-deps.sh
+++ fc-solve/site/wml/bin/install-npm-deps.sh
@@ -4,10 +4,13 @@
 #
 # Distributed under terms of the MIT license.
 #
-npm install \
-    amdefine big-integer browserify camel-case coffeescript eslint eslint-config-google flatted html-minifier jquery prettier qunit qunit-cli requirejs rollup sass tslint tslint-config-prettier tslint-config-radiosilence tslint-plugin-prettier typescript uglify-es yarn \
-    shlomif/qunit-puppeteer \
-    @babel/cli @babel/core @babel/preset-env \
-    @types/jquery @types/qunit \
-    @typescript-eslint/parser \
-    @typescript-eslint/eslint-plugin \
+set -e
+npm install
+exit
+
+#     amdefine big-integer browserify camel-case coffeescript eslint eslint-config-google flatted html-minifier jquery prettier qunit qunit-cli requirejs rollup sass tslint tslint-config-prettier tslint-config-radiosilence tslint-plugin-prettier typescript uglify-es yarn \
+#     shlomif/qunit-puppeteer \
+#     @babel/cli @babel/core @babel/preset-env \
+#     @types/jquery @types/qunit \
+#     @typescript-eslint/parser \
+#     @typescript-eslint/eslint-plugin \
