324d1ed0a10af52b5b4f7a0fefcbc817f7a44072
Shlomi Fish <shlomif@shlomifish.org>
2020-02-03 18:46:13 +0200

    update.

--- cpan/Games-Solitaire-Verify/benchmark/par-Source-Me.bash
+++ cpan/Games-Solitaire-Verify/benchmark/par-Source-Me.bash
@@ -1,2 +1,3 @@
 rm -f Results/*.res
-sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; time make -j4 -f par.mak"
+# sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; time make -j4 -f par.mak"
+sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; time ninja -j4"
