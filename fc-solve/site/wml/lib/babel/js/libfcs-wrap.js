e9d0e778518644149880b10c38f4087b22d138d5
Shlomi Fish <shlomif@shlomifish.org>
2020-02-09 11:22:06 +0200

    Fix the indentation.
    See http://perl-begin.org/tutorials/bad-elements/#no-indentation .

--- fc-solve/site/wml/lib/babel/js/libfcs-wrap.js
+++ fc-solve/site/wml/lib/babel/js/libfcs-wrap.js
@@ -10,8 +10,8 @@ function mydef(x, y) {
 // thanks!
 const supported = (() => {
     try {
-        if (typeof WebAssembly === "object"
-            && typeof WebAssembly.instantiate === "function") {
+        if (typeof WebAssembly === "object" &&
+            typeof WebAssembly.instantiate === "function") {
             const module = new WebAssembly.Module(
                 Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
             if (module instanceof WebAssembly.Module) {
