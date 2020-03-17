4ea875b9ae554d2db5facef6b3ba4d7076397551
Shlomi Fish <shlomif@shlomifish.org>
2020-03-17 14:14:16 +0200

    Made the verify benchmark process less errorprone
    Add sanity checks.

--- cpan/Games-Solitaire-Verify/benchmark/par-Source-Me.bash
+++ cpan/Games-Solitaire-Verify/benchmark/par-Source-Me.bash
@@ -1,3 +1,6 @@
 rm -f Results/*.res
-# sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; time make -j4 -f par.mak"
-sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; time ninja -j4"
+if perl FC_Solve/PreSanityCheck.pm
+then
+    # sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; time make -j4 -f par.mak"
+    sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; export LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ; time ninja -j4"
+fi
