b4fc23689bf5e56a0d2b32a208b09b641b70eda6
Shlomi Fish <shlomif@shlomifish.org>
2020-03-17 22:05:33 +0200

    add a faster compile theme + more streamlining.
    made sure the usability is better.

--- cpan/Games-Solitaire-Verify/benchmark/par-Source-Me.bash
+++ cpan/Games-Solitaire-Verify/benchmark/par-Source-Me.bash
@@ -1,4 +1,8 @@
-rm -f Results/*.res
+( rm -f Results/*.res ) 2> /dev/null
+if ! test -e build.ninja
+then
+    perl gen-par-mak.pl
+fi
 if perl FC_Solve/PreSanityCheck.pm
 then
     # sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; time make -j4 -f par.mak"
