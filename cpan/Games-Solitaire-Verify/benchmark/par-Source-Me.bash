579c219576858786f28f662acee81e3dc040d207
Shlomi Fish <shlomif@shlomifish.org>
2020-03-17 14:44:43 +0200

    update

--- cpan/Games-Solitaire-Verify/benchmark/par-Source-Me.bash
+++ cpan/Games-Solitaire-Verify/benchmark/par-Source-Me.bash
@@ -2,5 +2,5 @@ rm -f Results/*.res
 if perl FC_Solve/PreSanityCheck.pm
 then
     # sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; time make -j4 -f par.mak"
-    sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; export LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ; time ninja -j4"
+    sudo_renice bash -c "export FCS_PATH=\"$b\"; export FCS_SRC_PATH=\"$c_src\"; export LD_LIBRARY_PATH=\"$LD_LIBRARY_PATH\" ; time ninja -j4"
 fi
