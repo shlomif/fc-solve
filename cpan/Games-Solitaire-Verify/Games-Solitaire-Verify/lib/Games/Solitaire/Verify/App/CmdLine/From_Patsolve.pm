2b7ee1ee8b39fab0616705e64fc65d0028b554de
Shlomi Fish <shlomif@shlomifish.org>
2020-04-27 12:14:44 +0300

    Extract a common class/struct.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://en.wikipedia.org/wiki/God_object
    
    * https://en.wikipedia.org/wiki/Extract_class
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- cpan/Games-Solitaire-Verify/Games-Solitaire-Verify/lib/Games/Solitaire/Verify/App/CmdLine/From_Patsolve.pm
+++ cpan/Games-Solitaire-Verify/Games-Solitaire-Verify/lib/Games/Solitaire/Verify/App/CmdLine/From_Patsolve.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use autodie;
 
-use parent 'Games::Solitaire::Verify::Base';
+use parent 'Games::Solitaire::Verify::FromOtherSolversBase';
 
 use Games::Solitaire::Verify::VariantsMap      ();
 use Games::Solitaire::Verify::Solution         ();
@@ -12,8 +12,6 @@ use Games::Solitaire::Verify::State::LaxParser ();
 use Games::Solitaire::Verify::Move             ();
 
 use List::MoreUtils qw(firstidx);
-
-use Getopt::Long qw(GetOptionsFromArray);
 
 __PACKAGE__->mk_acc_ref(
     [
@@ -26,250 +24,6 @@ __PACKAGE__->mk_acc_ref(
             )
     ]
 );
-
-sub _init
-{
-    my ( $self, $args ) = @_;
-
-    my $argv = $args->{'argv'};
-
-    my $variant_map = Games::Solitaire::Verify::VariantsMap->new();
-
-    my $variant_params = $variant_map->get_variant_by_id("freecell");
-
-    GetOptionsFromArray(
-        $argv,
-        'g|game|variant=s' => sub {
-            my ( undef, $game ) = @_;
-
-            $variant_params = $variant_map->get_variant_by_id($game);
-
-            if ( !defined($variant_params) )
-            {
-                die "Unknown variant '$game'!\n";
-            }
-        },
-        'freecells-num=i' => sub {
-            my ( undef, $n ) = @_;
-            $variant_params->num_freecells($n);
-        },
-        'stacks-num=i' => sub {
-            my ( undef, $n ) = @_;
-            $variant_params->num_columns($n);
-        },
-        'decks-num=i' => sub {
-            my ( undef, $n ) = @_;
-
-            if ( !( ( $n == 1 ) || ( $n == 2 ) ) )
-            {
-                die "Decks should be 1 or 2.";
-            }
-
-            $variant_params->num_decks($n);
-        },
-        'sequences-are-built-by=s' => sub {
-            my ( undef, $val ) = @_;
-
-            my %seqs_build_by = (
-                ( map { $_ => $_ } (qw(alt_color suit rank)) ),
-                "alternate_color" => "alt_color",
-            );
-
-            my $proc_val = $seqs_build_by{$val};
-
-            if ( !defined($proc_val) )
-            {
-                die "Unknown sequences-are-built-by '$val'!";
-            }
-
-            $variant_params->seqs_build_by($proc_val);
-        },
-        'empty-stacks-filled-by=s' => sub {
-            my ( undef, $val ) = @_;
-
-            my %empty_stacks_filled_by_map =
-                ( map { $_ => 1 } (qw(kings any none)) );
-
-            if ( !exists( $empty_stacks_filled_by_map{$val} ) )
-            {
-                die "Unknown empty stacks filled by '$val'!";
-            }
-
-            $variant_params->empty_stacks_filled_by($val);
-        },
-        'sequence-move=s' => sub {
-            my ( undef, $val ) = @_;
-
-            my %seq_moves = ( map { $_ => 1 } (qw(limited unlimited)) );
-
-            if ( !exists( $seq_moves{$val} ) )
-            {
-                die "Unknown sequence move '$val'!";
-            }
-
-            $variant_params->sequence_move($val);
-        },
-    ) or die "Cannot process command line arguments";
-
-    my $filename = shift(@$argv);
-
-    if ( !defined($filename) )
-    {
-        $filename = "-";
-    }
-
-    my $sol_filename = shift(@$argv);
-
-    if ( !defined($sol_filename) )
-    {
-        die "Solution filename not specified.";
-    }
-
-    $self->_variant_params($variant_params);
-    $self->_filename($filename);
-    $self->_sol_filename($sol_filename);
-
-    my $s = '';
-    $self->_buffer_ref( \$s );
-
-    return;
-}
-
-sub _append
-{
-    my ( $self, $text ) = @_;
-
-    ${ $self->_buffer_ref } .= $text;
-
-    return;
-}
-
-sub _get_buffer
-{
-    my ($self) = @_;
-
-    return ${ $self->_buffer_ref };
-}
-
-sub _slurp
-{
-    my $filename = shift;
-
-    open my $in, '<', $filename
-        or die "Cannot open '$filename' for slurping - $!";
-
-    local $/;
-    my $contents = <$in>;
-
-    close($in);
-
-    return $contents;
-}
-
-sub _read_initial_state
-{
-    my $self = shift;
-
-    $self->_st(
-        Games::Solitaire::Verify::State::LaxParser->new(
-            {
-                string           => scalar( _slurp( $self->_filename ) ),
-                variant          => 'custom',
-                'variant_params' => $self->_variant_params(),
-            }
-        )
-    );
-
-    $self->_append("-=-=-=-=-=-=-=-=-=-=-=-\n\n");
-
-    $self->_out_running_state;
-
-    return;
-}
-
-sub _out_running_state
-{
-    my ($self) = @_;
-
-    $self->_append( $self->_st->to_string() . "\n\n====================\n\n" );
-
-    return;
-}
-
-sub _perform_and_output_move
-{
-    my ( $self, $move_s ) = @_;
-
-    $self->_append("$move_s\n\n");
-
-    $self->_st->verify_and_perform_move(
-        Games::Solitaire::Verify::Move->new(
-            {
-                fcs_string => $move_s,
-                game       => $self->_st->_variant(),
-            },
-        )
-    );
-    $self->_out_running_state;
-
-    return;
-}
-
-sub _find_col_card
-{
-    my ( $self, $card_s ) = @_;
-
-    return firstidx
-    {
-        my $col = $self->_st->get_column($_);
-        ( $col->len == 0 ) ? 0 : $col->top->fast_s eq $card_s
-    }
-    ( 0 .. $self->_st->num_columns - 1 );
-}
-
-sub _find_empty_col
-{
-    my ($self) = @_;
-
-    return firstidx
-    {
-        $self->_st->get_column($_)->len == 0
-    }
-    ( 0 .. $self->_st->num_columns - 1 );
-}
-
-sub _find_fc_card
-{
-    my ( $self, $card_s ) = @_;
-    my $dest_fc_idx = firstidx
-    {
-        my $card = $self->_st->get_freecell($_);
-        defined($card) ? ( $card->fast_s eq $card_s ) : 0;
-    }
-    ( 0 .. $self->_st->num_freecells - 1 );
-}
-
-sub _find_card_src_string
-{
-    my ( $self, $src_card_s ) = @_;
-
-    my $src_col_idx = $self->_find_col_card($src_card_s);
-
-    # TODO : try to find a freecell card.
-    if ( $src_col_idx < 0 )
-    {
-        my $src_fc_idx = $self->_find_fc_card($src_card_s);
-        if ( $src_fc_idx < 0 )
-        {
-            die "Cannot find card <$src_card_s>.";
-        }
-        return ( "a card", "freecell $src_fc_idx" );
-    }
-    else
-    {
-        return ( "1 cards", "stack $src_col_idx" );
-    }
-}
 
 sub _perform_move
 {
@@ -358,17 +112,6 @@ sub _process_main
     close($in_fh);
 
     $self->_append("This game is solveable.\n");
-
-    return;
-}
-
-sub run
-{
-    my ($self) = @_;
-
-    $self->_process_main;
-
-    print $self->_get_buffer;
 
     return;
 }
--- cpan/Games-Solitaire-Verify/Games-Solitaire-Verify/lib/Games/Solitaire/Verify/App/CmdLine/From_ShirlHartSolver.pm
+++ cpan/Games-Solitaire-Verify/Games-Solitaire-Verify/lib/Games/Solitaire/Verify/App/CmdLine/From_ShirlHartSolver.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use autodie;
 
-use parent 'Games::Solitaire::Verify::Base';
+use parent 'Games::Solitaire::Verify::FromOtherSolversBase';
 
 use Games::Solitaire::Verify::VariantsMap      ();
 use Games::Solitaire::Verify::Solution         ();
@@ -12,9 +12,6 @@ use Games::Solitaire::Verify::State::LaxParser ();
 use Games::Solitaire::Verify::Move             ();
 
 use List::Util qw(first);
-use List::MoreUtils qw(firstidx);
-
-use Getopt::Long qw(GetOptionsFromArray);
 
 __PACKAGE__->mk_acc_ref(
     [
@@ -28,250 +25,6 @@ __PACKAGE__->mk_acc_ref(
             )
     ]
 );
-
-sub _init
-{
-    my ( $self, $args ) = @_;
-
-    my $argv = $args->{'argv'};
-
-    my $variant_map = Games::Solitaire::Verify::VariantsMap->new();
-
-    my $variant_params = $variant_map->get_variant_by_id("freecell");
-
-    GetOptionsFromArray(
-        $argv,
-        'g|game|variant=s' => sub {
-            my ( undef, $game ) = @_;
-
-            $variant_params = $variant_map->get_variant_by_id($game);
-
-            if ( !defined($variant_params) )
-            {
-                die "Unknown variant '$game'!\n";
-            }
-        },
-        'freecells-num=i' => sub {
-            my ( undef, $n ) = @_;
-            $variant_params->num_freecells($n);
-        },
-        'stacks-num=i' => sub {
-            my ( undef, $n ) = @_;
-            $variant_params->num_columns($n);
-        },
-        'decks-num=i' => sub {
-            my ( undef, $n ) = @_;
-
-            if ( !( ( $n == 1 ) || ( $n == 2 ) ) )
-            {
-                die "Decks should be 1 or 2.";
-            }
-
-            $variant_params->num_decks($n);
-        },
-        'sequences-are-built-by=s' => sub {
-            my ( undef, $val ) = @_;
-
-            my %seqs_build_by = (
-                ( map { $_ => $_ } (qw(alt_color suit rank)) ),
-                "alternate_color" => "alt_color",
-            );
-
-            my $proc_val = $seqs_build_by{$val};
-
-            if ( !defined($proc_val) )
-            {
-                die "Unknown sequences-are-built-by '$val'!";
-            }
-
-            $variant_params->seqs_build_by($proc_val);
-        },
-        'empty-stacks-filled-by=s' => sub {
-            my ( undef, $val ) = @_;
-
-            my %empty_stacks_filled_by_map =
-                ( map { $_ => 1 } (qw(kings any none)) );
-
-            if ( !exists( $empty_stacks_filled_by_map{$val} ) )
-            {
-                die "Unknown empty stacks filled by '$val'!";
-            }
-
-            $variant_params->empty_stacks_filled_by($val);
-        },
-        'sequence-move=s' => sub {
-            my ( undef, $val ) = @_;
-
-            my %seq_moves = ( map { $_ => 1 } (qw(limited unlimited)) );
-
-            if ( !exists( $seq_moves{$val} ) )
-            {
-                die "Unknown sequence move '$val'!";
-            }
-
-            $variant_params->sequence_move($val);
-        },
-    ) or die "Cannot process command line arguments";
-
-    my $filename = shift(@$argv);
-
-    if ( !defined($filename) )
-    {
-        $filename = "-";
-    }
-
-    my $sol_filename = shift(@$argv);
-
-    if ( !defined($sol_filename) )
-    {
-        die "Solution filename not specified.";
-    }
-
-    $self->_variant_params($variant_params);
-    $self->_filename($filename);
-    $self->_sol_filename($sol_filename);
-
-    my $s = '';
-    $self->_buffer_ref( \$s );
-
-    return;
-}
-
-sub _append
-{
-    my ( $self, $text ) = @_;
-
-    ${ $self->_buffer_ref } .= $text;
-
-    return;
-}
-
-sub _get_buffer
-{
-    my ($self) = @_;
-
-    return ${ $self->_buffer_ref };
-}
-
-sub _slurp
-{
-    my $filename = shift;
-
-    open my $in, '<', $filename
-        or die "Cannot open '$filename' for slurping - $!";
-
-    local $/;
-    my $contents = <$in>;
-
-    close($in);
-
-    return $contents;
-}
-
-sub _read_initial_state
-{
-    my $self = shift;
-
-    $self->_st(
-        Games::Solitaire::Verify::State::LaxParser->new(
-            {
-                string           => scalar( _slurp( $self->_filename ) ),
-                variant          => 'custom',
-                'variant_params' => $self->_variant_params(),
-            }
-        )
-    );
-
-    $self->_append("-=-=-=-=-=-=-=-=-=-=-=-\n\n");
-
-    $self->_out_running_state;
-
-    return;
-}
-
-sub _out_running_state
-{
-    my ($self) = @_;
-
-    $self->_append( $self->_st->to_string() . "\n\n====================\n\n" );
-
-    return;
-}
-
-sub _perform_and_output_move
-{
-    my ( $self, $move_s ) = @_;
-
-    $self->_append("$move_s\n\n");
-
-    $self->_st->verify_and_perform_move(
-        Games::Solitaire::Verify::Move->new(
-            {
-                fcs_string => $move_s,
-                game       => $self->_st->_variant(),
-            },
-        )
-    );
-    $self->_out_running_state;
-
-    return;
-}
-
-sub _find_col_card
-{
-    my ( $self, $card_s ) = @_;
-
-    return firstidx
-    {
-        my $col = $self->_st->get_column($_);
-        ( $col->len == 0 ) ? 0 : $col->top->fast_s eq $card_s
-    }
-    ( 0 .. $self->_st->num_columns - 1 );
-}
-
-sub _find_empty_col
-{
-    my ($self) = @_;
-
-    return firstidx
-    {
-        $self->_st->get_column($_)->len == 0
-    }
-    ( 0 .. $self->_st->num_columns - 1 );
-}
-
-sub _find_fc_card
-{
-    my ( $self, $card_s ) = @_;
-    my $dest_fc_idx = firstidx
-    {
-        my $card = $self->_st->get_freecell($_);
-        defined($card) ? ( $card->fast_s eq $card_s ) : 0;
-    }
-    ( 0 .. $self->_st->num_freecells - 1 );
-}
-
-sub _find_card_src_string
-{
-    my ( $self, $src_card_s ) = @_;
-
-    my $src_col_idx = $self->_find_col_card($src_card_s);
-
-    # TODO : try to find a freecell card.
-    if ( $src_col_idx < 0 )
-    {
-        my $src_fc_idx = $self->_find_fc_card($src_card_s);
-        if ( $src_fc_idx < 0 )
-        {
-            die "Cannot find card <$src_card_s>.";
-        }
-        return ( "a card", "freecell $src_fc_idx" );
-    }
-    else
-    {
-        return ( "1 cards", "stack $src_col_idx" );
-    }
-}
 
 sub _perform_move
 {
@@ -487,70 +240,6 @@ DETECT_MOVE:
     }
     $self->_input_move_index( $self->_input_move_index + 1 );
     return;
-    if ( my ($src_card_s) = $move_line =~ /\A(.[HCDS]) to temp\z/ )
-    {
-        my $src_col_idx = $self->_find_col_card($src_card_s);
-        if ( $src_col_idx < 0 )
-        {
-            die "Cannot find card.";
-        }
-
-        my $dest_fc_idx = firstidx
-        {
-            !defined( $self->_st->get_freecell($_) )
-        }
-        ( 0 .. $self->_st->num_freecells - 1 );
-
-        if ( $dest_fc_idx < 0 )
-        {
-            die "No empty freecell.";
-        }
-
-        $self->_perform_and_output_move(
-            sprintf(
-                "Move a card from stack %d to freecell %d",
-                $src_col_idx, $dest_fc_idx,
-            ),
-        );
-
-    }
-    elsif ( ($src_card_s) = $move_line =~ /\A(.[HCDS]) out\z/ )
-    {
-        my @src_s = $self->_find_card_src_string($src_card_s);
-        $self->_perform_and_output_move(
-            sprintf( "Move a card from %s to the foundations", $src_s[1] ),
-        );
-    }
-    elsif ( ($src_card_s) = $move_line =~ /\A(.[HCDS]) to empty pile\z/ )
-    {
-        my $dest_col_idx = $self->_find_empty_col;
-        if ( $dest_col_idx < 0 )
-        {
-            die "Cannot find empty col.";
-        }
-        my @src_s = $self->_find_card_src_string($src_card_s);
-
-        $self->_perform_and_output_move(
-            sprintf( "Move %s from %s to stack %d", @src_s, $dest_col_idx ),
-        );
-    }
-    elsif ( ( $src_card_s, ( my $dest_card_s ) ) =
-        $move_line =~ /\A(.[HCDS]) to (.[HCDS])\z/ )
-    {
-        my $dest_col_idx = $self->_find_col_card($dest_card_s);
-        if ( $dest_col_idx < 0 )
-        {
-            die "Cannot find card <$dest_card_s>.";
-        }
-
-        my @src_s = $self->_find_card_src_string($src_card_s);
-        $self->_perform_and_output_move(
-            sprintf( "Move %s from %s to stack %d", @src_s, $dest_col_idx ) );
-    }
-    else
-    {
-        die "Unrecognised move_line <$move_line>";
-    }
 }
 
 sub _process_main
@@ -579,17 +268,6 @@ FIRST_lines:
     close($in_fh);
 
     $self->_append("This game is solveable.\n");
-
-    return;
-}
-
-sub run
-{
-    my ($self) = @_;
-
-    $self->_process_main;
-
-    print $self->_get_buffer;
 
     return;
 }
