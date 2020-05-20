e0a81b9560c30b504ce1bba3c6f8667bf0c8653d
Shlomi Fish <shlomif@shlomifish.org>
2020-05-20 20:16:42 +0300

    avoid XML-Feed test failures

--- fc-solve/scripts/ci-install-stage.bash
+++ fc-solve/scripts/ci-install-stage.bash
@@ -11,7 +11,7 @@ python3 -m pip install --user --upgrade cffi cookiecutter freecell_solver pycota
 cpanm --quiet --notest Dist::Zilla Pod::Coverage::TrustPod Pod::Weaver::Section::Support Test::EOL Test::NoTabs Test::Kwalitee Test::Perl::Critic::Policy Test::Pod Test::Pod::Coverage
 # For the website
 cpanm --quiet App::Deps::Verify App::XML::DocBook::Builder
-(cd fc-solve/site/wml/ && perl bin/my-cookiecutter.pl && deps-app plinst -i bin/common-required-deps.yml -i bin/required-modules.yml )
+(cd fc-solve/site/wml/ && perl bin/my-cookiecutter.pl && deps-app plinst --notest -i bin/common-required-deps.yml -i bin/required-modules.yml )
 cpanm --quiet --notest Code::TidyAll::Plugin::ClangFormat Code::TidyAll::Plugin::TestCount Games::Solitaire::Verify::HorneAutomovePrune Task::FreecellSolver::Testing
 (cd cpan/Games-Solitaire-Verify/Games-Solitaire-Verify/ && dzil authordeps --missing | cpanm --notest)
 ( cd fc-solve/site/wml && bash -x bin/install-npm-deps.sh )
