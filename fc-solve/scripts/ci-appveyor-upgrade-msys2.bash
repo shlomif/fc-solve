26826815ee7b9f1a1930bbd1248829d841a3c538
Shlomi Fish <shlomif@shlomifish.org>
2020-05-21 16:30:54 +0300

    appveyor fix #6 - msys2 .zstd vs .xz

--- fc-solve/scripts/ci-appveyor-upgrade-msys2.bash
+++ fc-solve/scripts/ci-appveyor-upgrade-msys2.bash
@@ -6,7 +6,7 @@ base_url='http://repo.msys2.org/mingw/x86_64/'
 packages="libzstd-1.4.4-2-x86_64.pkg.tar.xz pacman-5.2.1-6-x86_64.pkg.tar.xz zstd-1.4.4-2-x86_64.pkg.tar.xz"
 for p in $packages
 do
-curl ${base_url}/"$p" -o "$p"
+    curl "${base_url}$p" -o "$p"
 done
 pacman -U --noconfirm $packages
 rm -f $packages
