bb048b3c22896f6bbc383050071f31db4b938027
Shlomi Fish <shlomif@shlomifish.org>
2020-06-16 16:07:40 +0300

    optimize cookiecutter/build time

--- fc-solve/site/wml/bin/my-cookiecutter.pl
+++ fc-solve/site/wml/bin/my-cookiecutter.pl
@@ -14,11 +14,23 @@ sub _exec
     return;
 }
 
-_exec(
-    [
-        'cookiecutter', '-f', '--no-input',
-        'gh:shlomif/cookiecutter--shlomif-latemp-sites',
-        'project_slug=.',
-    ],
-    'cookiecutter failed.'
-);
+my $src_dn =
+"$ENV{HOME}/.cookiecutters/cookiecutter--shlomif-latemp-sites/\{\{cookiecutter.project_slug\}\}";
+if ( -d $src_dn )
+{
+    _exec( [ "rsync", "-ra", "$src_dn/", "./" ], "rsync failed", );
+    require Path::Tiny;
+    Path::Tiny::path("./bin/batch-inplace-html-minifier")
+        ->edit_raw( sub { s/\n\{% (?:end)?raw %\}\n/\n/g; } );
+}
+else
+{
+    _exec(
+        [
+            'cookiecutter', '-f', '--no-input',
+            'gh:shlomif/cookiecutter--shlomif-latemp-sites',
+            'project_slug=.',
+        ],
+        'cookiecutter failed.'
+    );
+}
