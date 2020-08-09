05b84f1eb6367b3e40377b0352bcda6311cf948b
Shlomi Fish <shlomif@shlomifish.org>
2020-08-09 16:44:06 +0300

    appveyor fix #2 : msys2 pgp keys

--- fc-solve/scripts/ci-appveyor-upgrade-msys2.bash
+++ fc-solve/scripts/ci-appveyor-upgrade-msys2.bash
@@ -6,7 +6,8 @@ set -e -x
 curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
 curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig
 
-pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz{.sig,}
+# pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz{.sig,}
+pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig
 pacman -U msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz
 
 if false
