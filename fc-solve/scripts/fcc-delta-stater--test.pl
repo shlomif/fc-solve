b0465052cd7f377d2e4d7140900266fc82dc6790
Shlomi Fish <shlomif@shlomifish.org>
2020-08-17 12:32:41 +0300

    check more deals

--- fc-solve/scripts/fcc-delta-stater--test.pl
+++ fc-solve/scripts/fcc-delta-stater--test.pl
@@ -62,7 +62,7 @@ sub mytest
 
     my $was_printed = '';
     open my $exe_fh,
-qq#pi-make-microsoft-freecell-board -t "$DEAL_IDX" | fc-solve -sam -sel -c -p -t -l lg --freecells-num 0 -mi 3000000 |#;
+qq#pi-make-microsoft-freecell-board -t "$DEAL_IDX" | fc-solve -l tfts --freecells-num 0 -sam -sel -c -p -t -mi 3000000 |#;
     while ( my $l = <$exe_fh> )
     {
         if ( $l =~ /\AFoundations:/ )
@@ -133,7 +133,7 @@ qq#pi-make-microsoft-freecell-board -t "$DEAL_IDX" | fc-solve -sam -sel -c -p -t
 }
 
 # foreach my $DEAL_IDX ( 210_521 .. 100_000_000 )
-foreach my $DEAL_IDX ( 20533 .. 100_000_000 )
+foreach my $DEAL_IDX ( 1 .. 100_000_000 )
 {
     mytest($DEAL_IDX);
 }
