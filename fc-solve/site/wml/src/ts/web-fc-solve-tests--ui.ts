262ccef6d5feee454eed4b01a6b36f551fe13276
Shlomi Fish <shlomif@shlomifish.org>
2020-08-27 14:01:03 +0300

    remove unused code

--- fc-solve/site/wml/src/ts/web-fc-solve-tests--ui.ts
+++ fc-solve/site/wml/src/ts/web-fc-solve-tests--ui.ts
@@ -2,7 +2,6 @@ import * as s2i from "./s2ints_js";
 import Module from "./libfcs-wrap";
 import * as w from "./web-fc-solve";
 import * as test_strings from "./web-fcs-tests-strings";
-import { perl_range } from "./prange";
 
 const FC_Solve = w.FC_Solve;
 const FC_Solve_init_wrappers_with_module = w.FC_Solve_init_wrappers_with_module;
@@ -59,21 +58,6 @@ function my_func(qunit: QUnit, _my_mod, my_callback: () => void) {
     FC_Solve_init_wrappers_with_module(_my_mod[0] || this);
 
     qunit.module("FC_Solve.Algorithmic", () => {
-        qunit.test("perl_range", (assert) => {
-            assert.expect(7);
-
-            function t(start, end, want, msg) {
-                return assert.deepEqual(perl_range(start, end), want, msg);
-            }
-            // TEST*7
-            t(0, 0, [0], "0->0");
-            t(0, 1, [0, 1], "0->1");
-            t(0, 2, [0, 1, 2], "0->2");
-            t(1, 6, [1, 2, 3, 4, 5, 6], "0->2");
-            t(-1, 1, [-1, 0, 1], "negative");
-            t(-5, -3, [-5, -4, -3], "negative");
-            t(4, 3, [], "reverse range");
-        });
         qunit.test("populate_deal", (assert) => {
             assert.expect(4);
 
