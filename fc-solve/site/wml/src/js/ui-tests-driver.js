86895672163795b16b7e07dda95facce66d1e45d
Shlomi Fish <shlomif@shlomifish.org>
2020-09-07 14:34:56 +0300

    Extract a common module/library/header.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- fc-solve/site/wml/src/js/ui-tests-driver.js
+++ fc-solve/site/wml/src/js/ui-tests-driver.js
@@ -1,33 +1,3 @@
-define(["web-fc-solve-tests--ui", "qunit"], function (web_fc_solve_tests, QUnit) {
-    QUnit.config.autostart = false;
-    return function() {
-        const _log_cb = ((window.location.href.indexOf("alert") >= 0) ? alert : console.log);
-        if (false) {
-            QUnit.done(function( details ) {
-                //alert( "Total: "+ details.total+ " Failed: "+ details.failed+ " Passed: "+ details.passed+ " Runtime: "+ details.runtime );
-                _log_cb( "Total: "+ details.total+ " Failed: "+ details.failed+ " Passed: "+ details.passed+ " Runtime: "+ details.runtime );
-            });
-            QUnit.moduleDone( ( { name, failed, total } ) => {
-                //alert( `Finished running module : ${name} Failed/total: ${failed}, ${total}` );
-                _log_cb( `Finished running module : ${name} Failed/total: ${failed}, ${total}` );
-            });
-        };
-
-        const test_js_fc_solve_class = web_fc_solve_tests.test_js_fc_solve_class;
-        let was_called = false;
-        try {
-            test_js_fc_solve_class(QUnit, function () {
-                QUnit.start();
-                was_called = true;
-                return;
-            });
-        } catch (err) {
-            alert(err);
-            QUnit.module("Meta");
-            QUnit.test("global_failure", function(assert) {
-                assert.ok (false, 'test_js_fc_solve_class() or test_fcs_validate() have thrown an exception or are invalid.');
-            });
-            throw err;
-        }
-    };
+define(["web-fc-solve-tests--ui", "generic-tests-driver"], function (web_fc_solve_tests, tests_driver) {
+    return tests_driver(web_fc_solve_tests, function(_q){return;});
 });
