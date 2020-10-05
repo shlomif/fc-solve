e129cafee2b42d6e5062733ebab8a90c79dad8df
Shlomi Fish <shlomif@shlomifish.org>
2020-10-05 14:08:59 +0300

    eliminate jquery-ui errors/warnings

---
+++ fc-solve/site/wml/src/js/jquery-ui/safe-active-element.js
@@ -0,0 +1,4 @@
+/*! jQuery UI - v1.13.0-pre - 2020-10-05
+* http://jqueryui.com
+* Copyright jQuery Foundation and other contributors; Licensed  */
+!function(a){"function"==typeof define&&define.amd?define(["jquery","./version"],a):a(jQuery)}(function(a){return a.ui.safeActiveElement=function(a){var b;try{b=a.activeElement}catch(c){b=a.body}return b||(b=a.body),b.nodeName||(b=a.body),b}});
---
+++ fc-solve/site/wml/src/js/jquery-ui/version.js
@@ -0,0 +1,4 @@
+/*! jQuery UI - v1.13.0-pre - 2020-10-05
+* http://jqueryui.com
+* Copyright jQuery Foundation and other contributors; Licensed  */
+!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):a(jQuery)}(function(a){return a.ui=a.ui||{},a.ui.version="@VERSION"});
