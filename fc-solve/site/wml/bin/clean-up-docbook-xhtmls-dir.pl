ac88e3604d8a9956b5c13e93e18805778f32292a
Shlomi Fish <shlomif@shlomifish.org>
2020-11-03 10:00:33 +0200

    fix the website build.
    Copy files from https://github.com/shlomif/shlomi-fish-homepage .
    Caught by travis-ci

---
+++ fc-solve/site/wml/bin/clean-up-docbook-xhtmls-dir.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+use lib './lib';
+
+use Getopt::Long qw/ GetOptions /;
+use Path::Tiny qw/ path /;
+
+use Shlomif::RenderDocBookIndiv ();
+
+my @dirs;
+
+GetOptions( "dir=s" => \@dirs, ) or die $!;
+
+foreach my $dir (@dirs)
+{
+    Shlomif::RenderDocBookIndiv::render_docbook( { htmls_dir => $dir } );
+}
