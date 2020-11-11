d4c081a5eff89d27752f1022053020c09dcdbe5f
Shlomi Fish <shlomif@shlomifish.org>
2020-11-11 13:41:25 +0200

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- fc-solve/scripts/opencl-test.bash
+++ fc-solve/scripts/opencl-test.bash
@@ -4,7 +4,7 @@ run_deal="$deal"
 (
     # ( python3 ../../source/board_gen/find-freecell-deal-index-julia-opencl.py --ms <(pi-make-microsoft-freecell-board -t "$deal" ) && \
     gcc -shared -fPIC -O3 -march=native -o lib"opencl_find_deal_idx.so" -I ~/Download/unpack/to-del/www.dmi.unict.it/bilotta/gpgpu/svolti/aa201920/opencl/ -Wall -Wextra "../../source/board_gen/opencl_find_deal_idx.c" -lOpenCL && \
-(
-    export LD_LIBRARY_PATH="$PWD" ; time python3 ../../source/board_gen/find-freecell-deal-index-using-opencl.py --ms <(pi-make-microsoft-freecell-board -t "$run_deal" )
-    )
+       (
+            time python3 ../../source/board_gen/find-freecell-deal-index-using-opencl.py --ms <(pi-make-microsoft-freecell-board -t "$run_deal" )
+        )
 )
