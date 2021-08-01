c813ec407675c6ccc4f739d788f64668fe34a2d6
Shlomi Fish <shlomif@shlomifish.org>
2021-08-01 12:49:10 +0300

    $convert to typescript [for strictness]\n\nBased on https://github.com/shlomif/shlomi-fish-homepage/tree/master/src/ts

---
+++ fc-solve/site/wml/src/ts/toggle_sect.ts
@@ -0,0 +1,33 @@
+"use strict";
+
+const toggle_sect_key: string = "fc_solve_section_navigation_menu_shown";
+const toggle_main_key: string = "fc_solve_main_navigation_menu_shown";
+
+function build_sect_nav_menu_toggler(): void {
+    build_toggler({
+        default_state: false,
+        hide_text: "Hide",
+        ls_key: toggle_sect_key,
+        show_text: "Show",
+        toggled_class: "novis",
+        toggled_selector: "#sect_menu_wrapper",
+        toggler_selector: "#toggle_sect_menu",
+    });
+}
+
+function build_main_nav_menu_toggler(): void {
+    build_toggler({
+        default_state: false,
+        hide_text: "Hide NavBar ⬈",
+        ls_key: toggle_main_key,
+        show_text: "Show NavBar ⬋",
+        toggled_class: "novis",
+        toggled_selector: "#navbar , #navbar_wrapper, #main",
+        toggler_selector: "#show_navbar",
+    });
+}
+
+$(document).ready(function () {
+    build_sect_nav_menu_toggler();
+    build_main_nav_menu_toggler();
+});
