0c0a978bf87bd26a07e823870831fe812ac80f49
Shlomi Fish <shlomif@shlomifish.org>
2021-08-08 16:11:36 +0300

    work on converting from jinja to TT
    getting-credit page

--- fc-solve/site/wml/bin/tot-master.bash
+++ fc-solve/site/wml/bin/tot-master.bash
@@ -1,5 +1,5 @@
 ./gen-helpers && make fastrender && make -j 17
-a="dest/arch_doc/index.html"
+a="dest/getting-credit.html"
 b="/home/shlomif/Backup/Arcs/fcs-site--post-dest/post-incs/${a##dest/}"
 adest=have.xhtml
 bdest=want.xhtml
