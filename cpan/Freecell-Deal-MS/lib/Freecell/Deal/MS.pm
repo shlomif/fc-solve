aea84db3ed59ffc3c92d48ebfbde71ce00a23c5c
Shlomi Fish <shlomif@shlomifish.org>
2021-12-06 14:21:07 +0200

    work on cpan/Freecell-Deal-MS

---
+++ cpan/Freecell-Deal-MS/lib/Freecell/Deal/MS.pm
@@ -0,0 +1,54 @@
+package Freecell::Deal::MS;
+
+use strict;
+use warnings;
+
+use Math::RNG::Microsoft ();
+
+use Class::XSAccessor {
+    constructor => 'new',
+    accessors   => [qw(deal)],
+};
+
+sub as_str
+{
+    my ($self) = @_;
+
+    my @cards = (
+        map {
+            my $s = $_;
+            map { $s . $_ } qw/C D H S/;
+        } ( 'A', ( 2 .. 9 ), 'T', 'J', 'Q', 'K' )
+    );
+    Math::RNG::Microsoft->new( seed => scalar( $self->deal ) )
+        ->shuffle( \@cards );
+    my @lines = ( map { [ ':', ] } 0 .. 7 );
+    my $i     = 0;
+    while (@cards)
+    {
+        push @{ $lines[$i] }, pop(@cards);
+        $i = ( ( $i + 1 ) & 7 );
+    }
+    my $str = join "", map { "@$_\n" } @lines;
+    return $str;
+}
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+Freecell::Deal::MS - deal Windows FreeCell / FC Pro layouts
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2
+
+=head2
+
+=cut
