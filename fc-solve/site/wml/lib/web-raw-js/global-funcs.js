b9efd5e3f2b89a0f411d3a3b92523c50672b50ca
Shlomi Fish <shlomif@shlomifish.org>
2022-05-16 14:14:57 +0300

    lto: extract module

--- fc-solve/site/wml/lib/web-raw-js/global-funcs.js
+++ fc-solve/site/wml/lib/web-raw-js/global-funcs.js
@@ -33,3 +33,38 @@ window._set_up_tabs = () => {
     });
     return;
 };
+window._find_deal_set_up = ()=> {
+    const controls = $('.phoenix');
+    if (window.location.search.length) {
+        controls.phoenix('remove');
+    } else {
+        controls.phoenix();
+    }
+    $( "#fc_solve_tabs" ).tabs(
+        {
+            activate: ()=> {
+                window.my_web_fc_solve_ui.set_up_handlers(module_wrapper);
+
+                return;
+            },
+            beforeActivate: (event, ui)=> {
+                const mynew = ui.newPanel;
+                const old = ui.oldPanel;
+
+                const common_old = old.find('.fcs_common');
+                const old_text = common_old.find('textarea').val();
+
+                const common_html = common_old.html();
+
+                common_old.html('');
+
+                const common_new = mynew.find('.fcs_common');
+
+                common_new.html(common_html);
+                common_new.find('textarea').val(old_text);
+
+                return;
+            }
+    });
+    return;
+};
