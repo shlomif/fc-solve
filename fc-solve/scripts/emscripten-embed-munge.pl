b8381a6f3e02728b6e4530052b570a3d7abb7336
Shlomi Fish <shlomif@shlomifish.org>
2022-09-28 15:50:11 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- fc-solve/scripts/emscripten-embed-munge.pl
+++ fc-solve/scripts/emscripten-embed-munge.pl
@@ -4,10 +4,21 @@ use strict;
 use warnings;
 use 5.014;
 
-my $dir = shift(@ARGV);
+use File::Find::Object ();
+my $dir        = shift(@ARGV);
+my $PRESET_DIR = "/fc-solve/share/freecell-solver/";
+my $tree       = File::Find::Object->new( {}, "$dir$PRESET_DIR" );
+my @f;
+while ( my $fn = $tree->next() )
+{
+    if ( -f $fn )
+    {
+        push @f, $fn;
+    }
+}
 
 my @output;
-foreach my $fn (@ARGV)
+foreach my $fn (@f)
 {
     push @output, $fn =~ s#\A(\Q$dir\E(/.*))\z#$1\@$2#r;
 }
