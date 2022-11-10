9fc92bff3023c4edc7902c9ad2f9721adf29fee8
Shlomi Fish <shlomif@shlomifish.org>
2022-11-10 14:49:46 +0200

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- fc-solve/scripts/opencl-test.bash
+++ fc-solve/scripts/opencl-test.bash
@@ -15,14 +15,19 @@ run_deal="$deal"
     board_gen_dir="../../source/board_gen"
     test -d "$board_gen_dir"
     gen_ocl_py_prog="$board_gen_dir"/find-freecell-deal-index-julia-opencl.py
+    lib=libfcs_find_deal.so
+    if test \( \! -e "$lib" \) -o \( "$REBUILD" = "1" \)
+    then
+        ${CC:-clang} -shared -fPIC -O3 -march=native -flto -o "$lib" -I ~/Download/unpack/to-del/www.dmi.unict.it/bilotta/gpgpu/svolti/aa201920/opencl/ -I "$board_gen_dir" -I "$board_gen_dir/.." ${WCFLAGS:--Weverything} "find_deal.c"
+    fi
     test -f "$gen_ocl_py_prog"
     lib=libopencl_find_deal_idx.so
-    if ( ! test -e "$lib" ) || ( test "$REBUILD" == "1" )
+    if test \( \! -e "$lib" \) -o \( "$REBUILD" = "1" \)
     then
         python3 "$gen_ocl_py_prog" --ms <(pi-make-microsoft-freecell-board -t "$deal")
         ${CC:-clang} -shared -fPIC -O3 -march=native -flto -o "$lib" -I ~/Download/unpack/to-del/www.dmi.unict.it/bilotta/gpgpu/svolti/aa201920/opencl/ -I "$board_gen_dir" ${WCFLAGS:--Weverything} "opencl_find_deal_idx.c" -lOpenCL
     fi
-    if ! test "$SKIP_CISH" == 1
+    if ! test "$SKIP_CISH" = 1
     then
         time python3 "$board_gen_dir"/find-freecell-deal-index-cish.py --ms <(pi-make-microsoft-freecell-board -t "$run_deal")
     fi
