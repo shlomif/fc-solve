8fde78eb419d90c85c36d0e3766bc7930539743a
Shlomi Fish <shlomif@shlomifish.org>
2023-12-08 20:58:52 +0200

    Fix the indentation.
    See http://perl-begin.org/tutorials/bad-elements/#no-indentation .

--- fc-solve/site/wml/bin/gen_listboxes.py
+++ fc-solve/site/wml/bin/gen_listboxes.py
@@ -18,31 +18,39 @@ listbox_template = (
     '</div>'
 )
 
+
 def _gen_options(start, end):
     """docstring for _gen_options"""
     return ''.join(
-            ['<option value="{num}">{num}</option>'.format(num=num)
-             for num in range(start, end+1)])
+        ['<option value="{num}">{num}</option>'.format(num=num)
+         for num in range(start, end+1)]
+    )
+
 
 fc_listbox_id = "num_freecells"
 fc_listbox = (listbox_template).format(
         id=fc_listbox_id,
         label_text='Freecells Number:',
-        options=_gen_options(start=0, end=8))
+        options=_gen_options(start=0, end=8)
+)
 
 cols_listbox_id = "num_columns"
 cols_listbox = (listbox_template).format(
         id=cols_listbox_id,
         label_text='Columns Number:',
-        options=_gen_options(start=1, end=12))
+        options=_gen_options(start=1, end=12)
+)
+
 
 def out(out_fn, text):
     """docstring for out"""
     with open(out_fn, 'wt') as f:
         f.write(text)
 
+
 def o_htmlish(base, text):
     return out('lib/' + base + '.htmlish', text)
+
 
 o_htmlish('fc_listbox', fc_listbox)
 o_htmlish('cols_listbox', cols_listbox)
