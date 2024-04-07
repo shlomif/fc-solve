8ea36ad4bd0f5e65ca81e3d360086fc867cb79d5
Shlomi Fish <shlomif@shlomifish.org>
2024-04-07 14:16:34 +0300

    try to fix gh actions/CI (website tests)
    Configure eslint; it does not like .yml configuration files.

---
+++ fc-solve/site/wml/eslint.js-rather-than-ts.config.mjs
@@ -0,0 +1,14 @@
+export default {
+    "extends": "google",
+    "parserOptions": { "ecmaVersion": 7 },
+    "rules": {
+        "camelcase": "off",
+        "indent": ["error", 4],
+        "new-cap": "off",
+        "no-invalid-this": "off",
+        "no-throw-literal": "off",
+        "no-unused-vars": "off",
+        "quotes": "off",
+        "require-jsdoc": "off",
+    },
+};
