5f47cb329f23abb3fdbcbaab9f5afa5a5874daab
Shlomi Fish <shlomif@shlomifish.org>
2024-06-14 09:16:38 +0300

    [load-time-optimization] improve expand-moves
    We did not really needed libfcs-wrap .
    
    Also see https://www.shlomifish.org/meta/FAQ/site_loads_quickly.xhtml

--- fc-solve/site/wml/src/ts/expand-moves-ui.ts
+++ fc-solve/site/wml/src/ts/expand-moves-ui.ts
@@ -2,23 +2,15 @@ import * as s2i from "./s2ints_js";
 import * as BaseApi from "./web-fcs-api-base";
 import * as base_ui from "./fcs-base-ui";
 import * as expander from "./web-fc-solve--expand-moves";
-import Module from "./libfcs-wrap";
 import * as w from "./find-fc-deal";
 
 let _module_wrapper: w.ModuleWrapper;
 
 export function init_module(cb: (mw: BaseApi.ModuleWrapper) => any): any {
-    const _my_module = Module({
-        onRuntimeInitialized: () => {
-            _my_module.then((result) => {
-                const module_wrapper =
-                    w.FC_Solve_init_wrappers_with_module(result);
-                _module_wrapper = module_wrapper;
-                cb(module_wrapper);
-                return 0;
-            });
-        },
-    });
+    const module_wrapper = null;
+    _module_wrapper = module_wrapper;
+    cb(module_wrapper);
+    return;
 }
 
 function _create_bmark_obj() {
