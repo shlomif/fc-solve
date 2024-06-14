41a313de49a2e53d297beb5fbe18e7fe8b1018f1
Shlomi Fish <shlomif@shlomifish.org>
2024-06-14 11:01:31 +0300

    [load-time-optimization] improve expand-moves

--- fc-solve/site/wml/src/ts/expand-moves-ui.ts
+++ fc-solve/site/wml/src/ts/expand-moves-ui.ts
@@ -63,11 +63,7 @@ export function expand_ui(): void {
 
 export let on__expand__click: () => void = null;
 export let on__run_do_solve__click: () => void = null;
-export let populate_input_pressed: () => void = null;
 export function set_up_handlers(module_wrapper: w.ModuleWrapper): void {
-    populate_input_pressed = () => {
-        return base_ui.populate_input_with_numbered_deal(module_wrapper, w);
-    };
     on__expand__click = expand_ui;
     on__run_do_solve__click = expand_ui;
     base_ui.set_up__capitalize_cards();
