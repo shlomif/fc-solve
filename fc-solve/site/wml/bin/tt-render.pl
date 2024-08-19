b5929178637e27b0fc4cc8ee6914d85510f8cca5
Shlomi Fish <shlomif@shlomifish.org>
2024-08-19 17:46:32 +0300

    investigate TT3 overridden block/scoping issue
    more minimal example

--- fc-solve/site/wml/bin/tt-render.pl
+++ fc-solve/site/wml/bin/tt-render.pl
@@ -6,19 +6,11 @@ use 5.014;
 
 use lib './lib';
 
-use Getopt::Long                   qw/ GetOptions /;
-use Path::Tiny                     qw/ path /;
 use FreecellSolver::Site::TTRender ();
 
 my $printable;
 my $stdout;
 my @filenames;
-
-GetOptions(
-    'fn=s'       => \@filenames,
-    'printable!' => \$printable,
-    'stdout!'    => \$stdout,
-) or die $!;
 
 my $obj = FreecellSolver::Site::TTRender->new(
     { printable => $printable, stdout => $stdout, } );
