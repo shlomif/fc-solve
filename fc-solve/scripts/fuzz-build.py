5ed78a8a1783e9818afa1e7769e3952733052f78
Shlomi Fish <shlomif@shlomifish.org>
2024-09-06 18:33:52 +0300

    Add a --byorder option to fuzz-build.py and Tatzer
    The count is not accurate for num_args > 1

--- fc-solve/scripts/fuzz-build.py
+++ fc-solve/scripts/fuzz-build.py
@@ -14,11 +14,12 @@ from subprocess import check_call, check_output
 
 
 @click.command()
+@click.option('--byorder/--no-byorder')
 @click.option('--clean/--no-clean')
 @click.option('-g/--no-g')
 @click.option('-t/--no-t')
 @click.option('--rb/--no-rb')
-def main(clean, g, t, rb):
+def main(byorder, clean, g, t, rb):
     if g:
         os.environ["FCS_GCC"] = "1"
         seed = 1 if rb else 1
@@ -27,6 +28,13 @@ def main(clean, g, t, rb):
         os.environ["CXX"] = check_output(['which', 'clang++']).decode().strip()
         os.environ["FCS_CLANG"] = "1"
         seed = 1 if rb else 1
+    num_flags = -1
+    if byorder:
+        query_flag = "FCS_THEME_RAND_TELL_FLAGS_COUNT"
+        os.environ[query_flag] = "1"
+        query_cmd = "../scripts/Tatzer"
+        num_flags = int(check_output([query_cmd]).decode().strip())
+        del os.environ[query_flag]
     os.environ["HARNESS_BREAK"] = "1"
     SLIGHTLY_WRONG_GCC_FLAG_SEE_man_gcc =\
         "-frandom-seed = 24"
@@ -42,6 +50,28 @@ def main(clean, g, t, rb):
     if rb:
         os.environ["REPRODUCIBLE_BUILDS"] = "1"
         cmd = 'bash -c ". ~/.bashrc && Theme fcs && _reprb_diff_builds"'
+    if byorder:
+        os.environ["FCS_THEME_RAND_BY_ORDER"] = "1"
+        while True:
+            for num_args in [1,]:
+                for pos in range(num_flags):
+                    print("Checking pos = {} numargs = {}".format(
+                        pos, num_args))
+                    os.environ["FCS_THEME_RAND"] = "{}".format(pos)
+                    os.environ["FCS_THEME_RAND_COUNT"] = "{}".format(num_args)
+                    try:
+                        check_call(["bash", "-c", cmd, ])
+                    except Exception as e:
+                        print(e)
+                        print("pos = {} numargs = {} failed".format(
+                            pos, num_args))
+                        sys.exit(1)
+                    if clean:
+                        fn = "run-t-{}-{}.bash".format(pos, num_args)
+                        check_call(["cat", fn])
+                        os.unlink(fn)
+            return
+
     while True:
         for num_args in range(1, 10+1):
             print("Checking seed = {} numargs = {}".format(seed, num_args))
