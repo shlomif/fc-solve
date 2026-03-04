75cb074a7c640c9c983a301c1c4e6ec8417d710f
Shlomi Fish <shlomif@shlomifish.org>
2026-03-04 15:23:35 +0200

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- fc-solve/presets/soft-threads/meta-moves/auto-gen/iterate-over-all-constant-quotas.bash
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/iterate-over-all-constant-quotas.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in `seq 100 1000` ; do
+for (( i=100 ; i<=1000 ; ++i ))
+do
     perl process.pl --quotas-expr "(($i)x1000)" |
-    printf "%d\\t%d\\n" "$i" "$(perl -lne 'print "$i\t$1" if m/^total_iters = (\d+)/')" ;
+        printf "%d\\t%d\\n" "$i" "$(perl -lne 'print "$1" if m/^total_iters = ([0-9]+)/')"
 done
