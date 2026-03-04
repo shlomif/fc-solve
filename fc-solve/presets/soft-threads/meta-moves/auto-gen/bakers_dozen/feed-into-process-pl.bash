b2a0d445e783e8ac724c93a7ac4e1360ffcd18cc
Shlomi Fish <shlomif@shlomifish.org>
2026-03-04 15:49:40 +0200

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- fc-solve/presets/soft-threads/meta-moves/auto-gen/bakers_dozen/feed-into-process-pl.bash
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/bakers_dozen/feed-into-process-pl.bash
@@ -2,7 +2,8 @@
 # Run this from ${trunk}/fc-solve/presets/soft-threads/meta-moves/auto-gen
 (
     ruby ../optimize-seq/scripts/process-optimal_quotas-dump.rb \
-    find_optimal_quotas-300.dump) | \
+    find_optimal_quotas-300.dump
+) | \
     perl -I.. ../process.pl --quotas-expr="$(
         echo -n '(';
         ruby -p -e 'sub(/\n/,",");END{print "((350)x#{300-$.}),";}' ;
--- fc-solve/presets/soft-threads/meta-moves/auto-gen/optimize-seq/scripts/feed-into-process-pl.bash
+++ fc-solve/presets/soft-threads/meta-moves/auto-gen/optimize-seq/scripts/feed-into-process-pl.bash
@@ -3,7 +3,8 @@
 path="$(pwd)"
 (cd optimize-seq &&
     ruby scripts/process-optimal_quotas-dump.rb \
-    "${path}/find_optimal_quotas-300.dump") | \
+    "${path}/find_optimal_quotas-300.dump"
+) | \
     perl process.pl --quotas-expr="$(
         echo -n '(';
         ruby -p -e 'sub(/\n/,",");END{print "((350)x#{300-$.}),";}' ;
